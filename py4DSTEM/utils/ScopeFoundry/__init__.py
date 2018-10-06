ec8a19c480d514cafd5318dfe7a1e5a99cbf5e9e
bsavitzky <ben.savitzky@gmail.com>
2018-10-05 17:51:14 -0700

    Competes removal of ScopeFoundry dependency.
    Former-commit-id: 16fd1f31acff6211a5721290f6dafd432bf3a93f

--- py4DSTEM/utils/ScopeFoundry/__init__.py
+++
@@ -1,4 +0,0 @@
-from .base_gui import BaseMicroscopeApp, BaseApp
-from .measurement import Measurement
-from .hardware import HardwareComponent
-from .logged_quantity import LoggedQuantity, LQRange, LQCollection
