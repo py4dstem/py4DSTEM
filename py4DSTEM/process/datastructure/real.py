c469319523f06b2dbd3e1ba1dcd3617e33d999bb
bsavitzky <ben.savitzky@gmail.com>
2018-10-28 19:50:25 -0700

    Edits remaining data objects - DiffractionSlice, RealSlice, and PointList - so that they all inherit from DataObject.

--- py4DSTEM/process/datastructure/real.py
+++ py4DSTEM/process/datastructure/real.py
@@ -4,16 +4,20 @@
 # RealSlice objects are generated from parent DataCube objects with various processing
 # functions, and must contain a reference to their parent DataCube instance.
 
-class RealSlice(object):
+from .dataobject import DataObject
+
+class RealSlice(DataObject):
 
     def __init__(self, data, parentDataCube):
         """
         Instantiate a RealSlice object.  Set the parent datacube, dimensions, and data.
         Confirms that the data shape agrees with real space of the parent datacube.
         """
+        DataObject.__init__(self, parent=parentDataCube)
+
         self.parentDataCube = parentDataCube
-        self.R_Ny, self.R_Nx = self.parentDataCube.R_Ny, self.R_Nx
-        assert data.shape == (self.R_Ny,self.R_Nx), "Shape of data is {}, but shape of diffraction space in parent DataCube is ({},{}).".format(data.shape,self.R_Ny,self.R_Nx)
+        self.R_Ny, self.R_Nx = self.parentDataCube.R_Ny, self.parentDataCube.R_Nx
+        assert data.shape == (self.R_Ny,self.R_Nx), "Shape of data is {}, but shape of real space in parent DataCube is ({},{}).".format(data.shape,self.R_Ny,self.R_Nx)
         self.data2D = data
 
 
