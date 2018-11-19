fa89d60824a9ed78ab98877a9bc2f32c8d867171
bsavitzky <ben.savitzky@gmail.com>
2018-11-19 09:18:05 -0800

    Begins addition of FileObjectBrowser class.
    Former-commit-id: 8fc2cd0b4d66ff7f44580b7308732624ddfc3185

--- py4DSTEM/readwrite/reader.py
+++ py4DSTEM/readwrite/reader.py
@@ -6,6 +6,85 @@ import hyperspy.api as hs
 from ..process.datastructure.datacube import RawDataCube
 from ..process.log import log
 
+
+class FileObjectBrowser(object):
+
+    def __init__(self, filepath):
+        self.filepath = filepath
+        self.is_py4DSTEMfile = is_py4DSTEMfile(filepath)
+        if self.is_py4DSTEMfile:
+            self.version = get_py4DSTEM_version
+
+
+###################### END FileObjectBrowser OBJECT #######################
+
+
+def is_py4DSTEM_file(h5_file):
+    if ('version_major' in h5_file.attrs) and ('version_minor' in h5_file.attrs) and (('4DSTEM_experiment' in h5_file.keys()) or ('4D-STEM_data' in h5_file.keys())):
+        return True
+    else:
+        return False
+
+def get_py4DSTEM_version(h5_file):
+    version_major = h5_file.attrs['version_major']
+    version_minor = h5_file.attrs['version_minor']
+    return version_major, version_minor
+
+
+
+
+
+@log
+def read(filename):
+    """
+    Takes a filename as input, and outputs a RawDataCube object.
+
+    If filename is a .h5 file, read_data() checks if the file was written by py4DSTEM.  If it
+    was, the metadata are read and saved directly.  Otherwise, the file is read with hyperspy,
+    and metadata is scraped and saved from the hyperspy file.
+    """
+    print("Reading file {}...\n".format(filename))
+    # Check if file was written by py4DSTEM
+    try:
+        h5_file = h5py.File(filename,'r')
+        if is_py4DSTEMfile(h5_file):
+            print("{} is a py4DSTEM HDF5 file.  Reading...".format(filename))
+            R_Ny,R_Nx,Q_Ny,Q_Nx = h5_file['4DSTEM_experiment']['rawdatacube']['datacube'].shape
+            rawdatacube = RawDataCube(data=h5_file['4DSTEM_experiment']['rawdatacube']['datacube'].value,
+                            R_Ny=R_Ny, R_Nx=R_Nx, Q_Ny=Q_Ny, Q_Nx=Q_Nx,
+                            is_py4DSTEM_file=True, h5_file=h5_file)
+            h5_file.close()
+            return rawdatacube
+        else:
+            h5_file.close()
+    except IOError:
+        pass
+
+    # Use hyperspy
+    print("{} is not a py4DSTEM file.  Reading with hyperspy...".format(filename))
+    try:
+        hyperspy_file = hs.load(filename)
+        if len(hyperspy_file.data.shape)==3:
+            R_N, Q_Ny, Q_Nx = hyperspy_file.data.shape
+            R_Ny, R_Nx = R_N, 1
+        elif len(hyperspy_file.data.shape)==4:
+            R_Ny, R_Nx, Q_Ny, Q_Nx = hyperspy_file.data.shape
+        else:
+            print("Error: unexpected raw data shape of {}".format(hyperspy_file.data.shape))
+            print("Initializing random datacube...")
+            return RawDataCube(data=np.random.rand(100,512,512),
+                            R_Ny=10,R_Nx=10,Q_Ny=512,Q_Nx=512,
+                            is_py4DSTEM_file=False)
+        return RawDataCube(data=hyperspy_file.data, R_Ny=R_Ny, R_Nx=R_Nx, Q_Ny=Q_Ny, Q_Nx=Q_Nx,
+                            is_py4DSTEM_file=False,
+                            original_metadata_shortlist=hyperspy_file.metadata,
+                            original_metadata_all=hyperspy_file.original_metadata)
+    except Exception as err:
+        print("Failed to load", err)
+        print("Initializing random datacube...")
+        return RawDataCube(data=np.random.rand(100,512,512),R_Ny=10,R_Nx=10,Q_Ny=512,Q_Nx=512,
+                           is_py4DSTEM_file=False)
+
 @log
 def read_data(filename):
     """
@@ -56,13 +135,6 @@ def read_data(filename):
         print("Initializing random datacube...")
         return RawDataCube(data=np.random.rand(100,512,512),R_Ny=10,R_Nx=10,Q_Ny=512,Q_Nx=512,
                            is_py4DSTEM_file=False)
-
-
-def is_py4DSTEMfile(h5_file):
-    if ('version_major' in h5_file.attrs) and ('version_minor' in h5_file.attrs) and ('4DSTEM_experiment' in h5_file.keys()):
-        return True
-    else:
-        return False
 
 
 def show_log(filename):
