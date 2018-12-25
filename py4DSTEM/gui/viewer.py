747a3a17bd48a19cb3cce5f5d3baa68ee31f1694
Ben Savitzky <ben.savitzky@gmail.com>
2018-12-25 15:00:12 -0800

    Adds rectangular, circular, and annular ROIs for virtual detectors.

--- py4DSTEM/gui/viewer.py
+++ py4DSTEM/gui/viewer.py
@@ -161,7 +161,7 @@ class DataViewer(QtWidgets.QMainWindow):
         self.diffraction_space_widget = pg.ImageView()
         self.diffraction_space_widget.setImage(np.zeros((512,512)))
 
-        # Create virtual detector ROI selector 
+        # Create virtual detector ROI selector
         self.virtual_detector_roi = pg.RectROI([256, 256], [50,50], pen=(3,9))
         self.diffraction_space_widget.getView().addItem(self.virtual_detector_roi)
         self.virtual_detector_roi.sigRegionChanged.connect(self.update_real_space_view)
@@ -525,8 +525,72 @@ class DataViewer(QtWidgets.QMainWindow):
             3: Annular
         """
         detector_shape = self.settings.virtual_detector_shape.val
-        print(detector_shape)
+        x,y = self.diffraction_space_view.shape
+        x0,y0 = x/2, y/2
+        xr,yr = x/10,y/10
+
+        # Remove existing detector
+        if hasattr(self,'virtual_detector_roi'):
+            self.diffraction_space_widget.view.scene().removeItem(self.virtual_detector_roi)
+        if hasattr(self,'virtual_detector_roi_inner'):
+            self.diffraction_space_widget.view.scene().removeItem(self.virtual_detector_roi_inner)
+        if hasattr(self,'virtual_detector_roi_outer'):
+            self.diffraction_space_widget.view.scene().removeItem(self.virtual_detector_roi_outer)
+
+        # Rectangular detector
+        if detector_shape==0:
+            self.virtual_detector_roi = pg.RectROI([int(x0-xr/2),int(y0-yr/2)], [int(xr),int(yr)], pen=(3,9))
+            self.diffraction_space_widget.getView().addItem(self.virtual_detector_roi)
+            self.virtual_detector_roi.sigRegionChanged.connect(self.update_real_space_view)
+
+        # Circular detector
+        elif detector_shape==1:
+            self.virtual_detector_roi = pg.CircleROI([int(x0-xr/2),int(y0-yr/2)], [int(xr),int(yr)], pen=(3,9))
+            self.diffraction_space_widget.getView().addItem(self.virtual_detector_roi)
+            self.virtual_detector_roi.sigRegionChanged.connect(self.update_real_space_view)
+
+        # Annular dector
+        elif detector_shape==2:
+            # Make outer detector
+            self.virtual_detector_roi_outer = pg.CircleROI([int(x0-xr),int(y0-yr)], [int(2*xr),int(2*yr)], pen=(3,9))
+            self.diffraction_space_widget.getView().addItem(self.virtual_detector_roi_outer)
+
+            # Make inner detector
+            self.virtual_detector_roi_inner = pg.CircleROI([int(x0-xr/2),int(y0-yr/2)], [int(xr),int(yr)], pen=(4,9), movable=False)
+            self.diffraction_space_widget.getView().addItem(self.virtual_detector_roi_inner)
+
+            # Connect size/position of inner and outer detectors
+            self.virtual_detector_roi_outer.sigRegionChanged.connect(self.update_annulus_pos)
+            self.virtual_detector_roi_outer.sigRegionChangeFinished.connect(self.update_annulus_radii)
+            self.virtual_detector_roi_inner.sigRegionChangeFinished.connect(self.update_annulus_radii)
+
+
+            #self.virtual_detector_roi_outer.sigRegionChanged.connect(self.update_real_space_view)
+
+        else:
+            print("Error: unknown detector shape value {}.  Must be 0, 1, or 2.".format(dector_shape))
+
         pass
+
+    def update_annulus_pos(self):
+        """
+        Function to keep inner and outer rings of annulus aligned.
+        """
+        R_outer = self.virtual_detector_roi_outer.size().x()/2
+        R_inner = self.virtual_detector_roi_inner.size().x()/2
+        # Only outer annulus is draggable; when it moves, update position of inner annulus
+        x0 = self.virtual_detector_roi_outer.pos().x() + R_outer
+        y0 = self.virtual_detector_roi_outer.pos().y() + R_outer
+        self.virtual_detector_roi_inner.setPos(x0-R_inner, y0-R_inner)
+
+    def update_annulus_radii(self):
+        R_outer = self.virtual_detector_roi_outer.size().x()/2
+        R_inner = self.virtual_detector_roi_inner.size().x()/2
+        if R_outer < R_inner:
+            x0 = self.virtual_detector_roi_outer.pos().x() + R_outer
+            y0 = self.virtual_detector_roi_outer.pos().y() + R_outer
+            self.virtual_detector_roi_outer.setSize(2*R_inner+6)
+            self.virtual_detector_roi_outer.setPos(x0-R_inner-3,y0-R_inner-3)
 
     def update_virtual_detector_mode(self):
         """
