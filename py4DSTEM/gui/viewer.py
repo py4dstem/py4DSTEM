4e9da4eff41ed675f6752f4a0e2649c5ae8441db
Ben Savitzky <ben.savitzky@gmail.com>
2019-02-21 15:26:20 -0800

    Makes notebooks/basic_demos/saving_and_loading.ipynb. Completes creating dataobjects, searching dataobjects, and saving .h5 files from dataobjects.
    Former-commit-id: 012a081cf90432bf884307f1f3aed1c46251c4c2

--- py4DSTEM/gui/viewer.py
+++ py4DSTEM/gui/viewer.py
@@ -28,7 +28,7 @@ import gc
 from .dialogs import ControlPanel, PreprocessingWidget, SaveWidget, EditMetadataWidget
 from .utils import sibling_path, pg_point_roi, LQCollection
 from ..readwrite.reader import read_data
-from ..readwrite.writer import save_datacube
+from ..readwrite.writer import save_dataobject
 from ..process.datastructure.datacube import DataCube
 
 import IPython
@@ -511,7 +511,7 @@ class DataViewer(QtWidgets.QMainWindow):
     def execute_saveas(self):
         f = self.save_widget.lineEdit_SavePath.text()
         print("Saving file to {}".format(f))
-        save_datacube(self.datacube,f)
+        save_dataobject(self.datacube,f)
         self.save_widget.close()
 
     def save_directory(self):
