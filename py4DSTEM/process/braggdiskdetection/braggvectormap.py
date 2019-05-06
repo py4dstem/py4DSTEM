d6aaa24f5ff2f7cfd4545070566cca4ee37a69f3
Ben Savitzky <ben.savitzky@gmail.com>
2019-05-06 16:20:52 -0700

    Adds new type of bragg vector map: bragg vector maxima map.  Adds function in braggvectormap.py and demo in associated notebook.
    Former-commit-id: 1ec68cd9228f782d4c3fc68207bf9a5248268a52

--- py4DSTEM/process/braggdiskdetection/braggvectormap.py
+++ py4DSTEM/process/braggdiskdetection/braggvectormap.py
@@ -18,6 +18,7 @@ def get_bragg_vector_map(pointlistarray, Q_Nx, Q_Ny):
     assert 'qx' in pointlistarray.dtype.names, "pointlistarray coords must include 'qx' and 'qy'"
     assert 'qy' in pointlistarray.dtype.names, "pointlistarray coords must include 'qx' and 'qy'"
     assert 'intensity' in pointlistarray.dtype.names, "pointlistarray coords must include 'intensity'"
+
     braggvectormap = np.zeros((Q_Nx,Q_Ny))
     for Rx in range(pointlistarray.shape[0]):
         for Ry in range(pointlistarray.shape[1]):
@@ -27,6 +28,33 @@ def get_bragg_vector_map(pointlistarray, Q_Nx, Q_Ny):
                 qy = pointlist.data['qy'][i]
                 I = pointlist.data['intensity'][i]
                 braggvectormap = add_to_2D_array_from_floats(braggvectormap,qx,qy,I)
+    return braggvectormap
+
+def get_bragg_vector_maxima_map(pointlistarray, Q_Nx, Q_Ny):
+    """
+    Calculates the Bragg vector maxima map from a PointListArray of Bragg peak positions.
+
+    Accepts:
+        pointlistarray      (PointListArray)
+        Q_Nx,Q_Ny           (ints)
+
+    Returns:
+        braggvectormap      (2D ndarray, shape (Q_Nx,Q_Ny))
+    """
+    assert np.all([name in pointlistarray.dtype.names for name in ['qx','qy','intensity']]), "pointlistarray coords must include coordinates: 'qx', 'qy', 'intensity'."
+    assert 'qx' in pointlistarray.dtype.names, "pointlistarray coords must include 'qx' and 'qy'"
+    assert 'qy' in pointlistarray.dtype.names, "pointlistarray coords must include 'qx' and 'qy'"
+    assert 'intensity' in pointlistarray.dtype.names, "pointlistarray coords must include 'intensity'"
+
+    braggvectormap = np.zeros((Q_Nx,Q_Ny))
+    for Rx in range(pointlistarray.shape[0]):
+        for Ry in range(pointlistarray.shape[1]):
+            pointlist = pointlistarray.get_pointlist(Rx,Ry)
+            for i in range(pointlist.length):
+                qx = int(np.round(pointlist.data['qx'][i]))
+                qy = int(np.round(pointlist.data['qy'][i]))
+                I = pointlist.data['intensity'][i]
+                braggvectormap[qx,qy] = max(I,braggvectormap[qx,qy])
     return braggvectormap
 
 
