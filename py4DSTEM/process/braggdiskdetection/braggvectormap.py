f99ed35686872ec210e0bff02da5e042e565d393
Ben Savitzky <ben.savitzky@gmail.com>
2019-05-06 15:25:06 -0700

    Changes deconvolution -> braggvectormapp in all .py files, and makes related edits in strain workflow notebooks.

--- py4DSTEM/process/braggdiskdetection/braggvectormap.py
+++ py4DSTEM/process/braggdiskdetection/braggvectormap.py
@@ -3,22 +3,22 @@
 import numpy as np
 from ..utils import add_to_2D_array_from_floats
 
-def get_deconvolution(pointlistarray, Q_Nx, Q_Ny):
+def get_bragg_vector_map(pointlistarray, Q_Nx, Q_Ny):
     """
-    Calculates the average deconvolution from a PointListArray of Bragg peak positions.
+    Calculates the Bragg vector map from a PointListArray of Bragg peak positions.
 
     Accepts:
         pointlistarray      (PointListArray)
         Q_Nx,Q_Ny           (ints)
 
     Returns:
-        deconvolution       (2D ndarray, shape (Q_Nx,Q_Ny))
+        braggvectormap      (2D ndarray, shape (Q_Nx,Q_Ny))
     """
     #assert np.all([name in pointlistarray.dtype.names for name in ['qx','qy','intensity']), "pointlistarray coords must include coordinates: 'qx', 'qy', 'intensity'."
     #assert 'qx' in pointlistarray.dtype.names, "pointlistarray coords must include 'qx' and 'qy'"
     #assert 'qy' in pointlistarray.dtype.names, "pointlistarray coords must include 'qx' and 'qy'"
     #assert 'intensity' in pointlistarray.dtype.names, "pointlistarray coords must include 'intensity'"
-    deconvolution = np.zeros((Q_Nx,Q_Ny))
+    braggvectormap = np.zeros((Q_Nx,Q_Ny))
     for Rx in range(pointlistarray.shape[0]):
         for Ry in range(pointlistarray.shape[1]):
             pointlist = pointlistarray.get_pointlist(Rx,Ry)
@@ -26,7 +26,7 @@ def get_deconvolution(pointlistarray, Q_Nx, Q_Ny):
                 qx = pointlist.data['qx'][i]
                 qy = pointlist.data['qy'][i]
                 I = pointlist.data['intensity'][i]
-                deconvolution = add_to_2D_array_from_floats(deconvolution,qx,qy,I)
-    return deconvolution
+                braggvectormap = add_to_2D_array_from_floats(braggvectormap,qx,qy,I)
+    return braggvectormap
 
 
