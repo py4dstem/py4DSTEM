c0c1f19516c16a5fd903959acb92f828427fd4e4
Ben Savitzky <ben.savitzky@gmail.com>
2019-05-10 17:09:09 -0700

    Edits to Steven's relativity branch pull request, including editing documentation strings, and merging the new merging the new get_shift_hybrid with the old get_shift function in utils.py.
    Former-commit-id: e40ef8b0fe2f70e470a28481f4b830058f44326e

--- py4DSTEM/process/preprocess/relativity.py
+++ py4DSTEM/process/preprocess/relativity.py
@@ -1,28 +1,40 @@
 # -*- coding: utf-8 -*-
 
-# Reads an IDES Relativity file, determines the locations of the subframes, and splits them into a stack.
+# Reads an IDES Relativity file, determines the locations of the subframes, and splits them into a
+# stack.
 #
-# The IDES Relativity splits the camera area into a matrix of subframes, which gives a higher framerate than
-# the native capability of the camera (1280 fps in 4x4 mode on the NCEM ThemIS). The frames are slightly
-# overlapping and highly distorted. This file provides functions for reading the *.mrc movies containing
-# the frames, locating the subframes and cropping them, measuring the distortion in each subframe, and
-# finally extracting a corrected DataCube from the movie.
+# The IDES Relativity splits the camera area into a matrix of subframes, which gives a higher
+# framerate than the native capability of the camera (1280 fps in 4x4 mode on the NCEM ThemIS).
+# The frames are slightly overlapping and highly distorted. This file provides functions for reading
+# the *.mrc movies containing the frames, locating the subframes and cropping them, measuring the
+# distortion in each subframe, and finally extracting a corrected DataCube from the movie.
 # 
 # Created on 7 May 2019
 # @author: sezeltmann
 
 import numpy as np
-from ..utils import get_shift_hybrid
 import matplotlib.pyplot as plt
 from ipywidgets import FloatProgress
 from time import time
+from ..utils import get_shift
 from ...file.datastructure import DataCube
 
 def slice_subframes(frame, x_cent, y_cent, wx=500, wy=500):
 	"""
-	Slice a movie frame into subframes, given the center points x_cent and y_cent (which are numpy meshgrids)
-	and the subframe width wx, wy
-	Returns a 3D stack of diffraction patterns.
+	Slice a movie frame into subframes.
+
+    x_cent and y_cent are shape (N,M) arrays, where the camera frames have been divided into an NxM
+    grid of subframes.
+
+    Accepts:
+        frame       (array) the original camera frame containing the tiled subframes
+        x_cent      (array) x coords of the centers of the frames
+        y_cent      (array) y coords of the centers of the frames
+        wx          (int) the x width of the subframes
+        wy          (int) the y width of the subframes
+
+	Returns:
+        stack       (ndarray) a 3D stack of diffraction patterns
 	"""
 	nDP = x_cent.shape[0] * x_cent.shape[1]
 	stack = np.zeros((wx,wy,nDP))
@@ -40,16 +52,28 @@ def slice_subframes(frame, x_cent, y_cent, wx=500, wy=500):
 
 def subframe_align(testDP, xcent, ycent, wx=500, wy=500, niter=10, maxshift=80, anchorDP=0, corrPower=0.5, damping=1 ):
 	"""
-	Detrmine the alignment of the subframes in a Relativity frame by gradient descent. Arguments are:
+	Determine the alignment of the subframes in a Relativity frame by gradient descent.
 
-	testDP			(numpy array) a single frame from a Relativity movie, ideally with all subframes similarly illuminated
-	xcent, ycent 	(numpy meshgrids) initial guesses of the centers
-	wx,wy 			(ints) frame x- and y-sizes
-	niter			(int) number of iterations of gradient descent to optimize shifts
-	maxshift		(int) maximum allowable shift. This only triggers an error if the shifts exceed it, it doesn't constrain fitting
-	anchorDP		(int) index of the DP that should be kept static. Other positions are fitted to align subframes with this one.
-	corrPower		(float) Hybrid correlation power. Phase-y correlation works well when you have a beamstop
-	damping			(float) damps the update steps. Set to 1 for no damping (you can probably get away with no damping)
+    Accepts:
+        testDP			(array) a single frame from a Relativity movie, ideally with all subframes
+                        similarly illuminated
+        xcent           (array) initial guess at the x position of the frame of the centers
+        ycent           (array) initial guess at the y position of the frame of the centers
+        wx,wy           (ints) subframe x- and y-sizes
+        niter           (int) number of iterations of gradient descent to optimize shifts
+        maxshift        (int) maximum allowable shift. This only triggers an error if the shifts
+                        exceed it; it doesn't constrain fitting
+        anchorDP		(int) index of the DP that should be kept static. Other positions are fitted
+                        to align subframes with this one.
+        corrPower		(float) hybrid correlation power. Phase-y correlation works well when you
+                        have a beamstop
+        damping			(float) damps the update steps. Set to 1 for no damping (you can probably get
+                        away with no damping)
+
+    Returns
+        bestx           words
+        besty           go
+        err             here
 	"""
 
 	err = np.zeros(niter)
@@ -67,7 +91,7 @@ def subframe_align(testDP, xcent, ycent, wx=500, wy=500, niter=10, maxshift=80, 
 		#compute the error metric
 		shifterr = np.zeros(stack.shape[2])
 		for ind in range(stack.shape[2]):
-			shiftx, shifty = get_shift_hybrid(stack[:,:,anchorDP],stack[:,:,ind],corrPower)
+			shiftx, shifty = get_shift(stack[:,:,anchorDP],stack[:,:,ind],corrPower)
 
 			#correct for the wraparound of the shifts from get_shift
 			if shiftx > int(wx/2): shiftx = shiftx - wx
@@ -117,17 +141,21 @@ def slice_mrc_stack(mrc, scratch, scanshape, optx, opty, startframe=0, wx=500, w
 	"""
 	Slice the *.mrc movie into all of its subframes
 
-	mrc 		(MrcMemmap) memory map into the mrc file (such as opened by py4DSTEM.file.readwrite.read(...,load='relativity'))
-	scratch 	(str) path to a scratch file where a numpy memmap containing the re-sliced stack will be buffered
-		      	NOTE! this will overwrite whatever file is at this path! be careful!
-		      	ALSO NOTE! this file is where the data in the DataCube will actually live!
-		      	Either save the DataCube as a py4DSTEM *.h5 or use separate scratches for different data!
-  	scanshape	(numpy array) 2-element array containing the scan shape (Rx, Ry)
-	optx, opty 	(numpy meshgrids) the optimized centers of the subframes from subframeAlign(...)
-	wx,wy 		(ints) subframe sizes x and y
+    Accepts:
+        mrc         (MrcMemmap) memory map into the mrc file (such as opened by
+                    py4DSTEM.file.readwrite.read(...,load='relativity'))
+        scratch     (str) path to a scratch file where a numpy memmap containing the re-sliced stack
+                    will be buffered
+                    NOTE! this will overwrite whatever file is at this path! be careful!
+                    ALSO NOTE! this file is where the data in the DataCube will actually live!
+                    Either save the DataCube as a py4DSTEM *.h5 or use separate scratches for
+                    different data!
+        scanshape   (numpy array) 2-element array containing the scan shape (Rx, Ry)
+        optx, opty  (numpy meshgrids) the optimized centers of the subframes from subframeAlign(...)
+        wx,wy       (ints) subframe sizes x and y
 
 	Returns:
-	dc 			(DataCube) a py4DSTEM DataCube containing the sliced up stack, in the correct order
+        dc          (DataCube) a py4DSTEM DataCube containing the sliced up stack, in the correct order
 	"""
 
 	nframe = scanshape.prod()//optx.size
