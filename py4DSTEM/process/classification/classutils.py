c06ef4200c8b8bc29d45a827eaf12d3b572f8355
Ben Savitzky <ben.savitzky@gmail.com>
2019-05-23 15:07:05 -0700

    Adds option of a darkreference in get_class_DP function.
    Former-commit-id: e0715d7e5d301f7ceb7f254a3f24810db83015ff

--- py4DSTEM/process/classification/classutils.py
+++ py4DSTEM/process/classification/classutils.py
@@ -4,7 +4,7 @@ import numpy as np
 from ..utils import get_shifted_ar
 from ...file.datastructure import DataCube
 
-def get_class_DP(datacube, class_image, xshifts=None, yshifts=None):
+def get_class_DP(datacube, class_image, xshifts=None, yshifts=None, darkref=None):
     """
     Get the average diffraction pattern for the class described in real space by class_image.
 
@@ -25,18 +25,22 @@ def get_class_DP(datacube, class_image, xshifts=None, yshifts=None):
         assert xshifts.shape == (datacube.R_Nx,datacube.R_Ny)
     if yshifts is not None:
         assert yshifts.shape == (datacube.R_Nx,datacube.R_Ny)
+    if darkref is not None:
+        assert darkref.shape == (datacube.Q_Nx,datacube.Q_Ny)
 
     N_DPs = 0
     class_DP = np.zeros((datacube.Q_Nx,datacube.Q_Ny))
     for Rx in range(datacube.R_Nx):
         for Ry in range(datacube.R_Ny):
             if class_image[Rx,Ry] != 0:
-                curr_DP = class_image[Rx,Ry]*datacube.data4D[Rx,Ry]
+                curr_DP = class_image[Rx,Ry]*datacube.data4D[Rx,Ry,:,:]
                 if xshifts is not None and yshifts is not None:
                     xshift = xshifts[Rx,Ry]
                     yshift = yshifts[Rx,Ry]
                     curr_DP = get_shifted_ar(curr_DP,-xshift,-yshift)
                 class_DP += curr_DP
+                if darkref is not None:
+                    class_DP -= darkref*class_image[Rx,Ry]
                 N_DPs += 1
     class_DP /= N_DPs
     return class_DP
