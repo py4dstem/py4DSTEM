777d8ef6225078b51fe34a30c96c8953599ff637
SE Zeltmann <steven.zeltmann@lbl.gov>
2019-11-04 14:28:23 -0800

    GUI fix for macOS Catalina and add ND TQDM wrapper
    Former-commit-id: 16a1303672b8db06e9dbf2f26615d4f20cff2443

---
+++ py4DSTEM/process/utils/tqdmnd.py
@@ -0,0 +1,34 @@
+"""
+A wrapper for tqdm (https://github.com/tqdm/tqdm) that supports 
+multidimensional iterators. Iterates over the Cartesian product
+of the input iterators. Integers in the input are automagically
+conveted to range(i) for convenience.
+Examples:
+for x,y in tqdmnd(range(20),range(10)):
+    sleep(0.1)
+    
+for x,y in tqdmnd(20,10):
+    sleep(0.1)
+"""
+
+from tqdm import tqdm
+from itertools import product
+
+from operator import mul
+
+from collections.abc import Iterator
+
+class nditer(Iterator):
+    def __init__(self,*args):
+        self._it = product(*args)
+        self._l = reduce(mul,[a.__len__() for a in args])
+    def __iter__(self):
+        return self._it.__iter__()
+    def __next__(self):
+        return self._it.__next__()
+    def __len__(self):
+        return self._l
+      
+def tqdmnd(*args):
+    r = [range(i) if isinstance(i,int) else i for i in args]
+    return tqdm(nditer(*r))
