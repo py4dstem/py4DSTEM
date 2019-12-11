d81e71d29d19330f1d38ed4e52d247af3e361dc3
Tom Pekin <tcpekin@gmail.com>
2019-12-11 11:53:00 +0100

    small changes working towards whole pattern fitting functinoality on stacks
    Former-commit-id: 57001e277763caf671182a57248a0d5d1edee9a4

--- py4DSTEM/process/rdf/amorph.py
+++ py4DSTEM/process/rdf/amorph.py
@@ -10,38 +10,6 @@ from tqdm import tqdm
 matplotlib.rcParams['figure.dpi'] = 100
 plt.ion()
 
-# %%
-# make ellipse
-"""
-Cost function for two sided gaussian.
-xx      = x coords
-yy      = y coords
-coef[0] = N (linear constant)
-coef[1] = I_BG
-coef[2] = SD_BG
-coef[3] = I_ring
-coef[4] = SD_1
-coef[5] = SD_2
-coef[6] = X_center
-coef[7] = Y_center
-coef[8] = B
-coef[9] = C
-coef[10] = R
-"""
-yy,xx = np.meshgrid(np.arange(256), np.arange(256))
-coef = [10,1,1,100,10,10,128,125,0.6,1,85]
-r = np.sqrt( (xx - coef[6])**2 + coef[8]*(xx - coef[6])*(yy - coef[7]) + coef[9]*(yy - coef[7])**2)
-ring = (coef[0] + coef[1] * np.exp( (-1/ (2*coef[2]**2)) * r**2) + coef[3] *np.exp( (-1/ (2*coef[4]**2)) * (coef[10] - r)**2) * np.heaviside((coef[10] - r),.5) + coef[3] * np.exp( (-1/ (2*coef[5]**2)) * (coef[10] - r)**2) * np.heaviside((r - coef[10]),.5) )
-plt.imshow(ring)
-
-# %%
-data = polar_elliptical_transform(calibration_image=ring, r_range=200)
-# params = data.fit_params(10,return_ans=True)
-# plt.imshow(data.polar_ar)
-data.fit_params_two_sided_gaussian()
-data.compare_coefs_two_sided_gaussian()
-# %%
-
 def fit_stack(datacube, init_coefs):
     """
     This will fit an ellipse using the polar elliptical transform code to all the diffraction patterns. It will take in a datacube and return a coefficient array which can then be used to map strain, fit the centers, etc.
