78d3aa0ce0bad04990e9e763925d404a96efc942
Ben Savitzky <ben.savitzky@gmail.com>
2019-12-18 10:49:17 -0800

    Renames coordTrans.py -> ellipticalCoords.py. Adds fitting function and braggpeak correction function.

--- py4DSTEM/process/utils/ellipticalCoords.py
+++ py4DSTEM/process/utils/ellipticalCoords.py
@@ -1,4 +1,12 @@
-# Performs the coordinate transformation:
+# Contains methods relating to polar-elliptical calculations.
+#
+# This includes
+#   - measuring / fitting elliptical distortions
+#   - transforming data from cartesian to polar-elliptical coordinates
+#   - converting between ellipse representations
+#   - correcting Bragg peak positions for elliptical distortions
+#
+# We define the transformation from cartesian to polar-elliptical coordinates by:
 #
 #       x = x0 + A*r*cos(theta)*cos(phi) + B*r*sin(theta)*sin(phi)
 #       y = y0 + B*r*sin(theta)*sin(phi) - A*r*cos(theta)*cos(phi)
@@ -6,6 +14,7 @@
 # All angular quantities are in radians.
 
 import numpy as np
+from scipy.optimize import leastsq
 
 def cartesianDataAr_to_polarEllipticalDataAr(cartesianData, params,
                                              dr=1, dtheta=np.radians(2), r_range=512,
@@ -117,7 +126,7 @@ def convert_ellipse_params(A0,B0,C0):
                                 A0*x^2 + B0*x*y + C0*y^2 = 1
 
     Returns:
-        a,b         (floats) the semi-axis lengths
+        A,B         (floats) the semi-axis lengths
         phi         (float) the tilt of the ellipse semi-axes, in radians
     """
     if A0==C0:
@@ -126,44 +135,114 @@ def convert_ellipse_params(A0,B0,C0):
     else:
         x = (A0-C0)*np.sqrt(1+(B0/(A0-C0))**2)
         phi = 0.5*np.arctan(B0/(A0-C0))
-    a = np.sqrt(2/(A0+C0+x))
-    b = np.sqrt(2/(A0+C0-x))
-    return a,b,phi
+    A = np.sqrt(2/(A0+C0+x))
+    B = np.sqrt(2/(A0+C0-x))
+    return A,B,phi
 
-
-
-
-
-
-
-
-def coordsA_to_coordsB(coordsA, paramsB, samplingParams):
+def fit_ellipse_inside_annulus(ar, x0, y0, r_inner, r_outer, datamask=None, output=None):
     """
-    Converts a single pointsin coordinate system A into points in coordinate system B.
+    Fits an ellipse to the data in an annular region of array ar, centered at (x0,y0) and with inner
+    and outer radii of r_inner, r_outer, respectively.  Returns the ellipse parameters.
 
     Accepts:
-        coordsA         a point in the initial coordinate system, e.g. (qx,qy)
-        paramsB         the parameters defining the final coord systsm, e.g. (x0,y0,A,B,phi)
-        samplingParams  the binning / sampling parameters to discretize the final coords
+        ar          (array) array containing the data to fit
+        x0,y0       (floats) the center of the annular fitting region
+        r_inner     (float) inner radius of the fit region
+        r_outer     (float) outer radius of the fit region
+        datamask    (ar-shaped array of bools) ignore datapoints where mask==False
+        output      (str or None) controls the parametrization of the output
 
-    Returns:
-        coordsB         equivalent point to coordsA in the final coord system
+    Returns
+        params      (5-tuple) the ellipse parameters. Parametrization depends on the flag 'output'.
+                    if output != ('canonical' or 'both'):
+                        returns (x0,y0,A,B,phi), using
+                            x = x0 + A*r*cos(theta)*cos(phi) + B*r*sin(theta)*sin(phi)
+                            y = y0 + B*r*sin(theta)*sin(phi) - A*r*cos(theta)*cos(phi)
+                    if output == 'canonical':
+                        returns (x0,y0,A0,B0,C0), using
+                            A0*(x-x0)^2 + B0*(x-x0)*(y-y0) + C0*(y-y0)^2 = 1
+                    if output == 'both':
+                        returns both of the parametrizations above, in the order written.
     """
-    pass
+    # Get the datapoints to fit
+    yy,xx = np.meshgrid(np.arange(ar.shape[1]),np.arange(ar.shape[0]))
+    rr = np.hypot(xx-x0,yy-y0)
+    mask = (rr>r_inner) * (rr<=r_outer)
+    if datamask is not None:
+        mask *= datamask
+    xs,ys = np.nonzero(mask)
+    vals = ar[mask]
 
-def dataArA_to_dataArB(dataArA, paramsB, samplingParams):
+    # Get initial parameters guess
+    p0_0 = x0
+    p0_1 = y0
+    p0_2 = (2/(r_inner+r_outer))**2
+    p0_3 = 0
+    p0_4 = (2/(r_inner+r_outer))**2
+    p0 = (p0_0,p0_1,p0_2,p0_3,p0_4)
+
+    # Fit
+    p2 = leastsq(ellipse_err, p0, args=(xs,ys,vals))[0]
+
+    # Convert between parametrizations
+    x,y,A0,B0,C0 = p2
+    A,B,phi = convert_ellipse_params(A0,B0,C0)
+    p1 = (x,y,A,B,phi)
+
+    if output=='canonical':
+        return p2
+    elif output=='both':
+        return p1,p2
+    else:
+        return p1
+
+def ellipse_err(p, x, y, val):
     """
-    Converts a data array in coordinate system A into a data array in coordinate system B.
+    Returns the error associated with point (x,y), which has value val, with respect to the ellipse
+    given by parameters p.  p is a 5-tuple of the form (x,y,A0,B0,C0) specifying the ellipse
+        A0*(x-x0)^2 + B0*(x-x0)*(y-y0) + C0*(y-y0)^2 = 1
+    """
+    x,y = x-p[0],y-p[1]
+    return (p[2]*x**2 + p[3]*x*y + p[4]*y**2 - 1)*val
+
+def correct_braggpeak_elliptical_distortions(braggpeaks, p):
+    """
+    Given some elliptical distortions with ellipse parameters p and some measured PointListArray
+    of Bragg peak positions braggpeaks, returns the elliptically corrected Bragg peaks.
 
     Accepts:
-        dataArA         data array in the initial coordinate system, e.g. a DP
-        paramsB         the parameters defining the final coord systsm, e.g. (x0,y0,A,B,phi)
-        samplingParams  the binning / sampling parameters to discretize the final coords
+        braggpeaks              (PointListArray) the Bragg peaks
+        p                       (5-tuple) the parameters (x0,y0,A,B,phi) of an ellipse describing
+                                the elliptical distortions in the data
 
     Returns:
-        dataArB         data array in the final coord system
+        braggpeaks_corrected    (PointListArray) the corrected Bragg peaks
     """
-    pass
+    # Get the transformation matrix
+    x0,y0,A,B,phi = p
+    s = min(A,B)/max(A,B)   # scale the larger semiaxis down to the size of the smaller semiaxis
+    phi += np.pi/2.*(np.argmin([a,b])==0)   # if A was smaller, rotated phi by pi/2 to make A->B
+    sinp,cosp = np.sin(phi),np.cos(phi)
+    T = np.squeeze(np.array([[sinp**2 + s*cosp**2, sinp*cosp*(s-1)],
+                             [sinp*cosp*(s-1), s*sinp**2 + cosp**2]])
+
+    # Correct distortions
+    braggpeaks_corrected = braggpeaks.copy(name=braggpeaks.name+"_ellipsecorrected")
+    for Rx in range(braggpeaks_corrected.shape[0]):
+        for Ry in range(braggpeaks_corrected.shape[1]):
+            pointlist = braggpeaks_corrected.get_pointlist(Rx,Ry)
+            x,y = pointlist.data['qx']-x0, pointlist.data['qy']-y0
+            xyar_i = np.vstack([x,y])
+            xyar_f = np.matmul(T,xyar_i)
+            pointlist.data['qx'] = xyar_f[0,:]+x0
+            pointlist.data['qy'] = xyar_f[1,:]+y0
+    return braggpeaks_corrected
+
+
+
+
+
+
 
 
 
