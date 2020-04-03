ade71cd5a76fa29b97263850381a1c50cfd3c08e
SE Zeltmann <steven.zeltmann@lbl.gov>
2020-04-02 20:12:07 -0700

    jacobians added but very slow
    Former-commit-id: f18b1a1bd1238268d8eff37ca59a928cbe5742d4

--- py4DSTEM/process/wholepatternfit/wp_models.py
+++ py4DSTEM/process/wholepatternfit/wp_models.py
@@ -2,6 +2,8 @@ from inspect import signature
 
 import numpy as np
 
+from pdb import set_trace
+
 
 class WPFModelPrototype:
     """
@@ -75,18 +77,12 @@ class GaussianBackground(WPFModelPrototype):
             params["x center"] = x0
             params["y center"] = y0
             self.func = self.local_center_func
-            self.jacobian = self.global_center_jacobian
+            self.jacobian = self.local_center_jacobian
 
         super().__init__(name, params)
 
     def global_center_func(self, DP: np.ndarray, sigma, level, **kwargs) -> None:
-        DP += level * np.exp(
-            (
-                (kwargs["xArray"] - kwargs["global_x0"]) ** 2
-                + (kwargs["yArray"] - kwargs["global_y0"]) ** 2
-            )
-            / (-2 * sigma ** 2)
-        )
+        DP += level * np.exp(kwargs["global_r"] ** 2 / (-2 * sigma ** 2))
 
     def global_center_jacobian(
         self, J: np.ndarray, sigma, level, offset: int, **kwargs
@@ -94,68 +90,35 @@ class GaussianBackground(WPFModelPrototype):
 
         # dF/d(global_x0)
         J[:, 0] += (
-            -1
-            * level
+            level
             * (kwargs["xArray"] - kwargs["global_x0"])
-            * np.exp(
-                (
-                    (kwargs["xArray"] - kwargs["global_x0"]) ** 2
-                    + (kwargs["yArray"] - kwargs["global_y0"]) ** 2
-                )
-                / (-2 * sigma ** 2)
-            )
+            * np.exp(kwargs["global_r"] ** 2 / (-2 * sigma ** 2))
             / sigma ** 2
         ).ravel()
 
         # dF/d(global_y0)
         J[:, 1] += (
-            -1
-            * level
+            level
             * (kwargs["yArray"] - kwargs["global_y0"])
-            * np.exp(
-                (
-                    (kwargs["xArray"] - kwargs["global_x0"]) ** 2
-                    + (kwargs["yArray"] - kwargs["global_y0"]) ** 2
-                )
-                / (-2 * sigma ** 2)
-            )
+            * np.exp(kwargs["global_r"] ** 2 / (-2 * sigma ** 2))
             / sigma ** 2
         ).ravel()
 
         # dF/s(sigma)
         J[:, offset] = (
-            -1
-            * level
-            * (
-                (kwargs["xArray"] - kwargs["global_x0"]) ** 2
-                + (kwargs["yArray"] - kwargs["global_y0"]) ** 2
-            )
-            * np.exp(
-                (
-                    (kwargs["xArray"] - kwargs["global_x0"]) ** 2
-                    + (kwargs["yArray"] - kwargs["global_y0"]) ** 2
-                )
-                / (-2 * sigma ** 2)
-            )
+            level
+            * kwargs["global_r"] ** 2
+            * np.exp(kwargs["global_r"] ** 2 / (-2 * sigma ** 2))
             / sigma ** 3
         ).ravel()
 
         # dF/d(level)
-        J[:, offset + 1] = np.exp(
-            (
-                (kwargs["xArray"] - kwargs["global_x0"]) ** 2
-                + (kwargs["yArray"] - kwargs["global_y0"]) ** 2
-            )
-            / (-2 * sigma ** 2)
-        )
+        J[:, offset + 1] = np.exp(kwargs["global_r"] ** 2 / (-2 * sigma ** 2)).ravel()
 
     def local_center_func(self, DP: np.ndarray, sigma, level, x0, y0, **kwargs) -> None:
-        DP += level * np.exp(
-            ((kwargs["xArray"] - x0) ** 2 + (kwargs["yArray"] - y0) ** 2)
-            / (-2 * sigma ** 2)
-        )
+        DP += level * np.exp(kwargs["global_r"] ** 2 / (-2 * sigma ** 2))
 
-    def local_center_jacobianjacobian(
+    def local_center_jacobian(
         self, J: np.ndarray, sigma, level, x0, y0, offset: int, **kwargs
     ) -> None:
 
@@ -175,7 +138,7 @@ class GaussianBackground(WPFModelPrototype):
         J[:, offset + 1] = np.exp(
             ((kwargs["xArray"] - x0) ** 2 + (kwargs["yArray"] - y0) ** 2)
             / (-2 * sigma ** 2)
-        )
+        ).ravel()
 
         # dF/d(x0)
         J[:, offset + 2] = (
@@ -228,6 +191,7 @@ class SyntheticDiskLattice(WPFModelPrototype):
 
         if global_center:
             self.func = self.global_center_func
+            self.jacobian = self.global_center_jacobian
         else:
             params["x center"] = x0
             params["y center"] = y0
@@ -280,16 +244,131 @@ class SyntheticDiskLattice(WPFModelPrototype):
         for i, (u, v) in enumerate(zip(self.u_inds, self.v_inds)):
             x = x0 + (u * ux) + (v * vx)
             y = y0 + (u * uy) + (v * vy)
-            DP += args[i + 6] / (
-                1.0
-                + np.exp(
-                    4
-                    * (
-                        np.sqrt(
-                            (kwargs["xArray"] - x) ** 2 + (kwargs["yArray"] - y) ** 2
+            if (x > 0) & (x < kwargs["Q_Nx"]) & (y > 0) & (y < kwargs["Q_Nx"]):
+                DP += args[i + 6] / (
+                    1.0
+                    + np.exp(
+                        4
+                        * (
+                            np.sqrt(
+                                (kwargs["xArray"] - x) ** 2
+                                + (kwargs["yArray"] - y) ** 2
+                            )
+                            - disk_radius
                         )
-                        - disk_radius
+                        / disk_width
                     )
-                    / disk_width
                 )
-            )
+
+    def global_center_jacobian(
+        self, J: np.ndarray, *args, offset: int, **kwargs
+    ) -> None:
+
+        x0 = kwargs["global_x0"]
+        y0 = kwargs["global_y0"]
+        r = np.maximum(5e-1, kwargs["global_r"])
+        ux = args[0]
+        uy = args[1]
+        vx = args[2]
+        vy = args[3]
+
+        if self.refine_radius & self.refine_width:
+            disk_radius = args[-2]
+            radius_ind = -2
+        elif self.refine_radius:
+            disk_radius = args[-1]
+            radius_ind = -1
+        else:
+            disk_radius = self.disk_radius
+        disk_width = args[-1] if self.refine_width else self.disk_width
+
+        for i, (u, v) in enumerate(zip(self.u_inds, self.v_inds)):
+            x = x0 + (u * ux) + (v * vx)
+            y = y0 + (u * uy) + (v * vy)
+
+            if (x > 0) & (x < kwargs["Q_Nx"]) & (y > 0) & (y < kwargs["Q_Nx"]):
+                r_disk = np.maximum(
+                    5e-1,
+                    np.sqrt((kwargs["xArray"] - x) ** 2 + (kwargs["yArray"] - y) ** 2),
+                )
+
+                # dF/d(global_x0)
+                dx = (
+                    4
+                    * args[i + 4]
+                    * (kwargs["xArray"] - x)
+                    * np.exp(4 * (r_disk - disk_radius) / disk_width)
+                    / (
+                        (1.0 + np.exp(4 * (r_disk - disk_radius) / disk_width)) ** 2
+                        * disk_width
+                        * r
+                    )
+                ).ravel()
+
+                # dF/d(global_y0)
+                dy = (
+                    4
+                    * args[i + 4]
+                    * (kwargs["yArray"] - y)
+                    * np.exp(4 * (r_disk - disk_radius) / disk_width)
+                    / (
+                        (1.0 + np.exp(4 * (r_disk - disk_radius) / disk_width)) ** 2
+                        * disk_width
+                        * r
+                    )
+                ).ravel()
+
+                # because... reasons?, sometimes we get NaN
+                # very far from the disk center. let's zero those:
+                dx[np.isnan(dx)] = 0.0
+                dy[np.isnan(dy)] = 0.0
+
+                # insert global positional derivatives
+                J[:, 0] += dx
+                J[:, 1] += dy
+
+                # insert lattice vector derivatives
+                J[:, offset] += u * dx
+                J[:, offset + 1] += u * dy
+                J[:, offset + 2] += v * dx
+                J[:, offset + 3] += v * dy
+
+                # insert intensity derivative
+                dI = (
+                    1.0
+                    / (1.0 + np.exp(4 * (r_disk - disk_radius) / disk_width))
+                ).ravel()
+                dI[np.isnan(dI)] = 0.0
+                J[:, offset + i + 4] = dI
+
+                # insert disk radius derivative
+                if self.refine_radius:
+                    dR = (
+                        4.0
+                        * args[i + 4]
+                        * np.exp(4 * (r_disk - disk_radius) / disk_width)
+                        / (
+                            disk_width
+                            * (1.0 + np.exp(4 * (r_disk - disk_radius) / disk_width))
+                            ** 2
+                        )
+                    ).ravel()
+                    dR[np.isnan(dR)] = 0.0
+                    J[:, offset + len(args) + radius_ind] += dR
+
+                if self.refine_width:
+                    dW = (
+                        4.0
+                        * args[i + 4]
+                        * np.exp(4 * (r_disk - disk_radius) / disk_width)
+                        * (r_disk - disk_radius)
+                        / (
+                            disk_width ** 2
+                            * (1.0 + np.exp(4 * (r_disk - disk_radius) / disk_width))
+                            ** 2
+                        )
+                    ).ravel()
+                    dW[np.isnan(dW)] = 0.0
+                    J[:, offset + len(args) - 1] += dW
+
+                # set_trace()
