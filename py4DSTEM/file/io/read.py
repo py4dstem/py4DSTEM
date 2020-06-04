bc0176ae3ef808d7fccb5618fe0cb9536bd540b6
bsavitzky <ben.savitzky@gmail.com>
2020-06-04 09:43:41 -0700

    gatan_bin -> gatan_K2_bin
    Former-commit-id: bffab0f0f785542e2ddc66cdd868489821303319

--- py4DSTEM/file/io/read.py
+++ py4DSTEM/file/io/read.py
@@ -3,7 +3,7 @@
 import pathlib
 from os.path import splitext
 from .native import read_py4DSTEM
-from .nonnative import read_dm, read_empad, read_mrc_relativity, read_gatan_bin, read_kitware_counted
+from .nonnative import read_dm, read_empad, read_mrc_relativity, read_gatan_K2_bin, read_kitware_counted
 
 def read(fp, mem="RAM", binfactor=1, ft=None, **kwargs):
     """
@@ -27,7 +27,7 @@ def read(fp, mem="RAM", binfactor=1, ft=None, **kwargs):
                                 Default is 1.
         ft          str         (opt) Force py4DSTEM to attempt to read the file as a specified filetype, rather
                                 than trying to determine this automatically. Must be None or a str from 'dm',
-                                'empad', 'mrc_relativity', 'gatan_bin', 'kitware_counted'.  Default is None.
+                                'empad', 'mrc_relativity', 'gatan_K2_bin', 'kitware_counted'.  Default is None.
         **kwargs                (opt) When reading the native h5 file format, additional keyword arguments are
                                 used to indicate loading behavior in the case where the source file contains
                                 multiple data objects.
@@ -47,7 +47,7 @@ def read(fp, mem="RAM", binfactor=1, ft=None, **kwargs):
     assert(isinstance(fp,(str,pathlib.Path))), "Error: filepath fp must be a string or pathlib.Path"
     assert(mem in ['RAM','MEMMAP']), 'Error: argument mem must be either "RAM" or "MEMMAP"'
     assert(isinstance(binfactor,int)), "Error: argument binfactor must be an integer"
-    assert(ft in [None,'dm','empad','mrc_relativity','gatan_bin','kitware_counted']), "Error: ft argument not recognized"
+    assert(ft in [None,'dm','empad','mrc_relativity','gatan_K2_bin','kitware_counted']), "Error: ft argument not recognized"
 
     if ft is None:
         ft = parse_filetype(fp)
@@ -60,8 +60,8 @@ def read(fp, mem="RAM", binfactor=1, ft=None, **kwargs):
         data,md = read_empad(fp, mem, binfactor, **kwargs)
     elif ft == 'mrc_relativity':
         data,md = read_mrc_relativity(fp, mem, binfactor, **kwargs)
-    elif ft == "gatan_bin":
-        data,md = read_gatan_bin(fp, mem, binfactor, **kwargs)
+    elif ft == "gatan_K2_bin":
+        data,md = read_gatan_K2_bin(fp, mem, binfactor, **kwargs)
     elif ft == "kitware_counted":
         data,md = read_kitware_counted(fp, mem, binfactor, **kwargs)
     else:
@@ -84,9 +84,9 @@ def parse_filetype(fp):
     elif fext in ['mrc']:
         # TK TODO
         return 'mrc_relativity'
-    elif fext in ['gatan_bin']:
+    elif fext in ['gatan_K2_bin']:
         # TK TODO
-        return 'gatan_bin'
+        return 'gatan_K2_bin'
     elif fext in ['kitware_counted']:
         # TK TODO
         return 'kitware_counted'
