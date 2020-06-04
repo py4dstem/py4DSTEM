784238de45b174aaec53658b4cbfbee787bbacab
bsavitzky <ben.savitzky@gmail.com>
2020-06-04 09:20:46 -0700

    Adds metadata.py with empty class.
    Former-commit-id: dc03cc05b17dcb052cad721b508baf7626f3215f

---
+++ py4DSTEM/file/datastructure/metadata.py
@@ -0,0 +1,17 @@
+# Defines the MetaData class.
+
+import numpy as np
+import h5py
+
+class Metadata(object):
+
+    def __init__(self):
+        """
+        Instantiate a Metadata object.
+
+        Accepts:
+            things      thingy      stuff
+        """
+        return
+
+
