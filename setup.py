2087e38781587659c5c82b062daef606974b9e17
bsavitzky <ben.savitzky@gmail.com>
2020-06-12 13:13:02 -0700

    Versions to 0.9.3
    Former-commit-id: bfe166c577236d0c45fe97266f915fb62385b1d2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md","r") as f:
 
 setup(
     name='py4DSTEM',
-    version='0.9.2',
+    version='0.9.3',
     packages=find_packages(),
     description='An open source python package for processing and analysis of 4D STEM data.',
     long_description=long_description,
@@ -17,8 +17,8 @@ setup(
     keywords="STEM 4DSTEM",
     python_requires='>=3.6',
     install_requires=[
-        'numpy >= 1.15.0',
-        'scipy >= 1.1.0',
+        'numpy >= 1.15',
+        'scipy >= 1.1',
         'h5py >= 2.10.0',
         'ncempy >= 1.6.0',
         'pymatgen',
