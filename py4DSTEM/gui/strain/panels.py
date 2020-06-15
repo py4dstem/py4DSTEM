f25297bab6b9c047d4d8b652208b7508c6538e97
bsavitzky <ben.savitzky@gmail.com>
2020-06-14 23:53:48 -0700

    Bugfix of bugfix
    Former-commit-id: 534876e1a36a4665406ec32b9be561907f316ddf

--- py4DSTEM/gui/strain/panels.py
+++ py4DSTEM/gui/strain/panels.py
@@ -16,7 +16,7 @@ from ...file.datastructure import PointList
 from .cmaptopg import cmapToColormap
 from matplotlib.cm import get_cmap
 from ...process.latticevectors import get_strain_from_reference_region, fit_lattice_vectors_all_DPs
-from ...fil.io import read
+from ...file.io import read
 from ...file.io.native import save, append, is_py4DSTEM_file
 from ...file.datastructure import DiffractionSlice, RealSlice
 from .ImageViewMasked import ImageViewAlpha
