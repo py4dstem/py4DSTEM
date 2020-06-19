1b89ec7a5c044da4393e0a877f2bc8c46f0345c1
bsavitzky <ben.savitzky@gmail.com>
2020-06-19 15:56:57 -0700

    Adds get_probe_2D fn
    Former-commit-id: 5e06cf8f0aaf41fc9285ba6d03a118e3e1af5153

--- py4DSTEM/process/diskdetection/probe.py
+++ py4DSTEM/process/diskdetection/probe.py
@@ -134,6 +134,34 @@ def get_probe_from_vacuum_3Dstack(data, mask_threshold=0.2,
     return probe*mask
 
 
+def get_probe_from_vacuum_2Dimage(data, mask_threshold=0.2,
+                                        mask_expansion=12,
+                                        mask_opening=3):
+    """
+    A single image of the probe over vacuum is processed by zeroing values outside the
+    central disk, using a binary mask determined by the optional parameters mask_threshold,
+    mask_expansion, and mask_opening.  An initial binary mask is created
+    using a threshold of less than mask_threshold times the maximal probe value. A morphological
+    opening of mask_opening pixels is performed to eliminate stray pixels (e.g. from x-rays),
+    followed by a dilation of mask_expansion pixels to ensure the entire probe is captured.
+
+    Accepts:
+        data            (array) a 2D array of the vacuum diffraction pattern, shape (Q_Nx,Q_Ny)
+        mask_threshold  (float) threshold determining mask which zeros values outside of probe
+        mask_expansion  (int) number of pixels by which the zeroing mask is expanded to capture
+                        the full probe
+        mask_opening    (int) size of binary opening used to eliminate stray bright pixels
+
+    Returns:
+        probe           (array of shape (Q_Nx,Q_Ny)) the average probe
+    """
+    mask = data > np.max(data)*mask_threshold
+    mask = binary_opening(mask, iterations=mask_opening)
+    mask = binary_dilation(mask, iterations=mask_expansion)
+
+    return data*mask
+
+
 def get_probe_synthetic(radius, width, Q_Nx, Q_Ny):
     """
     Makes a synthetic probe, with the functional form of a disk blurred by a sigmoid (a logistic
