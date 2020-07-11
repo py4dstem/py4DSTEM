0b04fe7f99622453d823d128912313fa17944a76
bsavitzky <ben.savitzky@gmail.com>
2020-07-10 21:08:33 -0700

    Enables reading legacy v0.6.0 files
    Former-commit-id: 7de3746dea7c8d172226e234d2dd0dce959660d9

--- py4DSTEM/file/io/native/legacy/read_v060.py
+++ py4DSTEM/file/io/native/legacy/read_v060.py
@@ -3,14 +3,14 @@
 import h5py
 import numpy as np
 from os.path import splitext
-from .read_utils import is_py4DSTEM_file, get_py4DSTEM_topgroups, get_py4DSTEM_version
-from .read_utils import version_is_geq, get_py4DSTEM_dataobject_info
-from ...datastructure import DataCube, CountedDataCube, DiffractionSlice, RealSlice
-from ...datastructure import PointList, PointListArray
-from ....process.utils import tqdmnd
+from ..read_utils import is_py4DSTEM_file, get_py4DSTEM_topgroups, get_py4DSTEM_version, version_is_geq
+from ....datastructure import DataCube, CountedDataCube, DiffractionSlice, RealSlice
+from ....datastructure import PointList, PointListArray
+from .....process.utils import tqdmnd
+from .read_utils_v060 import get_py4DSTEM_dataobject_info
 
 
-def read_py4DSTEM_legacy(fp, **kwargs):
+def read_v060(fp, **kwargs):
     """
     File reader for files written by py4DSTEM v0.8.0.  Precise behavior is detemined by which
     arguments are passed -- see below.
@@ -160,12 +160,11 @@ def get_data_from_int(fp,tg,data_id,mem='RAM',binfactor=1,bindtype=None):
     assert(isinstance(data_id,int))
     with h5py.File(fp,'r') as f:
         grp_dc = f[tg+'/data/datacubes/']
-        grp_cdc = f[tg+'/data/counted_datacubes/']
         grp_ds = f[tg+'/data/diffractionslices/']
         grp_rs = f[tg+'/data/realslices/']
         grp_pl = f[tg+'/data/pointlists/']
-        grp_pla = f[tg+'/data/pointlists/']
-        grps = [grp_dc,grp_cdc,grp_ds,grp_rs,grp_pl,grp_pla]
+        grp_pla = f[tg+'/data/pointlistarrays/']
+        grps = [grp_dc,grp_ds,grp_rs,grp_pl,grp_pla]
 
         Ns = np.cumsum([len(grp.keys()) for grp in grps])
         i = np.nonzero(data_id<Ns)[0][0]
@@ -184,20 +183,18 @@ def get_data_from_str(fp,tg,data_id,mem='RAM',binfactor=1,bindtype=None):
     assert(isinstance(data_id,str))
     with h5py.File(fp,'r') as f:
         grp_dc = f[tg+'/data/datacubes/']
-        grp_cdc = f[tg+'/data/counted_datacubes/']
         grp_ds = f[tg+'/data/diffractionslices/']
         grp_rs = f[tg+'/data/realslices/']
         grp_pl = f[tg+'/data/pointlists/']
-        grp_pla = f[tg+'/data/pointlists/']
-        grps = [grp_dc,grp_cdc,grp_ds,grp_rs,grp_pl,grp_pla]
+        grp_pla = f[tg+'/data/pointlistarrays/']
+        grps = [grp_dc,grp_ds,grp_rs,grp_pl,grp_pla]
 
         l_dc = list(grp_dc.keys())
-        l_cdc = list(grp_cdc.keys())
         l_ds = list(grp_ds.keys())
         l_rs = list(grp_rs.keys())
         l_pl = list(grp_pl.keys())
         l_pla = list(grp_pla.keys())
-        names = l_dc+l_cdc+l_ds+l_rs+l_pl+l_pla
+        names = l_dc+l_ds+l_rs+l_pl+l_pla
 
         inds = [i for i,name in enumerate(names) if name==data_id]
         assert(len(inds)!=0), "Error: no data named {} found.".format(data_id)
@@ -322,12 +319,22 @@ def get_pointlistarray_from_grp(g):
         and returns a PointListArray.
     """
     name = g.name.split('/')[-1]
-    dset = g['data']
-    shape = g['data'].shape
-    coordinates = g['data'][0,0].dtype
+    l = list(g)
+    ar = np.array([l[i].split('_') for i in range(len(l))]).astype(int)
+    shape = (np.max(ar[:,0])+1,np.max(ar[:,1])+1)
+    coord_names = list(g['0_0'])
+    N = len(coord_names)
+    coord_types = [type(np.array(g['0_0/'+coord_names[i]+'/data'])[0]) for i in range(N)]
+    coordinates = [(coord_names[i],coord_types[i]) for i in range(N)]
     pla = PointListArray(coordinates=coordinates,shape=shape,name=name)
-    for (i,j) in tqdmnd(shape[0],shape[1],desc="Reading PointListArray",unit="PointList"):
-        pla.get_pointlist(i,j).add_dataarray(dset[i,j])
+    for (i,j) in tqdmnd(range(shape[0]),range(shape[1]),desc="Reading PointListArray",unit="PointList"):
+        g_pl = g[str(i)+'_'+str(j)]
+        L = len(np.array(g_pl[coordinates[0][0]+'/data']))
+        data = np.zeros(L,dtype=coordinates)
+        for i in range(N):
+            coord = coordinates[i][0]
+            data[coord] = np.array(g_pl[coord+'/data'])
+        pla.get_pointlist(i,j).add_dataarray(data)
     return pla
 
 
