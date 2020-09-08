08e6bbc3d007b81150f5b4fb10adf9801264573a
bsavitzky <ben.savitzky@gmail.com>
2020-09-08 15:04:11 -0700

    updates nbpublish.sh
    Former-commit-id: 829b85db49ddc0d437ec171a85ff22cd0d1d2854

--- sample_code/nbpublish.sh
+++ sample_code/nbpublish.sh
@@ -7,6 +7,5 @@ if [ "$HTML" = "html" ]
 then
     jupyter nbconvert $FILENAME --to html --output-dir="html_from_jupyter"
 fi
-jupyter nbconvert $FILENAME --to python --output-dir="python_from_jupyter"
-jupyter nbconvert $FILENAME --to notebook --ClearOutputPreprocessor.enabled=True --output-dir="jupyter"
+jupyter nbconvert $FILENAME --to notebook --ClearOutputPreprocessor.enabled=True --output $FILENAME
 
