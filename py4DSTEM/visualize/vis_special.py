d872e26522bf781c5bc84592475ceb25ad45c27f
bsavitzky <ben.savitzky@gmail.com>
2020-09-09 14:30:25 -0700

    vis functions update
    Former-commit-id: 5787ee30f13dfc6439aae138084addcb3d06a413

--- py4DSTEM/visualize/vis_special.py
+++ py4DSTEM/visualize/vis_special.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.spatial import Voronoi
-from . import show,show_circ,show_grid
+from . import show,show_circ,show_image_grid
 from ..process.utils import get_voronoi_vertices
 
 def show_kernel(kernel,R,L,W,figsize=(12,6)):
@@ -70,7 +70,7 @@ def show_class_BPs_grid(ar,H,W,x,y,get_s,s2,color='r',color2='y',returnfig=False
     """
     words
     """
-    fig,axs = show_grid(lambda i:ar,H,W,axsize=axsize,titlesize=titlesize,
+    fig,axs = show_image_grid(lambda i:ar,H,W,axsize=axsize,titlesize=titlesize,
                        get_bc=get_bc,returnfig=True,**kwargs)
     for i in range(H):
         for j in range(W):
