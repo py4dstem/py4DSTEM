ae3666d6e44ed21e311c85e4b73d2c92c825478e
bsavitzky <ben.savitzky@gmail.com>
2020-09-17 18:18:58 -0700

    Updates append with overwrite option
    Former-commit-id: f3360c7523d1a60589bba00efe25335438d1390e

---
+++ py4DSTEM/test/test_overwrite.py
@@ -0,0 +1,19 @@
+import numpy as np
+import py4DSTEM
+
+fp = '/Users/Ben/Desktop/test.h5'
+
+data_old = np.arange(100).reshape((10,10))
+data_new = data_old+1
+
+diffslice_old = py4DSTEM.file.datastructure.DiffractionSlice(
+                    data=data_old,name="data")
+diffslice_new = py4DSTEM.file.datastructure.DiffractionSlice(
+                    data=data_new,name="data")
+
+py4DSTEM.file.io.native.save(fp, diffslice_old, overwrite=True)
+py4DSTEM.file.io.native.append(fp, diffslice_new, overwrite=True)
+
+
+
+
