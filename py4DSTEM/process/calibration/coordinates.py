dcf5b8dfff3c27d34d65056abc4d3c1b720a4773
Ben Savitzky <ben.savitzky@gmail.com>
2021-03-31 14:22:10 -0700

    coordinates object update

--- py4DSTEM/process/calibration/coordinates.py
+++ py4DSTEM/process/calibration/coordinates.py
@@ -25,33 +25,46 @@ class Coordinates(object):
     will retrieve the value p if p is a number, and the value p[rx,ry] if p is an array.
     """
     def __init__(self,R_Nx,R_Ny,Q_Nx,Q_Ny,
-                 qx0=None,qy0=None,a=None,b=None,theta=None,
-                 Q_pixel_size=1,Q_pixel_units='pixels',R_pixel_size=1,R_pixel_units='pixels'):
+                 Q_pixel_size=1,Q_pixel_units='pixels',R_pixel_size=1,R_pixel_units='pixels',
+                 qx0=None,qy0=None,e=None,theta=None):
         """
         Initialize a coordinate system.
 
         Accepts:
             Q_Nx,Q_Ny         (ints) the shape of diffraction space
             R_Nx,R_Ny         (ints) the shape of real space
+            Q_pixel_size      (number)
+            Q_pixel_units     (string)
+            R_pixel_size      (number)
+            R_pixel_units     (string)
             qx0,qy0           (numbers or ndarrays) the origin of diffraction space
             e                 (number) the ratio of lengths of the semiminor to
                               semimajor axes of the elliptical distortions
             theta             (number) the (positive, right handed) tilt of the
                               semimajor axis of the elliptical distortions with
                               respect to the x-axis, in radians
-            Q_pixel_size      (number)
-            Q_pixel_units     (string)
-            R_pixel_size      (number)
-            R_pixel_units     (string)
 
         """
         self.Q_Nx,self.Q_Ny = Q_Nx,Q_Ny
         self.R_Nx,self.R_Ny = R_Nx,R_Ny
-        self.Q_pixel_size,self.Q_pixel_units = Q_pixel_size,Q_pixel_units
-        self.R_pixel_size,self.R_pixel_units = R_pixel_size,R_pixel_units
-        self.qx0,self.qy0 = qx0,qy0
-        self.a,self.b,self.theta = a,b,theta
+        self.set_Q_pixel_size(Q_pixel_size)
+        self.set_Q_pixel_units(Q_pixel_units)
+        self.set_R_pixel_size(R_pixel_size)
+        self.set_R_pixel_units(R_pixel_units)
+        if qx0 is not None: self.set_qx0(qx0)
+        if qy0 is not None: self.set_qy0(qy0)
+        if e is not None: self.set_e(e)
+        if theta is not None: self.set_theta(theta)
 
+
+    def set_Q_pixel_size(self,Q_pixel_size):
+        self.Q_pixel_size = Q_pixel_size
+    def set_Q_pixel_units(self,Q_pixel_units):
+        self.Q_pixel_units = Q_pixel_units
+    def set_R_pixel_size(self,R_pixel_size):
+        self.R_pixel_size = R_pixel_size
+    def set_R_pixel_units(self,R_pixel_units):
+        self.R_pixel_units = R_pixel_units
     def set_qx0(self,qx0):
         self._validate_input(qx0)
         self.qx0 = qx0
@@ -72,15 +85,15 @@ class Coordinates(object):
         self._validate_input(e)
         self._validate_input(theta)
         self.e,self.theta = e,theta
-    def set_Q_pixel_size(self,Q_pixel_size):
-        self.Q_pixel_size = Q_pixel_size
-    def set_Q_pixel_units(self,Q_pixel_units):
-        self.Q_pixel_units = Q_pixel_units
-    def set_R_pixel_size(self,R_pixel_size):
-        self.R_pixel_size = R_pixel_size
-    def set_R_pixel_units(self,R_pixel_units):
-        self.R_pixel_units = R_pixel_units
 
+    def get_Q_pixel_size(self):
+        return self._get_value(self.Q_pixel_size)
+    def get_Q_pixel_units(self):
+        return self._get_value(self.Q_pixel_units)
+    def get_R_pixel_size(self):
+        return self._get_value(self.R_pixel_size)
+    def get_R_pixel_units(self):
+        return self._get_value(self.R_pixel_units)
     def get_qx0(self,rx=None,ry=None):
         return self._get_value(self.qx0,rx,ry)
     def get_qy0(self,rx=None,ry=None):
@@ -93,14 +106,6 @@ class Coordinates(object):
         return self._get_value(self.theta,rx,ry)
     def get_ellipse(self,rx=None,ry=None):
         return self.get_e(rx,ry),self.get_theta(rx,ry)
-    def get_Q_pixel_size(self):
-        return self._get_value(self.Q_pixel_size)
-    def get_Q_pixel_units(self):
-        return self._get_value(self.Q_pixel_units)
-    def get_R_pixel_size(self):
-        return self._get_value(self.R_pixel_size)
-    def get_R_pixel_units(self):
-        return self._get_value(self.R_pixel_units)
 
     def _validate_input(self,p):
         assert isinstance(p,Number) or isinstance(p,np.ndarray)
@@ -117,6 +122,6 @@ class Coordinates(object):
                 assert np.all([isinstance(i,(int,np.integer)) for i in (rx,ry)])
                 assert rx<self.R_Nx and ry<self.R_Ny
                 return p[rx,ry]
-        except NameError:
+        except (NameError,AttributeError):
             return None
 
