51d4a3a446e074e8a896ccc960495ebc7fe0fcff
bsavitzky <ben.savitzky@gmail.com>
2021-06-10 13:01:03 -0400

    updates fit docstrings

--- py4DSTEM/process/fit/fit.py
+++ py4DSTEM/process/fit/fit.py
@@ -9,9 +9,10 @@ def gaussian(x, A, mu, sigma):
 
 def fit_1D_gaussian(xdata,ydata,xmin,xmax):
     """
-    Fits a 1D gaussian to the subset of the 1D curve f(xdata)=ydata within the window (xmin,xmax).
-    Returns A,mu,sigma.  Retrieve the full curve with
-        fit_gaussian = py4DSTEM.process.fit.gaussian(xdata,A,mu,sigma)
+    Fits a 1D gaussian to the subset of the 1D curve f(xdata)=ydata within the window
+    (xmin,xmax). Returns A,mu,sigma.  Retrieve the full curve with
+
+        >>> fit_gaussian = py4DSTEM.process.fit.gaussian(xdata,A,mu,sigma)
     """
     mask = (xmin<=xdata)*(xmax>xdata)
     inds = np.nonzero(mask)[0]
@@ -25,39 +26,46 @@ def fit_1D_gaussian(xdata,ydata,xmin,xmax):
     A,mu,sigma = scale*popt[0],popt[1],popt[2]
     return A,mu,sigma
 
-def fit_2D(function, data, data_mask=None, popt=None, 
+def fit_2D(function, data, data_mask=None, popt=None,
     robust=False, robust_steps=3, robust_thresh=2):
     """
-    Performs a 2D fit, where the fit function takes its first input in the form of a length 2
-    vector (ndarray) of (x,y) positions, followed by the remaining parameters, and the data to
-    fit takes the form of an (n,m) shaped array. Robust fitting can be enabled to iteratively reject
-    outlier data points, which have a root-mean-square error beyond the user-specified threshold.
+    Performs a 2D fit, where the fit function takes its first input in the form of a
+    length 2 vector (ndarray) of (x,y) positions, followed by the remaining parameters,
+    and the data to fit takes the form of an (n,m) shaped array. Robust fitting can be
+    enabled to iteratively reject outlier data points, which have a root-mean-square
+    error beyond the user-specified threshold.
 
-    Inputs:
-        function     - First input should be a length 2 array xy, where (xy[0],xy[1]) are the (x,y)
-                       coordinates
-        data         - Data to fit, in an (n,m) shaped ndarray
-        data_mask    - Optional parameter. If specified, must be a boolean array of the same shape
-                       as data, specifying which elements of data to use in the fit
-        return_ar    - Optional parameter. If False, only the fit parameters and covariance matrix
-                       are returned. If True, return an array  of the same shape as data with the
-                       fit values. Defaults to True
-        popt         - Optional parameter for input. If specified, should be a tuple of initial 
-                       guesses for the fit parameters.
-        robust       - Optional parameter. If set to True, fit will be repeated with outliers removed.
-        robust_steps - Optional parameter. Number of robust iterations performed after initial fit.
-        robust_thresh- Optional parameter. Threshold for including points, in units of root-mean-square
-                       (standard deviations) error of the predicted values after fitting. 
+    Args:
+        function: First input should be a length 2 array xy, where (xy[0],xy[1]) are the
+            (x,y) coordinates
+        data: Data to fit, in an (n,m) shaped ndarray
+        data_mask:  Optional parameter. If specified, must be a boolean array of the same
+            shape as data, specifying which elements of data to use in the fit
+        return_ar: Optional parameter. If False, only the fit parameters and covariance
+            matrix are returned. If True, return an array  of the same shape as data with
+            the fit values. Defaults to True
+        popt: Optional parameter for input. If specified, should be a tuple of initial
+            guesses for the fit parameters.
+        robust: Optional parameter. If set to True, fit will be repeated with outliers
+            removed.
+        robust_steps: Optional parameter. Number of robust iterations performed after
+            initial fit.
+        robust_thresh: Optional parameter. Threshold for including points, in units of
+            root-mean-square (standard deviations) error of the predicted values after
+            fitting.
 
-    Outputs:
-        popt      - optimal fit parameters to function
-        pcov      - the covariance matrix
-        fit_ar    - optional. If return_ar==True, fit_ar is returned, and is an array of the
-                    same shape as data, containing the fit values
+    Returns:
+        (3-tuple) A 3-tuple containing:
+
+            * **popt**: optimal fit parameters to function
+            * **pcov**: the covariance matrix
+            * **fit_ar**: optional. If return_ar==True, fit_ar is returned, and is an
+              array of the same shape as data, containing the fit values
     """
     shape = data.shape
     shape1D = [1,np.prod(shape)]
-    x,y = np.linspace(0, 1, shape[0]),np.linspace(0, 1, shape[1])  # x and y coordinates normalized from 0 to 1
+    # x and y coordinates normalized from 0 to 1
+    x,y = np.linspace(0, 1, shape[0]),np.linspace(0, 1, shape[1])
     ry,rx = np.meshgrid(y,x)
     rx_1D = rx.reshape((1,np.prod(shape)))
     ry_1D = ry.reshape((1,np.prod(shape)))
@@ -82,11 +90,11 @@ def fit_2D(function, data, data_mask=None, popt=None,
             # include user-specified mask if provided
             if data_mask is not None:
                 mask[data_mask==False] = False
-        
+
         # perform fitting
-        popt, pcov = curve_fit(function, 
-            np.vstack((rx_1D[mask.reshape(shape1D)],ry_1D[mask.reshape(shape1D)])), 
-            data[mask], 
+        popt, pcov = curve_fit(function,
+            np.vstack((rx_1D[mask.reshape(shape1D)],ry_1D[mask.reshape(shape1D)])),
+            data[mask],
             p0=popt)
 
     fit_ar = function(xy,*popt).reshape(shape)
@@ -115,10 +123,6 @@ def bezier_two(xy, c00, c01, c02, c10, c11, c12, c20, c21, c22):
         c02  *((1-xy[0])**2)  * (xy[1]**2) + \
         c12*2*(1-xy[0])*xy[0] * (xy[1]**2) + \
         c22  *(xy[0]**2)      * (xy[1]**2)
-
-
-
-
 
 
 
