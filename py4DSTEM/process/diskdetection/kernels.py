dc2383c1bd8b4548aaeefa0c18de0fd449bfccdd
SE Zeltmann <steven.zeltmann@lbl.gov>
2021-06-18 14:53:42 -0700

    add maximal points kernel. integration not tested. 50x performance vs CPU for this step

---
+++ py4DSTEM/process/diskdetection/kernels.py
@@ -0,0 +1,47 @@
+__all__ = ['kernels']
+
+kernels = {}
+
+kernels['maximal_pts_float32'] = r'''
+extern "C" __global__
+void maximal_pts(const float *ar, bool *out, const long long sizex, const long long sizey, const long long N){
+    int tid = blockDim.x * blockIdx.x + threadIdx.x;
+    int x = tid % sizex;
+    int y = tid / sizey; // Floor divide
+    bool res = false;
+    if (tid < N && x>0 && x<(sizex-1) && y>0 && y<(sizey-1)) {
+        float val = ar[tid];
+        
+        out[tid] = ( val > ar[tid + sizey]) &
+                    (val > ar[tid - sizey]) &
+                    (val > ar[tid + 1]) &
+                    (val > ar[tid - 1]) &
+                    (val > ar[tid - sizey - 1]) &
+                    (val > ar[tid - sizey + 1]) &
+                    (val > ar[tid + sizey - 1]) &
+                    (val > ar[tid+sizey + 1]);
+    }
+}
+'''
+
+kernels['maximal_pts_float64'] = r'''
+extern "C" __global__
+void maximal_pts(const double *ar, bool *out, const long long sizex, const long long sizey, const long long N){
+    int tid = blockDim.x * blockIdx.x + threadIdx.x;
+    int x = tid % sizex;
+    int y = tid / sizey; // Floor divide
+    bool res = false;
+    if (tid < N && x>0 && x<(sizex-1) && y>0 && y<(sizey-1)) {
+        double val = ar[tid];
+        
+        out[tid] = ( val > ar[tid + sizey]) &
+                    (val > ar[tid - sizey]) &
+                    (val > ar[tid + 1]) &
+                    (val > ar[tid - 1]) &
+                    (val > ar[tid - sizey - 1]) &
+                    (val > ar[tid - sizey + 1]) &
+                    (val > ar[tid + sizey - 1]) &
+                    (val > ar[tid+sizey + 1]);
+    }
+}
+'''
