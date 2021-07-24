ce0bb8184b4080458c6c93621f7e0f7967773480
Colin <cophus@gmail.com>
2021-07-24 16:37:22 -0700

    Added structure factors and associated plot
    Former-commit-id: 1e13aa6fe779a6e70953115e16a8bacfa5d6429b

--- py4DSTEM/process/diffraction/crystal.py
+++ py4DSTEM/process/diffraction/crystal.py
@@ -1,9 +1,13 @@
 # Functions for calculating diffraction patterns, matching them to experiments, and creating orientation and phase maps.
 
 import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
+from matplotlib.axes import Axes
 
 from ...io.datastructure import PointList, PointListArray
 from ..utils import tqdmnd
+from ..utils import single_atom_scatter
 
 
 
@@ -27,7 +31,7 @@ class Crystal:
         """
         
         # Initialize Crystal
-        self.positions = positions   #: fractional atomic coordinates
+        self.positions = np.asarray(positions)   #: fractional atomic coordinates
 
         #: atomic numbers - if only one value is provided, assume all atoms are same species
         numbers = np.asarray(numbers, dtype='intp')
@@ -65,7 +69,18 @@ class Crystal:
 
 
 
-    def calculate_structure_factors(self, k_max):
+    def calculate_structure_factors(self, k_max, tol_structure_factor=1e-4):
+        """
+        Calculate structure factors for all hkl indices up to max scattering vector k_max
+        
+        Args:
+            k_max (numpy float):                max scattering vector to include
+            tol_structure_factor (numpy float): tolerance for removing low-valued structure factors
+        """
+
+        # Store k_max
+        self.k_max = np.asarray(k_max)
+
         # Inverse lattice vectors
         lat_inv = np.linalg.inv(self.lat_real)
 
@@ -85,7 +100,7 @@ class Crystal:
         k_leng_min = np.min(np.linalg.norm(k_test, axis=1))
 
         # Tile lattice vectors
-        num_tile = np.ceil(k_max / k_leng_min)
+        num_tile = np.ceil(self.k_max / k_leng_min)
         ya,xa,za = np.meshgrid(
             np.arange(-num_tile, num_tile+1),
             np.arange(-num_tile, num_tile+1),
@@ -94,24 +109,91 @@ class Crystal:
         k_vec_all = np.matmul(lat_inv, hkl) 
 
         # Delete lattice vectors outside of k_max
-        keep = np.linalg.norm(k_vec_all, axis=0) <= k_max
+        keep = np.linalg.norm(k_vec_all, axis=0) <= self.k_max
         self.hkl = hkl[:,keep]
         self.k_vec_all = k_vec_all[:,keep]
         self.k_vec_leng = np.linalg.norm(self.k_vec_all, axis=0)
 
         # Calculate single atom scattering factors
-        f_all = np.zeros((np.size(self.k_vec_leng, 0), self.positions.shape[0]), dtype='complex64')
+        # Note this can be sped up a ton, but we may want to generalize to allow non-1.0 occupancy in the future.
+        f_all = np.zeros((np.size(self.k_vec_leng, 0), self.positions.shape[0]), dtype='float_')
         for a0 in range(self.positions.shape[0]):
-            f = single_atom_scatter()
-
+            atom_sf = single_atom_scatter(
+                [self.numbers[a0]],
+                [1],
+                self.k_vec_leng,
+                'A')
+            atom_sf.get_scattering_factor(
+                [self.numbers[a0]],
+                [1],
+                self.k_vec_leng,
+                'A')
+            f_all[:,a0] = atom_sf.fe
 
         # Calculate structure factors
         self.struct_factors = np.zeros(np.size(self.k_vec_leng, 0), dtype='complex64')
-        for a0 in range(self.struct_factors.shape[0]):
-            print(a0)
+        for a0 in range(self.positions.shape[0]):
+            self.struct_factors += f_all[:,a0] * \
+                np.exp((2j * np.pi) * \
+                np.sum(self.hkl * np.expand_dims(self.positions[a0,:],axis=1),axis=0))
+
+        # Remove structure factors below tolerance level
+        keep = np.abs(self.struct_factors) > tol_structure_factor
+        self.hkl = self.hkl[:,keep]
+        self.k_vec_all = self.k_vec_all[:,keep]
+        self.k_vec_leng = self.k_vec_leng[keep]
+        self.struct_factors = self.struct_factors[keep]
 
 
-        # print(self.k_vec_leng.shape)
+
+    def plot_structure_factors(
+        self,
+        proj_dir=[10,30],
+        scale_markers=1,
+        figsize=(12,12),
+        returnfig=False):
+        """
+        3D scatter plot of the structure factors using magnitude^2, i.e. intensity.
+
+        Args:
+            dir_proj (2 or 3 element numpy array):    projection direction, either [azim elev] or normal vector
+        """
 
 
+        if np.size(proj_dir) == 2:
+            el = proj_dir[0]
+            az = proj_dir[1]
+        elif np.size(proj_dir) == 3:
+            if proj_dir[0] == 0 and proj_dir[1] == 0:
+                el = 90 * np.sign(proj_dir[2])
+            else:
+                el = np.arctan(proj_dir[2]/np.sqrt(proj_dir[0]**2 + proj_dir[1]**2)) * 180/np.pi
+            az = np.arctan2(proj_dir[1],proj_dir[0]) * 180/np.pi
+        else:
+            raise Exception('Projection direction cannot contain ' + np.size(proj_dir) + ' elements')
 
+
+        # 3D plotting
+        fig = plt.figure(figsize=figsize)
+        ax = fig.add_subplot(
+            projection='3d',
+            elev=el, 
+            azim=az)
+
+        ax.scatter(
+            xs=self.k_vec_all[0,:], 
+            ys=self.k_vec_all[1,:], 
+            zs=self.k_vec_all[2,:],
+            s=scale_markers*np.abs(self.struct_factors)**2)
+
+        # axes limits
+        r = self.k_max * 1.05
+        ax.axes.set_xlim3d(left=-r, right=r) 
+        ax.axes.set_ylim3d(bottom=-r, top=r) 
+        ax.axes.set_zlim3d(bottom=-r, top=r) 
+
+        plt.show()
+
+
+        if returnfig:
+            return fig, ax
