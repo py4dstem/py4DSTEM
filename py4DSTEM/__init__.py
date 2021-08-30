4a0e4518da80fd68f7c5d95d01aa29a784743e1e
Alex Rakowski <alexrakowski90@gmail.com>
2021-08-30 16:08:42 -0700

    trying to fix import issues

--- py4DSTEM/__init__.py
+++ py4DSTEM/__init__.py
@@ -2,7 +2,7 @@ from . import process
 from . import io
 from . import visualize
 from .version import __version__
-from .process.diskdetection.diskdetection_parallel_new import beta_parallel_disk_detection
+from .process.diskdetection.new_diskdetection_parallel_new import beta_parallel_disk_detection
 #from .file.log import Logger
 #logger = Logger()
 
