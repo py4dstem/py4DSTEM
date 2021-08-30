5bc236baf18924398530dcfa21c6df4aa7048a43
Alex Rakowski <alexrakowski90@gmail.com>
2021-08-30 16:08:42 -0700

    trying to fix import issues
    Former-commit-id: 4a0e4518da80fd68f7c5d95d01aa29a784743e1e

--- py4DSTEM/process/diskdetection/__init__.py
+++ py4DSTEM/process/diskdetection/__init__.py
@@ -1,5 +1,6 @@
 from .probe import *
 from .diskdetection import *
 from .braggvectormap import *
-from .diskdetection_parallel_new import *
+from .new_diskdetection_parallel_new import *
 
+from . import new_diskdetection_parallel_new
