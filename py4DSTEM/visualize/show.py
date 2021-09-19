fixes returnclipvals in show() so that returned values are *unscaled*, and can be passed back into a new call of show() with the same scaling parameters set
Former-commit-id: 18f8a34ec136de81c637c61732c34c3a2528928d

py4DSTEM/visualize/show.py
@@ -261,21 +261,16 @@ def show(ar,figsize=(8,8),cmap='gray',scaling='none',clipvals='minmax',
         _ar[_mask] = np.log(ar.data[_mask])
         _ar[~_mask] = np.nan
         if min != None:
-            if min > 0:
-                min = np.log(min)
-            else:
-                min = np.min(_ar[_mask])
-        if max != None:
-            max = np.log(max)
+            if min > 0: min = np.log(min)
+            else: min = np.min(_ar[_mask])
+        if max != None: max = np.log(max)
     elif scaling == 'power':
         _mask = ar.data>0
         _ar = np.zeros_like(ar.data,dtype=float)
         _ar[_mask] = np.power(ar.data[_mask],power)
         _ar[~_mask] = np.nan
-        if min != None:
-            min = np.power(min,power)
-        if max != None:
-            max = np.power(max,power)
+        if min != None: min = np.power(min,power)
+        if max != None: max = np.power(max,power)
     else:
         raise Exception
 
@@ -471,11 +466,15 @@ def show(ar,figsize=(8,8),cmap='gray',scaling='none',clipvals='minmax',
     if rtheta_grid is not None:
         add_rtheta_grid(ax,grid)
 
-
     # Show or return
     returnval = []
     if returnfig: returnval.append((fig,ax))
-    if returnclipvals: returnval.append((vmin,vmax))
+    if returnclipvals:
+        if scaling == 'log':
+            vmin,vmax = np.power(np.e,vmin),np.power(np.e,vmax)
+        elif scaling == 'power':
+            vmin,vmax = np.power(vmin,1/power),np.power(vmax,1/power)
+        returnval.append((vmin,vmax))
     if returncax: returnval.append(cax)
     if len(returnval)==0:
         if figax is None: