6c772dcb35ccb4ca252ff4c3098b71319ffe9d50
bsavitzky <bsavitzky@users.noreply.github.com>
2021-11-15 14:29:29 -0800

    Update overlay.py
    Former-commit-id: 6e26342faefa88020f002e362225a4f1f979e604

--- py4DSTEM/visualize/overlay.py
+++ py4DSTEM/visualize/overlay.py
@@ -1,5 +1,4 @@
 import numpy as np
-import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle,Circle,Wedge,Ellipse
 from matplotlib.axes import Axes
 from matplotlib.colors import is_color_like
