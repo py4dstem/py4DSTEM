95d6d22a4a55755a9912afaf9ffed63e3925e779
SE Zeltmann <steven.zeltmann@lbl.gov>
2021-11-24 16:20:41 -0800

    untested addition of A matrix calculation

--- py4DSTEM/process/diffraction/crystal_bloch.py
+++ py4DSTEM/process/diffraction/crystal_bloch.py
@@ -3,33 +3,36 @@ import numpy as np
 from typing import Union, Optional
 
 from ...io.datastructure import PointList, PointListArray
+from ..dpc import get_interaction_constant
+from ..utils import electron_wavelength_angstrom
+
 
 def generate_dynamical_diffraction_pattern(
     self,
     beams: PointList,
-    thickness: Union[float,list,tuple,np.ndarray],
+    thickness: Union[float, list, tuple, np.ndarray],
     zone_axis: Union[list, tuple, np.ndarray] = [0, 0, 1],
     foil_normal: Optional[Union[list, tuple, np.ndarray]] = None,
-):
+) -> PointList:
     """
-    Generate a dynamical diffraction pattern (or thickness series of patterns) 
+    Generate a dynamical diffraction pattern (or thickness series of patterns)
     using the Bloch wave method.
 
     The beams to be included in the Bloch calculation must be pre-calculated
     and passed as a PointList containing at least (qx, qy, h, k, l) fields.
 
     If ``thickness`` is a single value, one new PointList will be returned.
-    If ``thickness`` is a sequence of values, a list of PointLists will be returned, 
+    If ``thickness`` is a sequence of values, a list of PointLists will be returned,
         corresponding to each thickness value in the input.
-    
-    Frequent reference will be made to 
+
+    Frequent reference will be made to
 
     Args:
         beams (PointList):              PointList from the kinematical diffraction generator
                                         which will define the beams included in the Bloch calculation
         thickness (float or list/array) thickness to evaluate diffraction patterns at.
                                         The main Bloch calculation can be reused for multiple thicknesses
-                                        without much overhead. 
+                                        without much overhead.
         zone_axis (np float vector):     3 element projection direction for sim pattern
                                          Can also be a 3x3 orientation matrix (zone axis 3rd column)
         foil_normal:                     3 element foil normal - set to None to use zone_axis
@@ -39,12 +42,93 @@ def generate_dynamical_diffraction_pattern(
         bragg_peaks (PointList):         Bragg peaks with fields [qx, qy, intensity, h, k, l]
     """
 
-    # Compute the reduced structure matrix \hat{A} in DeGraef 5.52
+    n_beams = beams.length
+    beam_g, beam_h = np.meshgrid(2 * (np.arange(n_beams)))
 
-    # Compute eigen-decomposition of \hat{A} to yield C (the matrix containing the eigenvectors
-    # as its columns) and gamma (the reduced eigenvalues), as in DeGraef 5.52
+    # Clean up zone axis input (same as in kinematic function)
+    zone_axis = np.asarray(zone_axis) / np.linalg.norm(zone_axis)
+    if not self.cartesian_directions:
+        zone_axis = self.cartesian_to_crystal(zone_axis)
 
-    # Compute thickness matrix/matrices E (DeGraef 5.60)
+    # Foil normal vector
+    if foil_normal is None:
+        foil_normal = zone_axis
+    else:
+        foil_normal = np.asarray(foil_normal, dtype="float")
+        if not self.cartesian_directions:
+            foil_normal = self.crystal_to_cartesian(foil_normal)
+        else:
+            foil_normal = foil_normal / np.linalg.norm(foil_normal)
 
-    # Compute diffraction intensities by calculating exit wave \Psi in DeGraef 5.60, and collect
-    # values into PointLists
+    # Note the difference in notation versus kinematic function:
+    # k0 is the scalar magnitude of the wavevector, rather than
+    # a vector along the zone axis. That is instead called ``ZA``
+    k0 = 1.0 / electron_wavelength_angstrom(self.accel_voltage)
+    ZA = zone_axis * k0
+
+    ################################################################
+    # Compute the reduced structure matrix \hat{A} in DeGraef 5.52 #
+    ################################################################
+    
+    hkl = np.vstack((beams.data["h"], beams.data["k"], beams.data["l"]))
+
+    # get hkl indices of \vec{g} - \vec{h}
+    g_minus_h = np.vstack(
+        (
+            beams.data["h"][beam_g] - beams.data["h"][beam_h],
+            beams.data["k"][beam_g] - beams.data["k"][beam_h],
+            beams.data["l"][beam_g] - beams.data["l"][beam_h],
+        )
+    )
+
+    # Check if each beam has a computed structure factor. We'll ignore coupling for scattering
+    # greater than the k_max used in compute_structure_factors. We also flag
+    # beams where g-h=0 to ignore.
+    nonzero_beams = [gmh in self.hkl and not np.array_equal(gmh,[0,0,0]) for gmh in g_minus_h]
+
+    # Relativistic correction to the potentials [2.38]
+    prefactor = get_interaction_constant(self.accel_voltage) / (
+        np.pi * electron_wavelength_angstrom(self.accel_voltage)
+    )
+
+    # Get the structure factors for each nonzero element, and zero otherwise
+    U_gmh = np.array(
+        [
+            prefactor
+            * self.structure_factors[int(np.where((self.hkl == gmh).all(axis=1))[0])]
+            if nonzero
+            else 0.0 + 0.0j
+            for gmh, nonzero in zip(g_minus_h, nonzero_beams)
+        ],
+        dtype=np.complex64,
+    ).reshape(beam_g.shape)
+
+    # Compute the diagonal entries of \hat{A}: 2 k_0 s_g
+    g = np.linalg.inv(self.lat_real) @ hkl
+    cos_alpha = np.sum(
+        (ZA[:, None] + g) * foil_normal[:, None], axis=0
+    ) / np.linalg.norm(ZA[:, None] + g, axis=0)
+
+    sg = (
+        (-0.5)
+        * np.sum((2 * ZA[:, None] + g) * g, axis=0)
+        / (np.linalg.norm(ZA[:, None] + g, axis=0))
+        / cos_alpha
+    )
+
+    # Fill in the diagonal, completing the structure mattrx
+    np.fill_diagonal(U_gmh, 2 * k0 * sg)
+
+    #############################################################################################
+    # Compute eigen-decomposition of \hat{A} to yield C (the matrix containing the eigenvectors #
+    # as its columns) and gamma (the reduced eigenvalues), as in DeGraef 5.52                   #
+    #############################################################################################
+
+    ######################################################
+    # Compute thickness matrix/matrices E (DeGraef 5.60) #
+    ######################################################
+
+    ##############################################################################################
+    # Compute diffraction intensities by calculating exit wave \Psi in DeGraef 5.60, and collect #
+    # values into PointLists                                                                     #
+    ##############################################################################################
