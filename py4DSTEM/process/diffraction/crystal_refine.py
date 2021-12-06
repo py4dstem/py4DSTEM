f8899a040a50e22078feb7fed4daff44a0d0cafd
SE Zeltmann <steven.zeltmann@lbl.gov>
2021-12-06 13:32:29 -0800

    signature for refinement pattern indexer

--- py4DSTEM/process/diffraction/crystal_refine.py
+++ py4DSTEM/process/diffraction/crystal_refine.py
@@ -19,6 +19,22 @@ def estimate_thickness(
         bragg_peaks (PointList):        experimental measured disk intensities, with hkl indices
         bloch_beams (PointList):        beams to include in the Bloch wave dynamical diffraction
                                         calculation
-        thickness (ndarray):            Array of thickness values to compare against 
+        thickness (ndarray):            Array of thickness values to compare against
+    """
+    return
+
+
+def index_pattern(
+    self,
+    bragg_peaks: PointList,
+    orientation: np.ndarray,
+    tol_distance: float,
+    signal_excitation_error: float,
+) -> PointList:
+    """
+    Given a set of experimental Bragg disk locations and the orientation matrix
+    computed in ``match_single_pattern``, select the experimental peaks that are
+    within ``tol_distance`` of the kinematically predicted ones and return a
+    new PointList containing (qx,qy,Intensity,h,k,l) for the matching peaks.
     """
     return
