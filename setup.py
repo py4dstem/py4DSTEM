9ff32ffa8a8f3877693834281b615e73a9069f52
bsavitzky <ben.savitzky@gmail.com>
2021-12-23 13:22:00 -0800

    Versions to 0.13.0
    Former-commit-id: 632dbd1c34260aaec193e6dd2af2e936d8c5c042

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md","r") as f:
 
 setup(
     name='py4DSTEM',
-    version='0.12.7',
+    version='0.13.0',
     packages=find_packages(),
     description='An open source python package for processing and analysis of 4D STEM data.',
     long_description=long_description,
