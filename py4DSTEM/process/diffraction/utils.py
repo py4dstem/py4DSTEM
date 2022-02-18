8a7bace2bf32a7523df2ab264e26f53431325917
Colin <cophus@gmail.com>
2022-02-18 14:05:43 -0800

    Plotting basics work for cubic
    Former-commit-id: 779ddc55bafde7ac39013f24876af77b12ba0907

--- py4DSTEM/process/diffraction/utils.py
+++ py4DSTEM/process/diffraction/utils.py
@@ -41,6 +41,10 @@ class OrientationMap:
     corr = None
     inds = None
     angles = None
+    # basis_zone_axis = None
+    # basis_in_plane = None
+    # map_fiber = None
+
 
     def __post_init__(self):
         # initialize empty arrays
@@ -67,8 +71,19 @@ class OrientationMap:
         orientation.inds = self.inds[ind_x,ind_y]
         orientation.mirror = self.mirror[ind_x,ind_y]
         orientation.angles = self.angles[ind_x,ind_y]
-
         return orientation
+
+    # def set_basis_zone_axis(self,basis,ind_match):
+    #     if basis_zone_axis is None:
+    #         self.basis_zone_axis = np.zeros((self.num_x,self.num_y,self.num_matches,3))
+    #     self.basis_zone_axis[:,:,ind_match,:] = basis
+
+    # def get_basis_zone_axis(self,ind_match):
+    #     return self.basis_zone_axis[:,:,ind_match,:]
+
+
+
+
 
 
 def axisEqual3D(ax):
