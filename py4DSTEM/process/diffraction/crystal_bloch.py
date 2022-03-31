fix foil normal and sign of s_g, intensities behave correctly now

py4DSTEM/process/diffraction/crystal_bloch.py
@@ -285,7 +285,7 @@ def generate_dynamical_diffraction_pattern(
     else:
         foil_normal = zone_axis
 
-    foil_normal = -foil_normal[:,2]
+    foil_normal = foil_normal[:,2]
 
     # Note the difference in notation versus kinematic function:
     # k0 is the scalar magnitude of the wavevector, rather than
@@ -336,7 +336,7 @@ def generate_dynamical_diffraction_pattern(
 
     # Compute the diagonal entries of \hat{A}: 2 k_0 s_g [5.51]
     g = (hkl @ self.lat_inv) @ zone_axis
-    sg = self.excitation_errors(g.T, foil_normal=foil_normal)
+    sg = self.excitation_errors(g.T, foil_normal=-foil_normal)
 
     # import matplotlib.pyplot as plt
     # sgp = np.sign(sg) >= 0
@@ -479,6 +479,8 @@ def generate_CBED(
         foil_normal_cartesian = zone_axis_cartesian
 
     # TODO: refine pixel size to center reflections on pixels
+    # from pdb import set_trace
+    # set_trace()
 
     # Generate list of plane waves inside aperture
     alpha_pix = np.round(