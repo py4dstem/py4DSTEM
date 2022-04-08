505ea25954fcc90c15cc125ac862452d71746dcc
bsavitzky <ben.savitzky@gmail.com>
2022-04-08 02:40:18 -0400

    adds arraystack
    Former-commit-id: 2f8776abcd710c34fb390dc3905aaa0150924625

---
+++ py4DSTEM/test/io/make_arraystack.py
@@ -0,0 +1,66 @@
+import py4DSTEM
+import numpy as np
+
+# Make a 3D numpy array
+shape = (10,10,4)
+data = np.arange(np.prod(shape)).reshape(shape)
+
+# Make the Array instance
+py4dstem_arraystack = py4DSTEM.io.datastructure.ArrayStack(
+    data = data,
+    name = 'test_arraystack',
+    units = 'intensity',
+    dims = [
+        5,
+        [0,5]
+    ],
+    dim_units = [
+        'nm',
+        'nm'
+    ],
+    dim_names = [
+        'rx',
+        'ry'
+    ],
+    labels = [
+        'meow',
+        'mix',
+        'quick',
+        'fix'
+    ]
+)
+
+print(py4dstem_arraystack)
+
+print(py4dstem_arraystack.get_data('meow'))
+
+
+# Write to and HDF5 file
+
+# import h5py
+# from os.path import exists
+# from os import remove
+# fp = "/Users/Ben/Desktop/test.h5"
+# if exists(fp): remove(fp)
+# 
+# with h5py.File(fp,'w') as f:
+#     group = f.create_group('experiment')
+#     # write the array to the h5 file
+#     py4dstem_array.to_h5(group)
+# 
+# 
+# with h5py.File(fp,'r') as f:
+#     grp = f['experiment']
+#     array_names = py4DSTEM.io.datastructure.find_Arrays(grp)
+#     exists = py4DSTEM.io.datastructure.Array_exists(grp,'test_array')
+#     ar = py4DSTEM.io.datastructure.Array_from_h5(grp,'test_array')
+# 
+#     print(array_names)
+#     print(exists)
+#     print(py4dstem_array)
+#     print(ar)
+# 
+#     ar.set_dim(1,[-25,25],'A')
+#     print(ar)
+
+
