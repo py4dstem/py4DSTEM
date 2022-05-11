904ff967573094e6d4d0616ca21d5b37cd9d328e
Ben Savitzky <ben.savitzky@gmail.com>
2022-05-11 14:14:01 -0700

    bugfix
    Former-commit-id: 9c28d03f31bdb831fa47bfdda3dfc4992cfea235

--- py4DSTEM/process/diskdetection/diskdetection_aiml.py
+++ py4DSTEM/process/diskdetection/diskdetection_aiml.py
@@ -759,7 +759,7 @@ def _get_latest_model(model_path = None):
         if os.path.exists(file_path) and file_id == file_id_old:
             print('Latest model weight is already available in the local system. Loading the model... \n')
             model_path = file_path
-            #os.remove('./tmp/model_metadata_old.json')
+            os.remove('./tmp/model_metadata_old.json')
             os.rename('./tmp/model_metadata.json', './tmp/model_metadata_old.json')
         else:
             print('Checking the latest model on the cloud... \n')
