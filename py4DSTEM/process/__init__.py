585a23ad695e1e67ceee1211f2618f0b41b0aca6
Colin <cophus@gmail.com>
2022-07-12 15:23:20 -0700

    Adding back in more imports
    Former-commit-id: f0bf3215cfef923e1910cc8b9bfdad773ccc95fe

--- py4DSTEM/process/__init__.py
+++ py4DSTEM/process/__init__.py
@@ -4,9 +4,8 @@ from . import probe
 from . import diskdetection
 from . import dpc
 from . import calibration
+from . import utils
 
-
-#from . import utils
 #from . import fit
 #from . import latticevectors
 #from . import classification
