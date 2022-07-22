6b4786116e2f6b80671f1b41137111ae44a63aeb
Ben Savitzky <ben.savitzky@gmail.com>
2022-07-22 11:54:11 -0700

    updates

--- py4DSTEM/__init__.py
+++ py4DSTEM/__init__.py
@@ -14,7 +14,7 @@ from . import visualize
 # functions
 
 from .visualize import show
-from .io import read, save, import_file
+from .io import read, save, import_file, print_h5_tree
 from .utils.configuration_checker import check_config
 
 
