82ad2c8b5a074c269ecd56f58e6324b5fb1a9e23
Colin <cophus@gmail.com>
2022-08-10 14:39:24 -0700

    Removing more bad attributes

--- py4DSTEM/preprocess/preprocess.py
+++ py4DSTEM/preprocess/preprocess.py
@@ -126,7 +126,6 @@ def bin_data_mmap(datacube, bin_factor, dtype=np.float32):
         data[Rx,Ry,:,:] = bin2D(datacube.data[Rx,Ry,:,:],bin_factor,dtype=dtype)
 
     datacube.data = data
-    datacube.R_Nx,datacube.R_Ny,datacube.Q_Nx,datacube.Q_Ny = datacube.data.shape
     return datacube
 
 def bin_data_real(datacube, bin_factor):
@@ -148,7 +147,6 @@ def bin_data_real(datacube, bin_factor):
         else:
             datacube.data = datacube.data[:-(R_Nx%bin_factor),:-(R_Ny%bin_factor),:,:]
         datacube.data = datacube.data.reshape(int(R_Nx/bin_factor),bin_factor,int(R_Ny/bin_factor),bin_factor,Q_Nx,Q_Ny).sum(axis=(1,3))
-        datacube.R_Nx,datacube.R_Ny,datacube.Q_Nx,datacube.Q_Ny = datacube.data.shape
         return datacube
 
 def filter_hot_pixels(
