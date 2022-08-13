2f1d6f503692e4fdb580869b64315779b5472f48
Stephanie Ribet <stephanie.ribet@gmail.com>
2022-08-13 07:53:35 -0700

    add doc strings and progress bar
    Former-commit-id: 75263884b097fb603a95d0510d91ce041166ade5

--- py4DSTEM/process/virtualimage2.py
+++ py4DSTEM/process/virtualimage2.py
@@ -18,12 +18,44 @@ def get_virtual_image(
     x_max = None, 
     y_min = None, 
     y_max = None,
-    virtual_mask = None
+    virtual_mask = None, 
+    verbose = True,
 ):
-    """
-
-    """
+    '''
+    Function to calculate virtual image
     
+    Args: 
+        datacube (Datacube)     : datacube class object which stores 4D-dataset
+                                needed for calculation
+        mode (string)           : defines geometry mode for calculating virtual image
+                                  options:
+                                    "point" uses singular point as detector
+                                    "circle" or "circular" uses round detector,
+                                        like bright field
+                                    "annular" or "annulus" uses annular detector,
+                                         like dark field
+                                    "rectangle", "square", "rectangular", uses square detector
+                                    "mask" flexible detector, any 2D array
+                    
+        qx (float)              : x index for center of detector for point, 
+                                circle and annular detector (pixels)
+        qy (float)              : y index for center of detector for point, 
+                                circle and annular detector (pixels)
+        radius_i (float)        : radius of circlular detector or inner radius 
+                                 of annulus (pixels)
+        radius_o (float)        : outer radius of annulus (pixels)
+        x_min (float)           : minimum x value of rectangular detector (pixels)
+        x_max (float)           : maximum x value of rectangular detector (pixels)
+        y_min (float)           : minimum y value of rectangular detector (pixels)
+        y_max (float)           : maximum y value of rectangular detector (pixels)
+        virtual_mask (2D array) : 2D array with shape Q_Nx, Q_Ny (same shape as 
+                                  diffraction space)
+        verbose (bool)          : if True, show progress bar
+
+    Returns:
+        virtual image (2D-array)
+    '''
+
     #point mode 
     if mode == 'point':
         assert(qx,qy), "specify qx and qy"
@@ -38,7 +70,7 @@ def get_virtual_image(
         mask = (qxa - qx) ** 2 + (y-jc) ** 2 < radius_i ** 2
 
     #annular mask 
-    if mode == 'annulus'
+    if mode in('annulus', 'annular') :
         assert(qx qy,radius_i, radius_o), "specify qx, qy, radius_i, radius_o"
         qxa, qya = np.indices((datacube.Q_Nx, datacube.Q_Ny))
         mask1 = (qxa - qx) ** 2 + (y-jc) ** 2 > radius_i ** 2
@@ -53,8 +85,16 @@ def get_virtual_image(
 
     #flexible mask
     if mode == 'mask'
+        assert (virtual_mask.shape == (datacube.Q_Nx, datacube.Q_Ny)), 
+            "check mask dimensions"
         mask = virtual_mask
 
-    virtual_image = np.sum(datacube*mask, axis = (2,3))
+   virtual_image = np.zeros((datacube.R_Nx, datacube.R_Ny)) 
+    for rx,ry in tqdmnd(
+        datacube.R_Nx, 
+        datacube.R_Ny,
+        disable = not verbose,
+    ):
+        virtual_image[rx,ry] = np.sum(datacube.data[rx,ry]*mask)
 
     return virtual_image
