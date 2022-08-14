3df918d343cf8e4b6edc17e92611bf6f96729f27
Stephanie Ribet <stephanie.ribet@gmail.com>
2022-08-14 13:11:11 -0700

    modify virtual detectors to be compatible with geometry syntax

--- py4DSTEM/process/virtualimage.py
+++ py4DSTEM/process/virtualimage.py
@@ -1,102 +1,92 @@
 # Functions for generating virtual images
 
 import numpy as np
-import dask.array as da
-import h5py
-import warnings
+#import dask.array as da
+#import h5py
+#import warnings
 
 from ..utils.tqdmnd import tqdmnd
 
 def get_virtual_image(
     datacube, 
     mode, 
-    qx = None,
-    qy = None,
-    radius_i = None,
-    radius_o = None,
-    x_min = None, 
-    x_max = None, 
-    y_min = None, 
-    y_max = None,
-    virtual_mask = None, 
+    geometry,
+    shift_center = False,
     verbose = True,
 ):
     '''
     Function to calculate virtual image
     
     Args: 
-        datacube (Datacube)     : datacube class object which stores 4D-dataset
-                                needed for calculation
-        mode (string)           : defines geometry mode for calculating virtual image
-                                  options:
-                                    "point" uses singular point as detector
-                                    "circle" or "circular" uses round detector,
-                                        like bright field
-                                    "annular" or "annulus" uses annular detector,
-                                         like dark field
-                                    "rectangle", "square", "rectangular", uses square detector
-                                    "mask" flexible detector, any 2D array
-                    
-        qx (float)              : x index for center of detector for point, 
-                                circle and annular detector (pixels)
-        qy (float)              : y index for center of detector for point, 
-                                circle and annular detector (pixels)
-        radius_i (float)        : radius of circlular detector or inner radius 
-                                 of annulus (pixels)
-        radius_o (float)        : outer radius of annulus (pixels)
-        x_min (float)           : minimum x value of rectangular detector (pixels)
-        x_max (float)           : maximum x value of rectangular detector (pixels)
-        y_min (float)           : minimum y value of rectangular detector (pixels)
-        y_max (float)           : maximum y value of rectangular detector (pixels)
-        virtual_mask (2D array) : 2D array with shape Q_Nx, Q_Ny (same shape as 
-                                  diffraction space)
-        verbose (bool)          : if True, show progress bar
+        datacube (Datacube) : datacube class object which stores 4D-dataset
+                              needed for calculation
+        mode (str)          : defines geometry mode for calculating virtual image
+                                options:
+                                    - 'point' uses singular point as detector
+                                    - 'circle' or 'circular' uses round detector,like bright field
+                                    - 'annular' or 'annulus' uses annular detector, like dark field
+                                    - 'rectangle', 'square', 'rectangular', uses rectangular detector
+                                    - 'mask' flexible detector, any 2D array
+        geometry (variable) : valid entries are determined by the `mode`, values in pixels
+                                argument, as follows:
+                                    - 'point': 2-tuple, (qx,qy)
+                                    - 'circle' or 'circular': nested 2-tuple, ((qx,qy),radius)
+                                    - 'annular' or 'annulus': nested 2-tuple, ((qx,qy),(radius_i,radius_o))
+                                    - 'rectangle', 'square', 'rectangular': 4-tuple, (xmin,xmax,ymin,ymax)
+                                    - `mask`: flexible detector, any 2D array, same size as datacube.QShape         
+        verbose (bool)      : if True, show progress bar
 
     Returns:
+
         virtual image (2D-array)
     '''
+    g = geometry
 
     #point mode 
     if mode == 'point':
-        assert(qx,qy) is not None, "specify qx and qy"
+        assert(isinstance(g,tuple) and len(g)==2), 'specify qx and qy as tuple (qx, qy)'
         mask = np.zeros((datacube.Q_Nx, datacube.Q_Ny))
         
-        if qx%1 > 1e-6: 
-            print("warning: rounding qx to integer")
+        if g[0]%1 > 1e-6: 
+            print('warning: rounding qx to integer')
 
-        if qy%1 > 1e-6: 
-            print("warning: rounding qy to integer")
+        if g[1]%1 > 1e-6: 
+            print('warning: rounding qy to integer')
             
-        qx = int(qx)
-        qy = int(qy)
+        qx = int(g[0])
+        qy = int(g[1])
         
         mask[qx,qy] = 1
 
     #circular mask
     if mode in('circle', 'circular') :
-        assert (qx, qy,radius_i) is not None, "specify qx, qy, radius_i"
+        assert(isinstance(g,tuple) and len(g)==2 and len(g[0])==2 and isinstance(g[1],float or int)), \
+        'specify qx, qy, radius_i as ((qx, qy), radius)'
 
         qxa, qya = np.indices((datacube.Q_Nx, datacube.Q_Ny))
-        mask = (qxa - qx) ** 2 + (qya - qy) ** 2 < radius_i ** 2
+        mask = (qxa - g[0][0]) ** 2 + (qya - g[0][1]) ** 2 < g[1] ** 2
 
     #annular mask 
     if mode in('annulus', 'annular') :
-        assert (qx, qy,radius_i, radius_o) is not None, "specify qx, qy, radius_i, radius_o"
+        assert(isinstance(g,tuple) and len(g)==2 and len(g[0])==2 and len(g[1])==2), \
+        'specify qx, qy, radius_i, radius_0 as ((qx, qy), radius_i, radius_o)'
         qxa, qya = np.indices((datacube.Q_Nx, datacube.Q_Ny))
-        mask1 = (qxa - qx) ** 2 + (qya - qy) ** 2 > radius_i ** 2
-        mask2 = (qxa - qx) ** 2 + (qya - qy) ** 2 < radius_o ** 2
+        mask1 = (qxa - g[0][0]) ** 2 + (qya - g[0][1]) ** 2 > g[1][0] ** 2
+        mask2 = (qxa - g[0][0]) ** 2 + (qya - g[0][1]) ** 2 < g[1][1] ** 2
         mask = np.logical_and(mask1, mask2)
 
     #rectangle mask 
     if mode in('rectangle', 'square', 'rectangular') :
-        assert (x_min, x_max, y_min, y_max) is not None, "specify x_min, x_max, y_min, y_max"
+        assert(isinstance(g,tuple) and len(g)==4), \
+       'specify x_min, x_max, y_min, y_max as (x_min, x_max, y_min, y_max)'
         mask = np.zeros((datacube.Q_Nx, datacube.Q_Ny))
-        mask[x_min:x_max, y_min:y_max] = 1
+        mask[g[0]:g[1], g[2]:g[3]] = 1
 
     #flexible mask
     if mode == 'mask' :
-        assert (virtual_mask.shape == (datacube.Q_Nx, datacube.Q_Ny)), "check mask dimensions"
-        mask = virtual_mask
+        assert type(g) == np.ndarray, '`geometry` type should be `np.ndarray`'
+        assert (g.shape == datacube.Qshape), 'mask and diffraction pattern shapes do not match'
+        mask = g
 
     virtual_image = np.zeros((datacube.R_Nx, datacube.R_Ny)) 
     for rx,ry in tqdmnd(
