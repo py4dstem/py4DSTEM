6f5bc37492a4480515ce5d78adb5d4a71ed5fc6f
bsavitzky <ben.savitzky@gmail.com>
2022-08-24 18:43:46 -0400

    cleans commented code/imports

--- py4DSTEM/io/datastructure/emd/__init__.py
+++ py4DSTEM/io/datastructure/emd/__init__.py
@@ -4,6 +4,5 @@ from py4DSTEM.io.datastructure.emd.metadata import *
 from py4DSTEM.io.datastructure.emd.array import *
 from py4DSTEM.io.datastructure.emd.pointlist import *
 from py4DSTEM.io.datastructure.emd.pointlistarray import *
-
 from py4DSTEM.io.datastructure.emd.io import *
 
--- py4DSTEM/process/__init__.py
+++ py4DSTEM/process/__init__.py
@@ -6,11 +6,8 @@ from py4DSTEM.process import dpc
 from py4DSTEM.process import calibration
 from py4DSTEM.process import utils
 from py4DSTEM.process import classification
+from py4DSTEM.process import latticevectors
+from py4DSTEM.process import diffraction
 
 #from . import fit
-#from . import calibration
-from py4DSTEM.process import latticevectors
-#from . import classification
 #from . import rdf
-#from . import virtualimage
-from py4DSTEM.process import diffraction
