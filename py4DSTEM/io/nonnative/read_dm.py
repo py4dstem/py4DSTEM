e82d146806113aad60db53569403a5f2ec5c9fca
SE Zeltmann <steven.zeltmann@lbl.gov>
2022-09-08 12:58:36 -0700

    fix DM bin on load and read calibrations

--- py4DSTEM/io/nonnative/read_dm.py
+++ py4DSTEM/io/nonnative/read_dm.py
@@ -3,17 +3,13 @@
 import numpy as np
 from pathlib import Path
 from ncempy.io import dm
+
+from py4DSTEM import tqdmnd
 from py4DSTEM.io.datastructure import DataCube, Array, Metadata
 from py4DSTEM.process.utils import bin2D
 
 
-def read_dm(
-    filepath,
-    name = 'dm_dataset',
-    mem = "RAM",
-    binfactor = 1,
-    **kwargs
-    ):
+def read_dm(filepath, name="dm_dataset", mem="RAM", binfactor=1, **kwargs):
     """
     Read a digital micrograph 4D-STEM file.
 
@@ -25,6 +21,8 @@ def read_dm(
             See docstring for py4DSTEM.file.io.read.  Default is 1.
         metadata (bool, optional): if True, returns the file metadata as a Metadata
             instance.
+        kwargs:
+            "dtype": a numpy dtype specifier to use for data binned on load, defaults to np.float32
 
     Returns:
        DataCube, if a 4D dataset is found, else Array if a 2D dataset is found
@@ -39,74 +37,69 @@ def read_dm(
         dataset_index = 0
         for i in range(dmFile.numObjects - thumbanil_count):
             temp_data = dmFile.getMemmap(dataset_index)
-            if len(np.squeeze(temp_data).shape) > 2 :
+            if len(np.squeeze(temp_data).shape) > 2:
                 dataset_index = i
                 break
+
+        # The pixel sizes of all datasets are chained together, so
+        # we have to figure out the right offset
+        scale_offset = sum(dmFile.dataShape[:dataset_index]) + 2 * thumbanil_count
+        pixelsize = dmFile.scale[scale_offset:]
+        pixelunits = dmFile.scaleUnit[scale_offset:]
+        print(scale_offset,pixelsize,pixelunits)
+
+        # Get the calibration pixel sizes
+        Q_pixel_size = pixelsize[0]
+        Q_pixel_units = "px" if pixelunits[0] == "" else pixelunits[0]
+        R_pixel_size = pixelsize[2]
+        R_pixel_units = "px" if pixelunits[2] == "" else pixelunits[2]
 
         # Handle 3D NCEM TitanX data
         titan_shape = _process_NCEM_TitanX_Tags(dmFile)
 
-        if (mem, binfactor) == ("RAM", 1):
-                _data = dmFile.getDataset(dataset_index)['data']
+        if mem == "RAM":
+            if binfactor == 1:
+                _data = dmFile.getDataset(dataset_index)["data"]
+            else:
+                # get the dtype for the binned data
+                dtype = kwargs.get("dtype", np.float32)
+
+                _mmap = dmFile.getMemmap(dataset_index)
+                if titan_shape is not None:
+                    # NCEM TitanX tags were found
+                    _mmap = np.reshape(_mmap, titan_shape + _mmap.shape[-2:])
+                new_shape = (
+                    *_mmap.shape[:2],
+                    _mmap.shape[2] // binfactor,
+                    _mmap.shape[3] // binfactor,
+                )
+                _data = np.zeros(new_shape)
+
+                for rx, ry in tqdmnd(*_data.shape[:2]):
+                    _data[rx, ry] = bin2D(_mmap[rx, ry], binfactor, dtype=dtype)
+
         elif (mem, binfactor) == ("MEMMAP", 1):
             _data = dmFile.getMemmap(dataset_index)
-        elif (mem) == ("RAM"):
-            raise NotImplementedError("Bin-on-load currently broken.")
-            # with dm.fileDM(filepath, on_memory=True) as dmFile:
-            #     # loop through the datasets until a >2D one is found:
-            #     i = 0
-            #     valid_data = False
-            #     while not valid_data:
-            #         memmap = dmFile.getMemmap(i)
-            #         if len(np.squeeze(memmap).shape) > 2 :
-            #             valid_data = True
-            #         i += 1
-            #     if "dtype" in kwargs.keys():
-            #         dtype = kwargs["dtype"]
-            #     else:
-            #         dtype = memmap.dtype
-            #     shape = memmap.shape
-            #     rank = len(shape)
-            #     if rank==4:
-            #         R_Nx, R_Ny, Q_Nx, Q_Ny = shape
-            #     elif rank==3:
-            #         titanTags = _process_NCEM_TitanX_Tags(dmFile)
-            #         if titanTags is not None:
-            #             R_Nx, R_Ny = titanTags
-            #             Q_Nx, Q_Ny = shape[1:]
-            #         else:
-            #             R_Nx, Q_Nx, Q_Ny = shape
-            #             R_Ny = 1
-            #     else:
-            #         raise Exception(f"Data should be 4-dimensional; found {rank} dimensions")
-            #     Q_Nx, Q_Ny = Q_Nx // binfactor, Q_Ny // binfactor
-            #     data = np.empty((R_Nx, R_Ny, Q_Nx, Q_Ny), dtype=dtype)
-            #     for Rx in range(R_Nx):
-            #         for Ry in range(R_Ny):
-            #             if rank==4:
-            #                 data[Rx, Ry, :, :] = bin2D(
-            #                     memmap[Rx, Ry, :, :,], binfactor, dtype=dtype
-            #                 )
-            #             else:
-            #                 data[Rx, Ry, :, :] = bin2D(
-            #                     memmap[Rx, :, :,], binfactor, dtype=dtype
-            #                 )
-            #     dc = DataCube(data=data)
         else:
-            raise Exception("Memory mapping and on-load binning together is not supported.  Either set binfactor=1 or mem='RAM'.")
+            raise Exception(
+                "Memory mapping and on-load binning together is not supported.  Either set binfactor=1 or mem='RAM'."
+            )
             return
 
         if titan_shape is not None:
             # NCEM TitanX tags were found
-            _data = np.reshape(_data,titan_shape + _data.shape[-2:])
+            _data = np.reshape(_data, titan_shape + _data.shape[-2:])
 
         if len(_data.shape) == 4:
             data = DataCube(_data, name=name)
+            data.calibration.set_Q_pixel_size(Q_pixel_size * binfactor)
+            data.calibration.set_Q_pixel_units(Q_pixel_units)
+            data.calibration.set_R_pixel_size(R_pixel_size)
+            data.calibration.set_R_pixel_units(R_pixel_units)
         else:
             data = Array(_data, name=name)
 
     return data
-
 
 
 def _process_NCEM_TitanX_Tags(dmFile, dc=None):
@@ -116,21 +109,21 @@ def _process_NCEM_TitanX_Tags(dmFile, dc=None):
     Also fixes the two-pixel roll issue present in TitanX data. If no datacube
     is passed, return R_Nx and R_Ny
     """
-    scanx = [v for k,v in dmFile.allTags.items() if "4D STEM Tags.Scan shape X" in k]
-    scany = [v for k,v in dmFile.allTags.items() if "4D STEM Tags.Scan shape Y" in k]
+    scanx = [v for k, v in dmFile.allTags.items() if "4D STEM Tags.Scan shape X" in k]
+    scany = [v for k, v in dmFile.allTags.items() if "4D STEM Tags.Scan shape Y" in k]
     if len(scanx) == 1 and len(scany) == 1:
         # TitanX tags found!
-        R_Nx = int(scany[0]) # need to flip x/y
+        R_Nx = int(scany[0])  # need to flip x/y
         R_Ny = int(scanx[0])
 
         if dc is not None:
-            dc.set_scan_shape(R_Nx,R_Ny)
-            dc.data = np.roll(dc.data,shift=-2,axis=1)
+            dc.set_scan_shape(R_Nx, R_Ny)
+            dc.data = np.roll(dc.data, shift=-2, axis=1)
         else:
             return R_Nx, R_Ny
 
 
-#def get_metadata_from_dmFile(fp):
+# def get_metadata_from_dmFile(fp):
 #    """ Accepts a filepath to a dm file and returns a Metadata instance
 #    """
 #    metadata = Metadata()
@@ -151,7 +144,3 @@ def _process_NCEM_TitanX_Tags(dmFile, dc=None):
 #        metadata.set_Q_pixel_size_units__microscope(pixelUnits[2])
 #
 #    return metadata
-
-
-
-
