57b225cdd614838e9e03b9d964b38b5a1100c741
Ben Savitzky <ben.savitzky@gmail.com>
2022-09-18 21:46:09 -0700

    updates path handling. Adds file descriptions

--- py4DSTEM/test/test_io/test_file_import.py
+++ py4DSTEM/test/test_io/test_file_import.py
@@ -1,17 +1,15 @@
 # Test non-native file loading with `py4DSTEM.import_file`
 
-from py4DSTEM import import_file
+
+import py4DSTEM
 from os.path import join
-from download_test_data import filepath as dirpath
 
 
 # Set filepaths
-dirpath = join(dirpath, 'unit_test_data')
-filepath_dm = join(dirpath, "dm_test_file.dm3")
-
+filepath_dm = join(py4DSTEM._TESTPATH, "dm_test_file.dm3")
 
 def test_import_dmfile():
-    data = import_file( filepath_dm )
-
+    data = py4DSTEM.import_file( filepath_dm )
+    #assert isinstance(data, py4DSTEM.io.DataCube) # currently fails because this dm file is 3D, so the data reads as an Array and not a DataCube
 
 
