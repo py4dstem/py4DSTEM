Show fix to compute clipvals after masking (#343)
* This commit changes clipval computation

This commit changes how the clipvals are computed when a mask is
passed into py4DSTEM.show. Previously it did not take into account
masked areas, so that if you masked off the central beam, its
intensity was still being used for computing vmin and vmax. So in
the situation where you called `py4DSTEM.show(ar, mask=mask)`, you
would see nothing since the vmax was still scaling to the central
beam's intensity. Now the mask that is passed in is combined with
the mask from the scaling operation before clipvals are computed,
which is a better default.

* This commit fixes the show.ipynb for v0.13.6

This commit fixes function definition changes in the `show.ipynb`
file to match v0.13.6 and changes the function definition in
`show_ellipses` to match what `add_ellipses` expects. This is only
changing the ellipse equation to accept (a,b,theta) instead of
(a,e,theta).

* tiny bugfix because you want the array when changing the mask to a boolean, not the py4DSTEM Array object

Co-authored-by: SE Zeltmann <steven.zeltmann@lbl.gov>
Former-commit-id: 561d46613555fbeacfa394a2bed64d269d6bb14f

py4DSTEM/visualize/show.py
@@ -365,8 +365,8 @@ def show(
     elif isinstance(ar,np.ma.masked_array):
         pass
     else:
-        mask = np.ones_like(ar,dtype=bool)
-        ar = np.ma.array(data=ar,mask=mask==False)
+        mask = np.zeros_like(ar,dtype=bool)
+        ar = np.ma.array(data=ar,mask=mask)
 
     # New intensity scaling logic
     assert scaling in ('none','full','log','power','hist')
@@ -408,8 +408,9 @@ def show(
     else:
         raise Exception
 
-    # init array
-    _ar = np.ma.array(data=_ar.data,mask=~_mask)
+    # Create the masked array applying the user mask (this is done before the 
+    # vmin and vmax are determined so the mask affects those)
+    _ar = np.ma.array(data=_ar.data,mask=np.logical_or(~_mask, ar.mask))
 
     # Set the clipvalues
     if intensity_range == 'manual':
@@ -455,9 +456,6 @@ def show(
         assert(isinstance(fig,Figure))
         assert(isinstance(ax,Axes))
 
-    # Create the masked array applying the user mask (this is done after the 
-    # vmin and vmax are determined so the mask doesn't affect those)
-    _ar = np.ma.array(data=_ar.data,mask=np.logical_or(ar.mask,~_mask))
 
     # Create colormap with mask_color for bad values
     cm = copy(plt.cm.get_cmap(cmap))
@@ -963,7 +961,7 @@ def show_circles(ar,center,R,color='r',fill=True,alpha=0.3,linewidth=2,returnfig
     else:
         return fig,ax
 
-def show_ellipses(ar,center,a,e,theta,color='r',fill=True,alpha=0.3,linewidth=2,
+def show_ellipses(ar,center,a,b,theta,color='r',fill=True,alpha=0.3,linewidth=2,
                                                         returnfig=False,**kwargs):
     """
     Visualization function which plots a 2D array with one or more overlayed ellipses.
@@ -992,7 +990,7 @@ def show_ellipses(ar,center,a,e,theta,color='r',fill=True,alpha=0.3,linewidth=2,
         further edited.
     """
     fig,ax = show(ar,returnfig=True,**kwargs)
-    d = {'center':center,'a':a,'e':e,'theta':theta,'color':color,'fill':fill,
+    d = {'center':center,'a':a,'b':b,'theta':theta,'color':color,'fill':fill,
          'alpha':alpha,'linewidth':linewidth}
     add_ellipses(ax,d)
 