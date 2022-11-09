831f22551858dea105265532d47fb9d54fbcae47
Colin <cophus@gmail.com>
2022-11-08 16:08:36 -0800

    more ptycho code
    Former-commit-id: 2ae7f4778cc1fc4ffd1a38b58e7484b409496a56

--- py4DSTEM/process/phase/reconstruction.py
+++ py4DSTEM/process/phase/reconstruction.py
@@ -3,6 +3,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from py4DSTEM.utils.tqdmnd import tqdmnd
+from py4DSTEM.process.utils import get_shifted_ar
 from py4DSTEM.process.calibration import fit_origin
 
 class Reconstruction:
@@ -32,9 +33,10 @@ class Reconstruction:
         kya,kxa = np.meshgrid(ky,kx)
 
         # init
-        self.com_meas_x = np.zeros(dataset.data.shape[0:2])
-        self.com_meas_y = np.zeros(dataset.data.shape[0:2])
-        self.int_total = np.zeros(dataset.data.shape[0:2])
+        self.shape_xy = np.array(dataset.data.shape[0:2])
+        self.com_meas_x = np.zeros(self.shape_xy)
+        self.com_meas_y = np.zeros(self.shape_xy)
+        self.int_total = np.zeros(self.shape_xy)
 
         # calculate the center of mass for all probe positions
         for rx, ry in tqdmnd(
@@ -199,60 +201,355 @@ class Reconstruction:
             else:
                 print('No diffraction transposed needed')
 
+    def dpc_recon(
+        self,
+        padding_factor = 2,
+        num_iterations = 64,
+        step_size = 0.9,
+        plot_result = True,
+        figsize = (8,8),
+        progress_bar = True,
+        ):
+        """
+        This function performs iterative DPC reconstruction.
+        """
+
+        # init
+        shape_xy_pad = np.round(self.shape_xy * padding_factor).astype('int')
+        dpc_phase_pad = np.zeros(shape_xy_pad)
+        mask = np.zeros(shape_xy_pad, dtype='bool')
+        mask[:self.shape_xy[0],:self.shape_xy[1]] = True
+        mask_inv = np.logical_not(mask)
+
+        # Fourier coordinates and operators
+        kx = np.fft.fftfreq(shape_xy_pad[0],d=1.0)
+        ky = np.fft.fftfreq(shape_xy_pad[1],d=1.0)
+        kya,kxa = np.meshgrid(ky,kx)
+        k_den = kxa**2 + kya**2
+        k_den[0,0] = np.inf
+        k_den = 1 / k_den
+        kx_op = -1j*0.25*kxa*k_den
+        ky_op = -1j*0.25*kya*k_den
+
+        # main loop
+        for a0 in tqdmnd(
+            num_iterations,
+            desc="Reconstructing phase",
+            unit=" iter",
+            disable=not progress_bar,
+            ):
+            # forward projection
+            dx = 0.5 * (np.roll(dpc_phase_pad,1,axis=0) - np.roll(dpc_phase_pad,-1,axis=0))
+            dy = 0.5 * (np.roll(dpc_phase_pad,1,axis=1) - np.roll(dpc_phase_pad,-1,axis=1))
 
 
-            # fig,ax = plt.subplots(figsize=figsize)
-            # ax.plot(
-            #     self.rotation_deg,
-            #     self.rotation_div,
-            #     )
-            # ax.plot(
-            #     self.rotation_deg,
-            #     self.rotation_div_transpose,
-            #     )
+            # difference from measurement
+            dx[mask] += self.com_x.ravel()
+            dy[mask] += self.com_y.ravel()
+            dx[mask_inv] = 0
+            dy[mask_inv] = 0
+
+            # back projection
+            phase_update = np.real(
+                np.fft.ifft2(
+                    np.fft.fft2(dx)*kx_op + np.fft.fft2(dy)*ky_op
+                )
+            )
+
+            # update
+            dpc_phase_pad += step_size * phase_update
 
 
+        # crop result
+        self.dpc_phase = dpc_phase_pad[:self.shape_xy[0],:self.shape_xy[1]]
 
 
+        # plotting
+        if plot_result:
+            fig,ax = plt.subplots(figsize=figsize)
+            ax.imshow(
+                self.dpc_phase,
+                )
+            plt.show()
 
-
-
-# class Ptychography:
-#     """
-#     A class which stores phase/complex reconstructions of object and probe waves.
-
-#     """
-
-
-#     def __init__(
-#         self,
+    def ptychography_init(
+        self,
+        dataset,
+        probe,
+        pixel_size_inv_Ang,
+        accel_voltage,
+        recon_size_Ang,
+        probe_defocus_Ang = 0,
+        probe_step_Ang = None,
+        probe_position_list_Ang = None,
+        rotation_deg = None,
+        bilinear = True,
+        normalization_min = 0.1,
+        plot_result = True,
+        figsize = (8,8),
+        progress_bar = True,
+        ):
+        """
+        This function initializes a ptychographic reconstruction. Currently it
+        assumes the rotation has been previously found using DPC curl minimization,
+        but can also be manually specified. The result plotted is the field of view,
+        showing the (padded) reconstruction space and probe positions.
         
-#     ):
-#         """
-#         Args:
-            
+        Args:
+            dataset: (DataCube)       Raw intensity measurements.
 
-#         """
+        """
+
+        # init
+        self.probe_defocus_Ang = probe_defocus_Ang
+        self.probe_C1_Ang = -1.0*probe_defocus_Ang
+
+
+        # Conversion of accel voltage to wavelength
+        self.accel_voltage = accel_voltage
+        m = 9.109383*1e-31
+        e = 1.602177*1e-19
+        c = 2.99792458*1e8
+        h = 6.62607*1e-34
+        self.wavelength = h / np.sqrt(2*m*e*accel_voltage) \
+            / np.sqrt(1 + e*accel_voltage/2/m/c**2) * 10**10 # wavelength in A
+
+        # override rotation with user-specified value
+        if rotation_deg is not None:
+            self.rotation_best_deg = rotation_deg
+            self.rotation_best_rad = np.deg2rad(rotation_deg)
+
+        # pixel sizes
+        self.pixel_size_inv_Ang = np.array(pixel_size_inv_Ang) * np.ones((2))
+        self.k_max = self.pixel_size_inv_Ang * dataset.shape[2:4] / 2.0
+        self.pixel_size_Ang = 1 / (2.0 * self.k_max)
+
+        # init field of view
+        self.recon_size_pixels = (np.round(np.array(recon_size_Ang) \
+            / self.pixel_size_Ang / 2.0)*2).astype('int')
+        self.recon_size_Ang = self.recon_size_pixels * self.pixel_size_Ang
+        self.recon_object = np.ones(self.recon_size_pixels, dtype='complex')
+        self.recon_update = np.ones(self.recon_size_pixels, dtype='complex')
+        # self.recon_int = np.ones(self.recon_size_pixels)
+
+        # initialize probe positions
+        x = np.arange(dataset.data.shape[0]) * probe_step_Ang
+        y = np.arange(dataset.data.shape[1]) * probe_step_Ang
+        x -= np.mean(x)
+        y -= np.mean(y)
+        ya0,xa0 = np.meshgrid(y,x)
+        xa =  np.cos(self.rotation_best_rad)*xa0 + np.sin(self.rotation_best_rad)*ya0
+        ya = -np.sin(self.rotation_best_rad)*xa0 + np.cos(self.rotation_best_rad)*ya0
+        self.ptycho_probe_x_Ang = xa.ravel() + self.recon_size_Ang[0]/2.0
+        self.ptycho_probe_y_Ang = ya.ravel() + self.recon_size_Ang[1]/2.0
+        self.ptycho_probe_x_pixels = self.ptycho_probe_x_Ang / self.pixel_size_Ang[0]
+        self.ptycho_probe_y_pixels = self.ptycho_probe_y_Ang / self.pixel_size_Ang[1]
+
+        # measurement and probe coordinate system in pixels
+        self.probe_size_pixels = probe.data.shape
+        self.x_ind = np.round(np.arange(self.probe_size_pixels[0]) 
+            - self.probe_size_pixels[0]/2).astype('int')
+        self.y_ind = np.round(np.arange(self.probe_size_pixels[1]) 
+            - self.probe_size_pixels[1]/2).astype('int')
+
+        # probe coordinate system in 1/pixels, operators
+        kx = np.fft.fftfreq(self.probe_size_pixels[0], d=1.0)
+        ky = np.fft.fftfreq(self.probe_size_pixels[1], d=1.0)
+        self.kya_pixels,self.kxa_pixels = np.meshgrid(ky,kx)
+        self.kx_shift = -2j*np.pi*self.kxa_pixels
+        self.ky_shift = -2j*np.pi*self.kya_pixels
+
+        # probe coordinate system in 1/Angstroms, operators
+        kx = np.fft.fftfreq(self.probe_size_pixels[0], d=self.pixel_size_Ang[0])
+        ky = np.fft.fftfreq(self.probe_size_pixels[1], d=self.pixel_size_Ang[1])
+        self.kya,self.kxa = np.meshgrid(ky,kx)
+        self.ka2 = self.kxa**2 + self.kya**2
+
+        # initial probe amplitude and aberrations
+        ya,xa = np.meshgrid(
+            np.arange(self.probe_size_pixels[1]),
+            np.arange(self.probe_size_pixels[0]),
+            )
+        int_total = np.sum(probe.data)
+        probe_x0 = np.sum(xa * probe.data) / int_total
+        probe_y0 = np.sum(ya * probe.data) / int_total
+        probe_shift = get_shifted_ar(
+            probe.data,
+            -probe_x0,
+            -probe_y0,
+            bilinear = bilinear,
+            )
+        self.probe_amp = np.sqrt(np.maximum(probe_shift,0))
+        self.probe = self.probe_amp * \
+            np.exp((-1j*np.pi*self.wavelength*self.ka2)*self.probe_C1_Ang)
+
+        # measured amplitude for all probe positions
+        self.amp_meas = np.zeros((
+            dataset.data.shape[0]*dataset.data.shape[1],
+            dataset.data.shape[2],
+            dataset.data.shape[3],
+            ))
+        for rx,ry in tqdmnd(
+            dataset.data.shape[0],
+            dataset.data.shape[1],
+            desc="initializing measurements",
+            unit=" probes",
+            disable=not progress_bar,
+            ):
+            meas = get_shifted_ar(
+                dataset.data[rx,ry],
+                -self.com_fit_x[rx,ry],
+                -self.com_fit_y[rx,ry],
+                bilinear = bilinear,
+            )
+            ind = np.ravel_multi_index((rx,ry), dataset.data.shape[0:2])
+            self.amp_meas[ind] = np.sqrt(np.maximum(meas,0))
+
+        # compute inital intensity normalization
+        # initial probes
+        x0 = np.round(self.ptycho_probe_x_pixels).astype('int')
+        y0 = np.round(self.ptycho_probe_y_pixels).astype('int')
+        dx = self.ptycho_probe_x_pixels - x0 + self.recon_size_pixels[0]/2
+        dy = self.ptycho_probe_y_pixels - y0 + self.recon_size_pixels[0]/2
+        psi_0 = np.fft.ifft2(
+            self.probe[None,:,:] * np.exp(
+            self.kx_shift[None,:,:] * dx[:,None,None] + \
+            self.ky_shift[None,:,:] * dy[:,None,None]))
+        # Write intensities into normalization array
+        self.recon_int = np.reshape(np.bincount(
+            (((y0[:,None,None] + self.y_ind[None,None,:]) %  self.recon_size_pixels[1]) + \
+            ((x0[:,None,None] + self.x_ind[None,:,None]) % self.recon_size_pixels[0]) * \
+            self.recon_size_pixels[1]).ravel(),
+            weights = np.abs(psi_0.ravel())**2,
+            minlength = np.prod(self.recon_size_pixels),
+        ),self.recon_size_pixels)
+        self.recon_int_norm = 1 / np.sqrt(self.recon_int**2 + \
+            (normalization_min*np.max(self.recon_int))**2)
+
+        # self.recon_int = np.reshape(np.bincount(
+        #     np.ravel_multi_index((
+        #         x,
+        #         y),
+        #         self.recon_size_pixels,
+        #     ),
+        #     # (((x0[:,None,None] + self.x_ind[None,:,None]) % self.recon_size_pixels[0]) + \
+        #     # (((y0[:,None,None] + self.y_ind[None,None,:])) %  self.recon_size_pixels[1]) * \
+        #     # self.recon_size_pixels[0]).ravel(),
+        #     weights = np.abs(psi_0.ravel())**2,
+        #     minlength = np.prod(self.recon_size_pixels),
+        # ),self.recon_size_pixels)
         
-#         # Initialize Crystal
-#         self.positions = np.asarray(positions)  #: fractional atomic coordinates
+
+        # test = np.ravel_multi_index(
+        #     (
+        #         x0[:,None,None] + self.x_ind[None,:,None],
+        #         y0[:,None,None] + self.x_ind[None,:,None],
+        #     ),
+        #     self.amp_meas.shape)
+
+        # x0[:,None,None] + \
+        #     self.x_ind[None,:,None] + \
+        #     self.y_ind[None,None,:]
+        
 
 
-#     # def get_amplitude(
-#     #     dataset,
-#     #     ):
-#     #     """
-#     #     Create a Crystal object from a CIF file, using pymatgen to import the CIF
+        # plotting
+        if plot_result:
+            fig,ax = plt.subplots(figsize=figsize)
+            ax.imshow(
+                self.recon_int,
+                # np.angle(self.recon_object),
+                cmap='gray',
+                )
+            ax.scatter(
+                self.ptycho_probe_y_pixels,
+                self.ptycho_probe_x_pixels,
+                s=5,
+                color=(1,0,0,1),
+                )
+            plt.show()
 
-#     #     Note that pymatgen typically prefers to return primitive unit cells,
-#     #     which can be overridden by setting conventional_standard_structure=True.
-
-#     #     Args:
-#     #         dataset: (DataCube)     A py4DSTEM DataCube
-
-#     #     """
-
-#     #     1+1
 
 
-# def 
+    def ptychography_recon(
+        self,
+        num_iterations = 1,
+        step_size = 0.95,
+        plot_result = True,
+        figsize = (8,8),
+        progress_bar = True,
+        ):
+        """
+        This function performs ptychographic reconstruction.
+        
+        Args:
+            num_iterations: (int)       Number of full iterations to perform.
+
+        """
+
+        # init
+        # probes_grad = np.zeros(self.amp_meas.shape,dtype='complex')
+
+        # main loop
+        for iter in tqdmnd(
+            num_iterations,
+            desc="reconstructing... ",
+            unit=" iterations",
+            disable=not progress_bar,
+            ):
+            # indexing
+            # ind_2D = 
+            # ind_2D = np.reshape(np.bincount(
+            #     (((y0[:,None,None] + self.y_ind[None,None,:]) %  self.recon_size_pixels[1]) + \
+            #     ((x0[:,None,None] + self.x_ind[None,:,None]) % self.recon_size_pixels[0]) * \
+            #     self.recon_size_pixels[1]).ravel(),
+
+
+            # forward projection
+            x0 = np.round(self.ptycho_probe_x_pixels).astype('int')
+            y0 = np.round(self.ptycho_probe_y_pixels).astype('int')
+            dx = self.ptycho_probe_x_pixels - x0 + self.recon_size_pixels[0]/2
+            dy = self.ptycho_probe_y_pixels - y0 + self.recon_size_pixels[0]/2
+            psi_0 = np.fft.ifft2(
+                self.probe[None,:,:] * np.exp(
+                self.kx_shift[None,:,:] * dx[:,None,None] + \
+                self.ky_shift[None,:,:] * dy[:,None,None]))
+            Psi = np.fft.fft2(psi_0 * self.recon_object[
+                (x0[:,None,None] + self.x_ind[None,:,None]) %  self.recon_size_pixels[0],
+                (y0[:,None,None] + self.y_ind[None,None,:]) %  self.recon_size_pixels[1],
+            ])
+
+            # gradient from difference
+            Psi = np.conj(psi_0) * np.fft.ifft2((
+                self.amp_meas - np.abs(Psi)) * np.exp(1j*np.angle(Psi)))
+
+            # back projection
+            grad = np.reshape(np.bincount(
+                (((y0[:,None,None] + self.y_ind[None,None,:]) %  self.recon_size_pixels[1]) + \
+                ((x0[:,None,None] + self.x_ind[None,:,None]) % self.recon_size_pixels[0]) * \
+                self.recon_size_pixels[1]).ravel(),
+                weights = Psi.ravel(),
+                minlength = np.prod(self.recon_size_pixels),
+            ),self.recon_size_pixels) * self.recon_int_norm.astype('complex')
+
+
+
+            # update
+
+
+
+
+        # plotting
+        if plot_result:
+            fig,ax = plt.subplots(figsize=figsize)
+            ax.imshow(
+                np.angle(grad),
+                # np.angle(self.recon_object),
+                cmap='gray',
+                )
+            plt.show()
+
+
+
+
+
