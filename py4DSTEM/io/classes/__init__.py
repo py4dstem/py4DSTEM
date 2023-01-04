caab4f615193ff0400728445c5ff2005f3d804f3
bsavitzky <ben.savitzky@gmail.com>
2023-01-04 18:30:06 -0500

    rm io/classes/emd, moving contents to io/classes

--- py4DSTEM/io/classes/__init__.py
+++ py4DSTEM/io/classes/__init__.py
@@ -1,3 +1,12 @@
-from py4DSTEM.io.classes.emd import *
+# Base (emd) class imports
+from py4DSTEM.io.classes.tree import Tree
+from py4DSTEM.io.classes.root import Root
+from py4DSTEM.io.classes.metadata import Metadata
+from py4DSTEM.io.classes.array import Array
+from py4DSTEM.io.classes.pointlist import PointList
+from py4DSTEM.io.classes.pointlistarray import PointListArray
+#from py4DSTEM.io.classes.io import *
+
+# Child (package) class imports
 from py4DSTEM.io.classes.py4dstem import *
 
