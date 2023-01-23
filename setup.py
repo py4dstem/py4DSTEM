7416034e30432c5f850f267728a7337b8fb83f00
bsavitzky <ben.savitzky@gmail.com>
2023-01-23 16:29:01 -0500

    removes PyQt5 and pyqtgraph dependencies, removes gui
    Former-commit-id: 10e11495de72fc2a41a4305d3c1a97762447eca6

--- setup.py
+++ setup.py
@@ -30,8 +30,6 @@ setup(
         'matplotlib >= 3.2.2',
         'scikit-image >= 0.17.2',
         'scikit-learn >= 0.23.2',
-        'PyQt5 >= 5.10',
-        'pyqtgraph >= 0.11',
         'qtconsole >= 4.7.7',
         'ipywidgets >= 7.6.3',
         'tqdm >= 4.46.1',
