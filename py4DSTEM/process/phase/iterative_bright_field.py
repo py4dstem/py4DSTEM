599a589ce89054fcd50e29a407967842d36a9fea
Colin <cophus@gmail.com>
2023-01-30 15:21:33 -0800

    Aberration measurement and correction
    Former-commit-id: 1a1c77943803902c5670f4bbbd17753499f97e1d

--- py4DSTEM/process/phase/iterative_bright_field.py
+++ py4DSTEM/process/phase/iterative_bright_field.py
@@ -4,12 +4,15 @@ Module for reconstructing virtual bright field images by aligning each virtual B
 
 import matplotlib.pyplot as plt
 import numpy as np
+import scipy as sp
 from scipy.special import comb
+from scipy.ndimage import gaussian_filter
 
 from py4DSTEM.utils.tqdmnd import tqdmnd
 from py4DSTEM.process.utils import get_shifted_ar, get_shift
 from py4DSTEM.process.utils.get_maxima_2D import get_maxima_2D
 from py4DSTEM.process.utils.multicorr import upsampled_correlation
+from py4DSTEM.process.utils.utils import electron_wavelength_angstrom
 
 
 class BFreconstruction():
@@ -347,6 +350,192 @@ class BFreconstruction():
         kr_max = np.max(self.kr)
         ax.set_xlim([-1.2*kr_max, 1.2*kr_max])
         ax.set_ylim([-1.2*kr_max, 1.2*kr_max])
+
+
+
+    def aberration_fit(
+        self,
+        accelerating_voltage_kV = 300e3,
+        print_result = True,
+        plot_CTF_compare = False,
+        plot_dk = 0.001,
+        plot_k_sigma = 0.002,
+        ):
+        '''
+        Fit aberrations to the measured image shifts.
+        '''
+
+        # Convert real space shifts to Angstroms
+        self.xy_shifts_Ang = self.xy_shifts * self.calibration.get_R_pixel_size()
+
+        # Convert diffraction space coordinates to angles
+        self.accelerating_voltage_kV = accelerating_voltage_kV
+        self.wavelength = electron_wavelength_angstrom(self.accelerating_voltage_kV)
+        self.probe_angles = self.kxy * self.wavelength
+
+        # Solve affine transformation
+        m = np.linalg.lstsq(self.probe_angles, self.xy_shifts_Ang, rcond=None)[0]        
+        m_rotation, m_aberration = sp.linalg.polar(m, side='right')
+        # m_test = m_rotation @ m_aberration
+
+        # Convert into rotation and aberration coefficients
+        self.rotation_Q_to_R_rads = np.arctan2(m_rotation[1,0],m_rotation[0,0])
+        self.aberration_C1  = (m_aberration[0,0] + m_aberration[1,1]) / 4.0 
+        self.aberration_A1x = (m_aberration[0,0] - m_aberration[1,1]) / 4.0 # factor /2 for A1 astigmatism? /4?
+        self.aberration_A1y = (m_aberration[1,0] + m_aberration[0,1]) / 4.0
+
+        # Print results
+        if print_result:
+            print('Rotation of Q w.r.t. R = ' \
+                + str(np.round(np.rad2deg(self.rotation_Q_to_R_rads),decimals=3)) \
+                + ' deg')
+            print('Astigmatism (A1x,A1y)  = (' \
+                + str(np.round(self.aberration_A1x,decimals=0)) \
+                + ','
+                + str(np.round(self.aberration_A1y,decimals=0)) \
+                + ') Ang')
+            print('Defocus C1             = ' \
+                + str(np.round(self.aberration_C1,decimals=0)) \
+                + ' Ang')
+
+
+        # Plot the CTF comparison between experiment and fit
+        if plot_CTF_compare:
+            # Get polar mean from FFT of BF reconstruction
+            im_fft = np.abs(np.fft.fft2(self.recon_BF))
+
+            # coordinates
+            kx = np.fft.fftfreq(self.recon_BF.shape[0], self.calibration.get_R_pixel_size())
+            ky = np.fft.fftfreq(self.recon_BF.shape[1], self.calibration.get_R_pixel_size())
+            kra = np.sqrt(kx[:,None]**2 + ky[None,:]**2)
+            k_max = np.max(kra) / np.sqrt(2.0)
+            k_num_bins = np.ceil(k_max / plot_dk).astype('int')
+            k_bins = np.arange(k_num_bins+1) * plot_dk
+
+            # histogram
+            k_ind = kra / plot_dk
+            kf = np.floor(k_ind).astype('int')
+            dk = k_ind - kf
+            sub = kf <= k_num_bins
+            hist_exp = np.bincount(
+                kf[sub], 
+                weights = im_fft[sub] * (1-dk[sub]),
+                minlength = k_num_bins)
+            hist_norm = np.bincount(
+                kf[sub], 
+                weights = (1-dk[sub]),
+                minlength = k_num_bins)
+            sub = kf <= k_num_bins - 1
+            hist_exp += np.bincount(
+                kf[sub] + 1, 
+                weights = im_fft[sub] * ( dk[sub]),
+                minlength = k_num_bins)
+            hist_norm += np.bincount(
+                kf[sub] + 1, 
+                weights = (  dk[sub]),
+                minlength = k_num_bins)
+
+            # KDE and normalizing
+            k_sigma = plot_dk / plot_k_sigma
+            hist_exp[0] = 0.0
+            hist_exp = gaussian_filter(hist_exp, sigma = k_sigma, mode='nearest')
+            hist_norm = gaussian_filter(hist_norm, sigma = k_sigma, mode='nearest')
+            hist_exp /= hist_norm
+
+            # CTF comparison
+            CTF_fit = np.sin((-np.pi * self.wavelength * self.aberration_C1) * k_bins**2)
+
+            # plotting
+            hist_plot = hist_exp * k_bins
+            hist_plot /= np.max(hist_plot)
+            # ind = np.argmax(hist_exp * k_bins**0.7)
+            # hist_plot = hist_exp / hist_exp[ind]
+
+            # CTF_plot = np.abs(CTF_fit) * k_bins
+            # CTF_plot /= np.max(CTF_plot)
+
+
+            fig,ax = plt.subplots(figsize = (8,4))
+            ax.fill_between(
+                k_bins,
+                hist_plot,
+                color=(0.7,0.7,0.7,1),
+                )
+            ax.plot(
+                k_bins,
+                np.clip(CTF_fit, 0.0, np.inf),
+                color=(1,0,0,1),
+                linewidth=2,
+                )
+            ax.plot(
+                k_bins,
+                np.clip(-CTF_fit, 0.0, np.inf),
+                color=(0,0.5,1,1),
+                linewidth=2,
+                )
+            ax.set_xlim([0, k_bins[-1]])
+            ax.set_ylim([0, 1.05])
+
+
+
+    def aberration_correct_simple(
+        self,
+        k_info_limit = None,
+        k_info_power = 2.0,
+        plot_result = True,
+        plot_range = (-2,2),
+        returnval = False,
+        ):
+        '''
+        CTF correction of the BF image using the measured defocus aberration.
+        '''
+
+        # Fourier coordinates, aberration surface
+        kx = np.fft.fftfreq(self.recon_BF.shape[0], self.calibration.get_R_pixel_size())
+        ky = np.fft.fftfreq(self.recon_BF.shape[1], self.calibration.get_R_pixel_size())
+        kra2 = kx[:,None]**2 + ky[None,:]**2
+        CTF = np.sin((-np.pi*self.wavelength*self.aberration_C1) * kra2)
+        CTF_corr = np.sign(CTF)
+
+        # if needed, make low pass filter
+        if k_info_limit is not None:
+            k_filt = 1/(1 + (kra2**k_info_power)/((k_info_limit)**(2*k_info_power)))
+
+        # apply correction to reconstructed BF image
+        if k_info_limit is not None:
+            self.recon_BF_corr = np.real(np.fft.ifft2(np.fft.fft2(self.recon_BF) * CTF_corr * k_filt))
+        else:
+            self.recon_BF_corr = np.real(np.fft.ifft2(np.fft.fft2(self.recon_BF) * CTF_corr))
+
+        # sub = self.kxy[:,0] < -0.0
+
+
+        # im_test = np.mean(self.stack_BF[sub,:,:],axis=0)
+
+
+        # plotting
+        if plot_result:
+            im_plot = self.recon_BF_corr.copy()
+            im_plot -= np.mean(im_plot)
+            im_plot /= np.sqrt(np.mean(im_plot**2))
+
+
+            fig,ax = plt.subplots(figsize = (10,10))
+            ax.imshow(
+                im_plot[200:400,200:400],
+                vmin=plot_range[0],
+                vmax=plot_range[1],
+                cmap = 'gray',
+                )
+
+
+
+        # np.abs(np.fft.fftshift(CTF)),
+
+
+        # print(np.mean(sub))
+
+
 
 
 def align_images(
