9ec3638ef387e9e2406432700dc3ea3a35d49988
bsavitzky <ben.savitzky@gmail.com>
2023-02-03 16:13:33 -0500

    rm Branch from __init__

--- py4DSTEM/emd/classes/__init__.py
+++ py4DSTEM/emd/classes/__init__.py
@@ -1,7 +1,7 @@
 # Class imports
 
 from py4DSTEM.emd.classes.metadata import Metadata
-from py4DSTEM.emd.classes.tree import Node,Root,Branch,RootedNode
+from py4DSTEM.emd.classes.tree import Node,Root,RootedNode
 from py4DSTEM.emd.classes.array import Array
 from py4DSTEM.emd.classes.pointlist import PointList
 from py4DSTEM.emd.classes.pointlistarray import PointListArray
