82d40f6ee257f231810158a2a04ddc6f93eb067d
bsavitzky <ben.savitzky@gmail.com>
2023-02-06 14:31:51 -0500

    updates

---
+++ py4DSTEM/test/test_classes/test_crystal.py
@@ -0,0 +1,41 @@
+import numpy as np
+from py4DSTEM.classes import (
+    Crystal
+)
+
+
+
+
+class TestCrystal:
+
+    def setup_cls(self):
+        pass
+
+    def teardown_cls(self):
+        pass
+
+    def setup_method(self):
+        pass
+
+    def teardown_method(self):
+        pass
+
+
+
+    def test_Crystal():
+
+        #crystal = Crystal( **args )
+        #assert(isinstance(crystal,Crystal))
+
+        pass
+
+
+    def test_Crystal2():
+
+        #crystal = Crystal( **args )
+        #assert(isinstance(crystal,Crystal))
+
+        pass
+
+
+
