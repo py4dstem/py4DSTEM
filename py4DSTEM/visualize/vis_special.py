f648e45911eb5ab35bee2f4651da16cd48bd4224
Stephanie Ribet <stephanie.ribet@gmail.com>
2023-02-25 14:55:59 -0800

    scalebar True and vis grid complex
    Former-commit-id: 9eb0be6552f042d19323718a05e5d4640a6a6562

--- py4DSTEM/visualize/vis_special.py
+++ py4DSTEM/visualize/vis_special.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib.colors import hsv_to_rgb
 from matplotlib.patches import Wedge
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 from py4DSTEM.io.datastructure import Calibration, DataCube, PointList
@@ -801,127 +802,157 @@ def show_selected_dps(datacube,positions,im,bragg_pos=None,
                     get_pointcolors=lambda i:colors[i],
                     **kwargs)
 
+def Complex2HSV(complex_array, vmin, vmax, hue_start=90):
+    """
+    Function to turn a complex array into rgb for plotting
+    """
+    amp = np.abs(complex_array)
+    amp = np.where(amp < vmin, vmin, amp)
+    amp = np.where(amp > vmax, vmax, amp)
+
+    ph = np.angle(complex_array, deg=1) + hue_start
+
+    h = (ph % 360) / 360
+    s = 0.85 * np.ones_like(h)
+    v = (amp - vmin) / (vmax - vmin)
+
+    hsv = np.dstack((h, s, v))
+    if hsv.shape[-1] != 3:
+        hsv = hsv.reshape(hsv.shape[0], hsv.shape[1], 3, hsv.shape[2] // 3)
+        hsv = hsv.swapaxes(-1, -2)
+
+    return hsv_to_rgb(hsv)
+
+
 def show_complex(
     ar_complex,
-    vmin = None,
-    vmax = None,
-    cbar = True,
-    scalebar = None,
-    pixelunits = 'pixels',
-    pixelsize = 1,
-    returnfig = False,
+    vmin=None,
+    vmax=None,
+    cbar=True,
+    scalebar=False,
+    pixelunits="pixels",
+    pixelsize=1,
+    returnfig=False,
     **kwargs
-    ):
-    '''
+):
+    """
     Function to plot complex arrays
-    
-    Args: 
-        ar_complex (2d array)   : complex array to be plotted
-        vmin (float, optional)  : minimum absolute value 
-        vmax (float, optional)  : maximum absolute value 
-        vmin/vmax are set to fractions of the distribution of pixel values in the array, e.g. 
+
+    Args:
+        ar_complex (2d array)   : complex array to be plotted. If ar_complex is list of complex arrarys
+            such as [array1, array2], then arrays are horizonally plotted in one figure
+        vmin (float, optional)  : minimum absolute value
+        vmax (float, optional)  : maximum absolute value
+        vmin/vmax are set to fractions of the distribution of pixel values in the array, e.g.
         vmin=0.02 will set the minumum display value to saturate the lower 2% of pixels
         cbar (bool)             : if True, include color wheel
-        scalebar (boo)          : if True, adds scale bar
+        scalebar (bool)         : if True, adds scale bar
         pixelunits (str)        : units for scalebar
         pixelsize (float)       : size of one pixel in pixelunits for scalebar
         returnfig (bool)        : if True, the function returns the tuple (figure,axis)
-        
+
     Returns:
         if returnfig==False (default), the figure is plotted and nothing is returned.
         if returnfig==True, return the figure and the axis.
-    '''
+    """
 
-    #define min and max
-    amp = np.abs(ar_complex)  
+    # define min and max
+    amp = np.abs(ar_complex)
     if np.max(amp) == np.min(amp):
-        if vmin is None: vmin = 0 
-        if vmax is None: vmax = np.max(amp)
+        if vmin is None:
+            vmin = 0
+        if vmax is None:
+            vmax = np.max(amp)
     else:
-        if vmin is None: vmin = 0.02
-        if vmax is None: vmax = 0.98
+        if vmin is None:
+            vmin = 0.02
+        if vmax is None:
+            vmax = 0.98
         vals = np.sort(amp[~np.isnan(amp)])
-        ind_vmin = np.round((vals.shape[0]-1)*vmin).astype('int')
-        ind_vmax = np.round((vals.shape[0]-1)*vmax).astype('int')
-        ind_vmin = np.max([0,ind_vmin])
-        ind_vmax = np.min([len(vals)-1,ind_vmax])
+        ind_vmin = np.round((vals.shape[0] - 1) * vmin).astype("int")
+        ind_vmax = np.round((vals.shape[0] - 1) * vmax).astype("int")
+        ind_vmin = np.max([0, ind_vmin])
+        ind_vmax = np.min([len(vals) - 1, ind_vmax])
         vmin = vals[ind_vmin]
         vmax = vals[ind_vmax]
 
-    from matplotlib.colors import hsv_to_rgb
-
-    #function for converting to complex colors
-    def Complex2HSV(z, vmin, vmax, hue_start=90):
-        #based on stack overflow 17044052
-        amp = np.abs(z)
-        amp = np.where(amp < vmin, vmin, amp)
-        amp = np.where(amp > vmax, vmax, amp)
-        
-        ph = np.angle(z, deg=1) + hue_start
-        
-        h = (ph % 360) / 360
-        s = 0.85 * np.ones_like(h)
-        v = (amp -vmin) / (vmax - vmin)
-        
-        return hsv_to_rgb(np.dstack((h,s,v)))
-
-    #convert to complex colors
+    # convert to complex colors
     rgb = Complex2HSV(ar_complex, vmin, vmax)
-    
-    #plot
-    fig, ax = show(
-        rgb,
-        vmin = 0, 
-        vmax = 1,
-        intensity_range= 'absolute',
-        returnfig = True,
-        **kwargs
-    )
-    
-    if scalebar is not None and scalebar is not False:
-        scalebar = {
-        'Nx':ar_complex.shape[0],
-        'Ny':ar_complex.shape[1],
-        'pixelsize':pixelsize,
-        'pixelunits':pixelunits,
-        }
-        
-        add_scalebar(ax,scalebar)
 
-    #add color bar
+    # plot
+
+    if len(rgb.shape) > 3:
+        from py4DSTEM.visualize import show_image_grid
+
+        fig, ax = show_image_grid(
+            get_ar=lambda i: rgb[i],
+            H=1,
+            W=rgb.shape[0],
+            vmin=0,
+            vmax=1,
+            intensity_range="absolute",
+            returnfig=True,
+            **kwargs,
+        )
+        if scalebar is True:
+            scalebar = {
+                "Nx": ar_complex[0].shape[0],
+                "Ny": ar_complex[0].shape[1],
+                "pixelsize": pixelsize,
+                "pixelunits": pixelunits,
+            }
+
+            add_scalebar(ax[0, 0], scalebar)
+    else:
+        fig, ax = show(
+            rgb, vmin=0, vmax=1, intensity_range="absolute", returnfig=True, **kwargs
+        )
+
+        if scalebar is True:
+            scalebar = {
+                "Nx": ar_complex.shape[0],
+                "Ny": ar_complex.shape[1],
+                "pixelsize": pixelsize,
+                "pixelunits": pixelunits,
+            }
+
+            add_scalebar(ax, scalebar)
+
+    # add color bar
     if cbar == True:
-        ax0 = fig.add_axes([1, 0.35, 0.3, 0.3])
-        
-        #create wheel
+        if len(rgb.shape) > 2:
+            ax0 = fig.add_axes([1, 0.35, 0.3, 0.3])
+        else:
+            ax0 = fig.add_axes([1, 0.35, 0.3, 0.3])
+
+        # create wheel
         AA = 1000
         kx = np.fft.fftshift(np.fft.fftfreq(AA))
         ky = np.fft.fftshift(np.fft.fftfreq(AA))
-        kya,kxa = np.meshgrid(ky,kx)
-        kra = (kya**2+kxa**2)**0.5
-        ktheta = np.arctan2(-kxa,kya)
-        ktheta = kra*np.exp(1j*ktheta)
-        
-        #convert to hsv
+        kya, kxa = np.meshgrid(ky, kx)
+        kra = (kya**2 + kxa**2) ** 0.5
+        ktheta = np.arctan2(-kxa, kya)
+        ktheta = kra * np.exp(1j * ktheta)
+
+        # convert to hsv
         rgb = Complex2HSV(ktheta, 0, 0.4)
         ind = kra > 0.4
-        rgb[ind] = [1,1,1]
-        
-        #plot
-        ax0.imshow(
-            rgb
-        )
+        rgb[ind] = [1, 1, 1]
 
-        #add axes
-        ax0.axhline(AA/2, 0, AA, color = 'k')
-        ax0.axvline(AA/2, 0, AA, color = 'k')
-        ax0.axis('off')
+        # plot
+        ax0.imshow(rgb)
+
+        # add axes
+        ax0.axhline(AA / 2, 0, AA, color="k")
+        ax0.axvline(AA / 2, 0, AA, color="k")
+        ax0.axis("off")
 
         label_size = 16
 
-        ax0.text(AA, AA/2, 1, fontsize = label_size)
-        ax0.text(AA/2, 0, 'i', fontsize = label_size)
-        ax0.text(AA/2, AA, '-i', fontsize = label_size)
-        ax0.text(0, AA/2, -1, fontsize = label_size)
+        ax0.text(AA, AA / 2, 1, fontsize=label_size)
+        ax0.text(AA / 2, 0, "i", fontsize=label_size)
+        ax0.text(AA / 2, AA, "-i", fontsize=label_size)
+        ax0.text(0, AA / 2, -1, fontsize=label_size)
 
-    if returnfig == True: 
+    if returnfig == True:
         return fig, ax
