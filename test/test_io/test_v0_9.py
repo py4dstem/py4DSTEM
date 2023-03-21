87792cd67579dcb04003c57cd3eba9d8fdd24722
bsavitzky <ben.savitzky@gmail.com>
2023-03-21 17:27:41 -0400

    updates

--- test/test_io/test_v0_9.py
+++ test/test_io/test_v0_9.py
@@ -1,10 +1,15 @@
-from py4DSTEM import read, _TESTPATH
+from py4DSTEM import read, DataCube, _TESTPATH
 from os.path import join
 
 path = join(_TESTPATH, 'simulatedAuNanoplatelet_binned_v0_9.h5')
 
-def test_read_v0_9():
+def test_read_v0_9_noID():
 
     d = read(path)
     d
+def test_read_v0_9_withID():
+
+    d = read(path, data_id="polyAu_4DSTEM")
+    assert(isinstance(d,DataCube))
+
 
