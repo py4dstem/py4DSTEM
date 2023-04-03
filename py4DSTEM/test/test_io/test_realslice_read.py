76b9f59691fba9e2fc0ae3c38296042e48dd9bd4
bsavitzky <ben.savitzky@gmail.com>
2023-04-03 15:38:56 -0400

    updates
    Former-commit-id: f5523657b5a319069f5c10d7290aa89e1cd9712f

--- py4DSTEM/test/test_io/test_realslice_read.py
+++ py4DSTEM/test/test_io/test_realslice_read.py
@@ -1,4 +1,4 @@
-# Test non-native file loading with `py4DSTEM.import_file`
+# Test reading realslices in v13
 
 
 import py4DSTEM
@@ -6,11 +6,11 @@ from os.path import join
 
 
 # Set filepaths
-filepath_dm = join(py4DSTEM._TESTPATH, "dm_test_file.dm3")
+filepath = join(py4DSTEM._TESTPATH, "YanAiming_bilayer_v01.h5")
 
-def test_import_dmfile():
-    data = py4DSTEM.import_file( filepath_dm )
-    assert isinstance(data, py4DSTEM.io.DataCube) # currently fails because this dm file is 3D, so the data reads as an Array and not a DataCube
+def test_read_realslice():
+    realslice = py4DSTEM.read(filepath, root='4DSTEM/Fit Data')
+
 
 
 
