f139604b51e193af8c965c48db562c4e494d2e43
bsavitzky <ben.savitzky@gmail.com>
2023-04-21 03:21:54 -0400

    updates

--- py4DSTEM/process/diskdetection/braggvectors.py
+++ py4DSTEM/process/diskdetection/braggvectors.py
@@ -4,14 +4,10 @@ from typing import Optional,Union
 import numpy as np
 from os.path import basename
 
-from emdfile import (
-    Custom,
-    PointListArray,
-    Metadata
-)
-
 from py4DSTEM.classes import Data
 from py4DSTEM.process.diskdetection.braggvector_methods import BraggVectorMethods
+from emdfile import Custom,PointListArray,Metadata
+
 
 
 
@@ -29,27 +25,27 @@ class BraggVectors(Custom,BraggVectorMethods,Data):
 
         >>> braggvectors.cal[ scan_x, scan_y ]
 
-    to automatically choose which calibrations to perform based on the contents
-    of the instance's `calibrations` property, or, to manually select the
-    calibrations to perform, using an initial call of
+    To set which calibrations are being applied, call
 
-        >>> braggvectors.cal(
+        >>> braggvectors.setcal(
         >>>     center = bool,
         >>>     ellipse = bool,
         >>>     pixel = bool
         >>> )
 
-    before subsequently calling `braggvectors.cal[posx,posy]`. The calibrations
-    performed for the last call to `braggvectors.cal` are accessible at
+    If .setcal is not called, calibrations will be automatically selected based
+    based on the contents of the instance's `calibrations` property. The
+    calibrations performed in the last call to `braggvectors.cal` are exposed as
 
-        >>> braggvectors.vector_calibrations
+        >>> braggvectors.calstate
 
-    or at `braggvectors.vcal`.
-
-    The vectors themselves can be accesses as
+    After grabbing some vectors
 
         >>> vects = braggvectors.raw[ scan_x,scan_y ]
-        >>> vects.qx,vects.qy,vects.I                  # or
+
+    the values themselves are accessible as
+
+        >>> vects.qx,vects.qy,vects.I
         >>> vects['qx'],vects['qy'],vects['intensity']
 
     """
@@ -76,27 +72,135 @@ class BraggVectors(Custom,BraggVectorMethods,Data):
             name = '_v_uncal'
         )
 
-    # vector properties
+        # calibrated vector getters
+        # nested list of 2**3 vector getters for [center][ellipse][pixel]-like access
+        # where entries 0,1 correspond to False,True values of the params
+        self._cal_vector_getters = [[[
+            None for cen in range(2)] for ell in range(2)] for pix in range(2)]
+        self._calstate = None
 
     @property
-    def vectors(self):
-        try:
-            return self._v_cal
-        except AttributeError:
-            er = "No calibrated bragg vectors found. Try running .calibrate()!"
-            raise Exception(er)
+    def calstate(self):
+        return self._calstate
+
+
+
+    # raw vectors
+
     @property
-    def vectors_uncal(self):
-        return self._v_uncal
+    def raw(self):
+        """
+        Calling
 
-    # aliases
-    v = vectors
-    v_uncal = vectors_uncal
+            >>> raw[ scan_x, scan_y ]
+
+        returns those bragg vectors.
+        """
+
+        # check if a raw vector getter exists
+        # if it doesn't, make it
+        if not hasattr(self,'_raw_vector_getter'):
+            self._raw_vector_getter = RawVectorGetter(
+                data = self._v_uncal
+            )
+
+        # use the vector getter to grab the vector
+        return self._raw_vector_getter
+
+
+
+    # calibrated vectors
+
+    @property
+    def cal(self):
+        """
+        Calling
+
+            >>> cal[ scan_x, scan_y ]
+
+        retrieves data.  Use `.setcal` to set the calibrations to be applied, or
+        `.calstate` to see which calibrations are currently set.  Requesting data
+        before setting calibrations will automatically select values based
+        calibrations that are available.
+        """
+
+        # check if a calibration state is set
+        # if not, autoselect calibrations and make a getter
+        if self.calstate is None:
+            self.setcal()
+
+        # retrieve the getter and return
+        getter = self._cal_vector_getters[
+            self.calstate['center']][
+            self.calstate['ellipse']][
+            self.calstate['pixel']]
+        return getter
+
+
+    # set calibration state
+
+    def setcal(
+        self,
+        center = None,
+        ellipse = None,
+        pixel = None
+    ):
+        """
+        Calling
+
+            >>> braggvectors.setcal(
+            >>>     center = bool,
+            >>>     ellipse = bool,
+            >>>     pixel = bool
+            >>> )
+
+        sets the calibrations that will be applied to vectors subsequently
+        retrieved with
+
+            >>> braggvectors.cal[ scan_x, scan_y ]
+
+        Any arguments left as `None` will be automatically set based on
+        the calibration measurements available.
+        """
+
+        # autodetect
+        if center is None:
+            # TODO center = ...
+            pass
+        if ellipse is None:
+            # TODO
+            pass
+        if pixel is None:
+            # TODO
+            pass
+
+        # make the requested vector getter
+        # and set the calstate
+        transform = get_transform(  # TODO - write this.....!
+            calibration = self.calibration,
+            center = center,
+            ellipse = ellipse,
+            pixel = pixel
+        )
+        calstate = {
+            "center" : center,
+            "ellipse" : ellipse,
+            "pixel" : pixel
+        }
+        getter = CaliVectorGetter(
+            data = self._v_uncal,
+            transform = transform,
+            calstate = calstate
+        )
+        self._cal_vector_getters[center][ellipse][pixel] = getter
+        self._calstate = calstate
+
+        pass
 
 
 
 
-    # methods
+    # copy
 
     def copy(self, name=None):
         name = name if name is not None else self.name+"_copy"
@@ -112,9 +216,6 @@ class BraggVectors(Custom,BraggVectorMethods,Data):
 
         return braggvector_copy
 
-
-
-
     # write
 
     def to_h5(self,group):
@@ -126,9 +227,6 @@ class BraggVectors(Custom,BraggVectorMethods,Data):
         md['Qshape'] = self.Qshape
         self.metadata = md
         grp = Custom.to_h5(self,group)
-
-
-
 
     # read
 
@@ -158,8 +256,6 @@ class BraggVectors(Custom,BraggVectorMethods,Data):
         if '_v_cal' in dic.keys():
             self._v_cal = dic['_v_cal']
 
-
-
     # standard output display
 
     def __repr__(self):
@@ -168,5 +264,75 @@ class BraggVectors(Custom,BraggVectorMethods,Data):
         string = f"{self.__class__.__name__}( "
         string += f"A {self.shape}-shaped array of lists of bragg vectors )"
         return string
+
+
+
+
+
+# Support classes
+
+
+class RawVectorGetter(
+    def __init__(
+        self,
+        data
+    ):
+        self._data = _data
+
+    def __getitem__(self,pos):
+        x,y = pos
+        ans = self._data[x,y]
+        return BVects(ans)
+
+
+class CaliVectorGetter:
+    def __init__(
+        self,
+        data,
+        transform,
+        calstate
+    ):
+        self._data = data
+        self._transform = transform
+        self.calstate = calstate
+
+    def __getitem__(self,pos):
+        x,y = pos
+        ans = self._data[x,y]
+        ans = self._transform(ans)
+        return BVects(ans)
+
+
+class BVects:
+    """
+    Enables
+
+        >>> v.qx,v.qy,v.I
+
+    -like access to a collection of Bragg vector.
+    """
+
+    def __init__(
+        self,
+        pointlist
+        ):
+        """ pointlist must have fields 'qx', 'qy', and 'intensity'
+        """
+        assert(isinstance(pointlist,PointList))
+        self._data = poinstlist.data
+
+    @property
+    def qx(self):
+        return self._data['qx']
+
+    @property
+    def qy(self):
+        return self._data['qy']
+
+    @property
+    def I(self):
+        return self._data['intensity']
+
+
 
 
