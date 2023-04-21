339356becb7004998f4f445f85f170d46f508034
bsavitzky <ben.savitzky@gmail.com>
2023-04-20 22:02:42 -0400

    cleans
    Former-commit-id: 227b25197dafffa36f3f5de3dd58e696b8c84258

---
+++ test/test_braggvectors.py
@@ -0,0 +1,103 @@
+import numpy as np
+#from py4DSTEM.classes import (
+#    Crystal
+#)
+
+
+
+
+class TestCrystal:
+
+    def setup_cls(self):
+        pass
+
+    def teardown_cls(self):
+        pass
+
+    def setup_method(self):
+        pass
+
+    def teardown_method(self):
+        pass
+
+
+
+    def test_Crystal(self):
+
+        #crystal = Crystal( **args )
+        #assert(isinstance(crystal,Crystal))
+
+        pass
+
+
+
+
+
+import py4DSTEM
+from os.path import join
+from numpy import zeros
+
+# set filepath
+path = join(py4DSTEM._TESTPATH,"simulatedAuNanoplatelet_binned_v0_9.h5")
+
+
+
+class TestDiskDetectionBasic:
+
+    # setup/teardown
+    def setup_class(cls):
+
+        # Read sim Au datacube
+        datacube = py4DSTEM.io.read(
+            path,
+            data_id = 'polyAu_4DSTEM'
+        )
+        cls.datacube = datacube
+
+        # prepare a probe
+        mask = zeros(datacube.Rshape,dtype=bool)
+        mask[28:33,14:19] = 1
+        probe = datacube.get_vacuum_probe( ROI=mask )
+        alpha_pr,qx0_pr,qy0_pr = py4DSTEM.process.probe.get_probe_size( probe.probe )
+        probe.get_kernel(
+            mode='sigmoid',
+            origin=(qx0_pr,qy0_pr),
+            radii=(alpha_pr,2*alpha_pr)
+        )
+        cls.probe = probe
+
+        # Set disk detection parameters
+        cls.detect_params = {
+            'corrPower': 1.0,
+            'sigma': 0,
+            'edgeBoundary': 2,
+            'minRelativeIntensity': 0,
+            'minAbsoluteIntensity': 8,
+            'minPeakSpacing': 4,
+            'subpixel' : 'poly',
+            'maxNumPeaks': 1000,
+        #     'CUDA': True,
+        }
+
+
+    # tests
+
+    def test_disk_detection_selected_positions(self):
+
+        rxs = 36,15,11,59,32,34
+        rys = 9,15,31,39,20,68,
+
+        disks_selected = self.datacube.find_Bragg_disks(
+            data = (rxs, rys),
+            template = self.probe.kernel,
+            **self.detect_params,
+        )
+
+    def test_disk_detection(self):
+
+        braggpeaks = self.datacube.find_Bragg_disks(
+            template = self.probe.kernel,
+            **self.detect_params,
+        )
+
+
