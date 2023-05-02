f75404c46f01ca75f5e50a6f08dfd25dd2c7ddaf
Georgios Varnavides <gvarnavides@berkeley.edu>
2023-05-01 20:42:53 -0700

    added magnetic OT
    Former-commit-id: 0fccb714238fd381b692ec21d077544ff4c9831f

--- py4DSTEM/process/phase/__init__.py
+++ py4DSTEM/process/phase/__init__.py
@@ -1,6 +1,7 @@
 from py4DSTEM.process.phase.iterative_dpc import DPCReconstruction
 from py4DSTEM.process.phase.iterative_mixedstate_ptychography import MixedStatePtychographicReconstruction
 from py4DSTEM.process.phase.iterative_multislice_ptychography import MultislicePtychographicReconstruction
+from py4DSTEM.process.phase.iterative_overlap_magnetic_tomography import OverlapMagneticTomographicReconstruction
 from py4DSTEM.process.phase.iterative_overlap_tomography import OverlapTomographicReconstruction
 from py4DSTEM.process.phase.iterative_parallax import ParallaxReconstruction
 from py4DSTEM.process.phase.iterative_ptychography import PtychographicReconstruction
