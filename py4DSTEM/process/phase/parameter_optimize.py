d92098a9d1b3991510d99af0dfadf87870ad4898
Steve Zeltmann <steven.zeltmann@berkeley.edu>
2023-05-19 14:51:52 -0700

    add documentation and format
    Former-commit-id: 1840466e43730278a136f38cf72ab9298c006c87

--- py4DSTEM/process/phase/parameter_optimize.py
+++ py4DSTEM/process/phase/parameter_optimize.py
@@ -20,20 +20,48 @@ from py4DSTEM.process.phase.utils import AffineTransform
 
 class PtychographyOptimizer:
     """
-    doot doot
+    Optimize ptychographic hyperparameters with Bayesian Optimization of a
+    Gaussian process. Any of the arguments to the ptychographic
+    init-preprocess-reconstruct pipeline can be optimized over (including
+    boolean or other flag options)
     """
 
     def __init__(
         self,
         reconstruction_type: type[PhaseReconstruction],
         init_args: dict,
-        affine_args:dict,
+        affine_args: dict,
         preprocess_args: dict,
         reconstruction_args: dict,
     ):
         """
         Parameter optimization for ptychographic reconstruction based on Bayesian Optimization
         with Gaussian Process.
+
+        Usage
+        -----
+        Dictionaries of the arguments to __init__, AffineTransform (for distorting the initial
+        scan positions), preprocess, and reconstruct are required. For parameters not optimized
+        over, the value in the dictionary is used. To optimize a parameter, instead pass an
+        OptimizationParameter object inside the dictionary to specify the bounds, initial guess,
+        and type of parameter. Calling optimize will then run the optimization simultaneously
+        over all optimization parameters. To obtain the optimized parameters, call get_optimized_arguments
+        to return a set of dictionaries where the OptimizationParameter objects have been replaced
+        with the optimal values. These can then be modified for running a full reconstruction.
+
+        Parameters
+        ----------
+        reconstruction_type: class
+            Type of ptychographic reconstruction to perform
+        init_args: dict
+            Keyword arguments passed to the __init__ method of the reconstruction class
+        affine_args: dict
+            Keyword arguments passed to AffineTransform. The transform is applied to the initial
+            scan positions.
+        preprocess_args: dict
+            Keyword arguments passed to the preprocess method the reconstruction object
+        reconstruction_args: dict
+            Keyword arguments passed to the reconstruct method the the reconstruction object
         """
 
         # loop over each argument dictionary and split into static and optimization variables
@@ -79,7 +107,22 @@ class PtychographyOptimizer:
         self,
         n_calls: int,
         n_initial_points: int,
+        **skopt_kwargs: dict,
     ):
+        """
+        Run optimizer
+
+        Parameters
+        ----------
+        n_calls: int
+            Number of times to run ptychographic reconstruction
+        n_initial_points: int
+            Number of uniformly spaced trial points to test before
+            beginning Bayesian optimization (must be less than n_calls)
+        skopt_kwargs: dict
+            Additional arguments to be passed to skopt.gp_minimize
+
+        """
         self._optimization_function = self._get_optimization_function(
             self._reconstruction_type,
             self._parameter_list,
@@ -96,9 +139,10 @@ class PtychographyOptimizer:
 
         # Make a progress bar
         pbar = tqdm(total=n_calls, desc="Optimizing parameters")
+
         # We need to wrap the callback because if it returns a value
         # the optimizer breaks its loop
-        def callback(*args,**kwargs):
+        def callback(*args, **kwargs):
             pbar.update(1)
 
         self._skopt_result = gp_minimize(
@@ -108,6 +152,7 @@ class PtychographyOptimizer:
             n_initial_points=n_initial_points,
             x0=self._x0,
             callback=callback,
+            **skopt_kwargs,
         )
 
         print("Optimized parameters:")
@@ -126,6 +171,20 @@ class PtychographyOptimizer:
         objective=True,
         evaluations=False,
     ):
+        """
+        Visualize optimization results
+
+        Parameters
+        ----------
+        gp_model: bool
+            Display fitted Gaussian process model (only available for 1-dimensional problem)
+        convergence: bool
+            Display convergence history
+        objective: bool
+            Display GP objective function and partial dependence plots
+        evaluations: bool
+            Display histograms of sampled points
+        """
         if (len(self._parameter_list) == 1) and gp_model:
             plot_gaussian_process(self._skopt_result)
             plt.show()
@@ -141,15 +200,29 @@ class PtychographyOptimizer:
         return self
 
     def get_optimized_arguments(self):
+        """
+        Get argument dictionaries containing optimized hyperparameters
+
+        Returns
+        -------
+        init_opt, prep_opt, reco_opt: dicts
+            Dictionaries of arguments to __init__, preprocess, and reconstruct
+            where the OptimizationParameter items have been replaced with the optimal
+            values obtained from the optimizer
+        """
         init_opt = self._replace_optimized_parameters(
             self._init_args, self._parameter_list
         )
         affine_opt = self._replace_optimized_parameters(
             self._affine_args, self._parameter_list
         )
-        affine_transform = partial(AffineTransform, **self._affine_static_args)(**affine_opt)
-        scan_positions = self._get_scan_positions(affine_transform, init_opt['datacube'])
-        init_opt['initial_scan_positions'] = scan_positions
+        affine_transform = partial(AffineTransform, **self._affine_static_args)(
+            **affine_opt
+        )
+        scan_positions = self._get_scan_positions(
+            affine_transform, init_opt["datacube"]
+        )
+        init_opt["initial_scan_positions"] = scan_positions
 
         prep_opt = self._replace_optimized_parameters(
             self._preprocess_args, self._parameter_list
@@ -186,9 +259,12 @@ class PtychographyOptimizer:
 
     def _get_scan_positions(self, affine_transform, dataset):
         R_pixel_size = dataset.calibration.get_R_pixel_size()
-        x,y = np.arange(dataset.R_Nx)*R_pixel_size, np.arange(dataset.R_Ny)*R_pixel_size
+        x, y = (
+            np.arange(dataset.R_Nx) * R_pixel_size,
+            np.arange(dataset.R_Ny) * R_pixel_size,
+        )
         x, y = np.meshgrid(x, y, indexing="ij")
-        scan_positions = np.stack((x.ravel(),y.ravel()),axis=1)
+        scan_positions = np.stack((x.ravel(), y.ravel()), axis=1)
         scan_positions = scan_positions @ affine_transform.asarray()
         return scan_positions
 
@@ -198,7 +274,7 @@ class PtychographyOptimizer:
         parameter_list: list,
         n_iterations: int,
         init_static_args: dict,
-        affine_static_args:dict,
+        affine_static_args: dict,
         preprocess_static_args: dict,
         reconstruct_static_args: dict,
         init_optimization_params: dict,
@@ -240,10 +316,10 @@ class PtychographyOptimizer:
 
             # Create affine transform object
             tr = affine(**afft_args)
-            # Apply affine transform to pixel grid, using the 
+            # Apply affine transform to pixel grid, using the
             # calibrations lifted from the dataset
-            dataset = init_static_args['datacube']
-            init_args['initial_scan_positions'] = self._get_scan_positions(tr, dataset)
+            dataset = init_static_args["datacube"]
+            init_args["initial_scan_positions"] = self._get_scan_positions(tr, dataset)
 
             ptycho = obj(**init_args)
             prep(ptycho, **prep_args)
@@ -274,13 +350,29 @@ class OptimizationParameter:
 
     def __init__(
         self,
-        lower_bound: Union[float, int, bool],
-        upper_bound: Union[float, int, bool],
         initial_value: Union[float, int, bool],
+        lower_bound: Union[float, int, bool] = None,
+        upper_bound: Union[float, int, bool] = None,
         scaling: str = "uniform",
         space: str = "real",
         categories: list = [],
     ):
+        """
+        Wrapper for scikit-optimize Space objects used as inputs to PtychographyOptimizer
+
+        Parameters
+        ----------
+        initial_value:
+            Initial value, used for first evaluation in optimizer
+        lower_bound, upper_bound:
+            Bounds on real or integer variables (not needed for bool or categorical)
+        scaling: str
+            Prior knowledge on sensitivity of the variable. Can be 'uniform' or 'log-uniform'
+        space: str
+            Type of variable. Can be 'real', 'integer', 'bool', or 'categorical'
+        categories: list
+            List of options for Categorical parameter
+        """
         # Check input
         space = space.lower()
         assert space in (
