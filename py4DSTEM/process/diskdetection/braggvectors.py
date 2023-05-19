bfab592e4de817a786dcede2e7197a29a6183bbe
Ben Savitzky <ben.savitzky@gmail.com>
2023-05-18 22:49:50 -0700

    updates braggvectors internals
    Former-commit-id: 89bd6cfe0e620d68be136dfcaa02750d78becbe5

--- py4DSTEM/process/diskdetection/braggvectors.py
+++ py4DSTEM/process/diskdetection/braggvectors.py
@@ -59,7 +59,7 @@ class BraggVectors(Custom,BraggVectorMethods,Data):
         self.Rshape = Rshape
         self.shape = self.Rshape
         self.Qshape = Qshape
-        self.verbose = verbose 
+        self.verbose = verbose
 
         self._v_uncal = PointListArray(
             dtype = [
@@ -86,7 +86,7 @@ class BraggVectors(Custom,BraggVectorMethods,Data):
         return self._calstate
     def _set_raw_vector_getter(self):
         self._raw_vector_getter = RawVectorGetter(
-            data = self._v_uncal
+            braggvects = self
         )
     def _set_cal_vector_getter(self):
         self._cal_vector_getter = CalibratedVectorGetter(
@@ -318,7 +318,7 @@ class BVects:
 class RawVectorGetter:
     def __init__(
         self,
-        data
+        braggvects,
     ):
         self._data = data
 
