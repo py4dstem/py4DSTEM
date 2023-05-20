3ee482d5c03a60b1e8f78e4fde6fa53981d4c7d9
Ben Savitzky <ben.savitzky@gmail.com>
2023-05-19 22:33:24 -0700

    refactors

--- py4DSTEM/process/polar/polar_datacube.py
+++ py4DSTEM/process/polar/polar_datacube.py
@@ -53,10 +53,11 @@ class PolarDatacube:
         # setup sampling
 
         # polar
+        self._qscale = qscale
         if qmax is None:
             qmax = np.min(self._datacube.Qshape) / np.sqrt(2)
-        self.set_radial_bins(qmin,qmax,qstep)
         self.set_annular_bins(n_annular)
+        self.set_radial_bins(qmin,qmax,qstep)
 
         # cartesian
         self._xa,self._ya = np.meshgrid(
@@ -64,9 +65,6 @@ class PolarDatacube:
             np.arange(self._datacube.Q_Ny),
             indexing = 'ij'
         )
-
-        # scaling data by q**power
-        self.qscale = qscale
 
         # mask
         self.mask = mask
@@ -93,6 +91,7 @@ class PolarDatacube:
             self._qstep
         )
         self.set_polar_shape()
+        self.qscale = self._qscale
 
     @property
     def qmin(self):
@@ -164,6 +163,17 @@ class PolarDatacube:
             self.polar_size = np.prod(self.polar_shape)
 
 
+    # scaling property
+    @property
+    def qscale(self):
+        return self._qscale
+    @qscale.setter
+    def qscale(self,x):
+        self._qscale = x
+        if x is not None:
+            self._qscale_ar = np.arange(self.polar_shape[1])**x
+
+
     # expose raw data
     @property
     def data_raw(self):
@@ -180,20 +190,6 @@ class PolarDatacube:
             polarcube = self
         )
 
-    # expose transformation
-    @property
-    def transform(self):
-        return self._polar_data_getter._transform
-
-    # scaling property
-    @property
-    def qscale(self):
-        return self._qscale
-    @qscale.setter
-    def qscale(self,x):
-        self._qscale = x
-        if x is not None:
-            self._qscale_ar = np.arange(self.polar_shape[1])**x
 
     # mask properties
     @property
@@ -212,6 +208,33 @@ class PolarDatacube:
     @property
     def mask_polar(self):
         return self._mask_polar
+
+
+    # expose transformation
+    @property
+    def transform(self):
+        """
+        Return a transformed copy of the diffraction pattern `cartesian_data`.
+
+        Parameters
+        ----------
+        cartesian_data : array
+            The data
+        origin : tuple or list or None
+            Variable behavior depending on the arg type. Length 2 tuples uses these
+            values directly. Length 2 list of ints uses the calibrated origin value
+            at this scan position. None uses the calibrated mean origin.
+        mask : boolean array or None
+            A mask applied to the data before transformation.  The value of masked
+            pixels (0's) in the output is determined by `returnval`. Note that this
+            mask is applied in combination with any mask at PolarData.mask.
+        returnval : 'masked' or 'nan' or None
+            Controls the returned data. 'masked' returns a numpy masked array. 'nan'
+            returns a normal numpy array with masked pixels set to np.nan.  None
+            returns a 2-tuple of numpy arrays - the transformed data with masked
+            pixels set to 0, and the transformed mask.
+        """
+        return self._polar_data_getter._transform
 
 
     def __repr__(self):
@@ -238,60 +261,108 @@ class PolarDataGetter:
         x,y = pos
         # get the data
         cartesian_data = self._polarcube._datacube[x,y]
-        # find the origin
-        origin = self._get_origin(
-            (x,y),
-            self._polarcube.calibration
-        )
-        # apply the transform
+        # transform
         ans = self._transform(
-            cartesian_data = cartesian_data,
-            origin = origin
+            cartesian_data,
+            origin = [x,y],
+            returnval = 'masked'
         )
-        # get a normalization array
-        norm = self._transform(
-            np.ones_like(cartesian_data),
-            origin = origin,
-        )
-        # normalize
-        ans = np.divide(ans, norm, out=np.zeros_like(ans), where=norm!=0)
-        # scaling
-        if self._polarcube.qscale is not None:
-            ans *= self._polarcube._qscale_ar[np.newaxis,:]
-        # return...
-        if self._polarcube.mask is None:
-            return ans
-        # ...or mask and return
-        else:
-            # TODO
-            #mask = self._transform(
-
-            #)
-            pass
-            return ans
-
-    def _get_origin(
-        self,
-        pos,
-        cal
-        ):
-        x,y = pos
-        assert(cal.get_origin(x,y) is not None), "No center found! Try setting the origin."
-        origin = cal.get_origin(x,y)
-        return origin
+        # return
+        return ans
 
 
     def _transform(
         self,
         cartesian_data,
         origin = None,
+        mask = None,
+        returnval = 'masked',
         ):
         """
         Return a transformed copy of the diffraction pattern `cartesian_data`.
+
+        Parameters
+        ----------
+        cartesian_data : array
+            The data
+        origin : tuple or list or None
+            Variable behavior depending on the arg type. Length 2 tuples uses these
+            values directly. Length 2 list of ints uses the calibrated origin value
+            at this scan position. None uses the calibrated mean origin.
+        mask : boolean array or None
+            A mask applied to the data before transformation.  The value of masked
+            pixels (0's) in the output is determined by `returnval`. Note that this
+            mask is applied in combination with any mask at PolarData.mask.
+        returnval : 'masked' or 'nan' or None
+            Controls the returned data. 'masked' returns a numpy masked array. 'nan'
+            returns a normal numpy array with masked pixels set to np.nan.  None
+            returns a 2-tuple of numpy arrays - the transformed data with masked
+            pixels set to 0, and the transformed mask.
         """
         # get calibrations
         if origin is None:
             origin = self._polarcube.calibration.get_origin_mean()
+        elif isinstance(origin,list):
+            origin = self._polarcube.calibration.get_origin(origin[0],origin[1])
+        elif isinstance(origin,tuple):
+            pass
+        else:
+            raise Exception(f"Invalid type for `origin`, {type(origin)}")
+
+        # combine passed mask with default mask
+        mask0 = self._polarcube.mask
+        if mask is None and mask0 is None:
+            mask = np.ones_like(cartesian_data)
+        elif mask is None:
+            mask = mask0
+        elif mask0 is None:
+            mask = mask
+        else:
+            mask = mask*mask0
+
+        # transform data
+        ans = self._transform_array(
+            cartesian_data,
+            origin
+        )
+
+        # transform mask
+        norm = self._transform_array(
+            mask,
+            origin,
+        )
+
+        # normalize
+        out = np.empty_like(norm)
+        out[:] = np.nan
+        ans = np.divide(ans, norm, out=out, where=norm!=0)
+
+        # scaling
+        if self._polarcube.qscale is not None:
+            ans *= self._polarcube._qscale_ar[np.newaxis,:]
+
+        # return
+        if returnval == 'masked':
+            ans = np.ma.array(
+                data = ans,
+                mask = np.isnan(ans)
+            )
+            return ans
+        elif returnval == 'nan':
+            return ans
+        elif returnval is None:
+            ans[np.isnan(ans)] = 0
+            return (ans,norm)
+        else:
+            raise Exception(f"Unexpected value {returnval} encountered for `returnval`")
+
+
+
+    def _transform_array(
+        self,
+        data,
+        origin
+        ):
 
         # shifted coordinates
         x = self._polarcube._xa - origin[0]
@@ -311,32 +382,35 @@ class PolarDataGetter:
         im = np.bincount(
             r_ind_floor[sub] + \
             np.mod(t_ind_floor[sub],self._polarcube.polar_shape[0]) * self._polarcube.polar_shape[1],
-            weights = cartesian_data[sub] * (1 - dr[sub]) * (1 - dt[sub]),
+            weights = data[sub] * (1 - dr[sub]) * (1 - dt[sub]),
             minlength = self._polarcube.polar_size,
         )
         im += np.bincount(
             r_ind_floor[sub] + \
             np.mod(t_ind_floor[sub] + 1,self._polarcube.polar_shape[0]) * self._polarcube.polar_shape[1],
-            weights = cartesian_data[sub] * (1 - dr[sub]) * (    dt[sub]),
+            weights = data[sub] * (1 - dr[sub]) * (    dt[sub]),
             minlength = self._polarcube.polar_size,
         )
         sub = np.logical_and(r_ind_floor >= -1, r_ind_floor < self._polarcube.polar_shape[1]-1)
         im += np.bincount(
             r_ind_floor[sub] + 1 + \
             np.mod(t_ind_floor[sub],self._polarcube.polar_shape[0]) * self._polarcube.polar_shape[1],
-            weights = cartesian_data[sub] * (    dr[sub]) * (1 - dt[sub]),
+            weights = data[sub] * (    dr[sub]) * (1 - dt[sub]),
             minlength = self._polarcube.polar_size,
         )
         im += np.bincount(
             r_ind_floor[sub] + 1 + \
             np.mod(t_ind_floor[sub] + 1,self._polarcube.polar_shape[0]) * self._polarcube.polar_shape[1],
-            weights = cartesian_data[sub] * (    dr[sub]) * (    dt[sub]),
+            weights = data[sub] * (    dr[sub]) * (    dt[sub]),
             minlength = self._polarcube.polar_size,
         )
 
         # output
         ans = np.reshape(im, self._polarcube.polar_shape)
         return ans
+
+
+
 
     def __repr__(self):
         space = ' '*len(self.__class__.__name__)+'  '
