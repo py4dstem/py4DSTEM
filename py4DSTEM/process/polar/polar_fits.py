79657016f94f94fc15da5ed017c990c8ca161666
cophus <cophus@gmail.com>
2023-06-05 14:23:50 -0700

    Updating ellipse fitting functions
     split the elliptic fitting / plotting into 2 functions

--- py4DSTEM/process/polar/polar_fits.py
+++ py4DSTEM/process/polar/polar_fits.py
@@ -16,13 +16,11 @@ def fit_amorphous_ring(
     plot_log_scale = True,
     plot_int_scale = (-3,3),
     figsize = (8,8),
-    return_all_coefs = False,
+    return_all_coefs = True,
     ):
     """
     Fit an amorphous halo with a two-sided Gaussian model, plus a background 
     Gaussian function.
-
-    TODO - move these functions to another 
 
     Parameters
     --------
@@ -46,6 +44,7 @@ def fit_amorphous_ring(
         Figure size for plots
     return_all_coefs: bool
         Set to True to return the 11 parameter fit, rather than the 5 parameter ellipse
+    
     Returns
     --------
     params_ellipse: np.array
@@ -160,72 +159,157 @@ def fit_amorphous_ring(
         print('t  = ' + str(np.round(np.rad2deg(coefs[4]),3)) + ' deg')
 
     if plot_result and not show_fit_mask:
-        # Generate resulting best fit image
-        im_fit = np.reshape(amorphous_model(
-            np.vstack((xa.ravel(),ya.ravel())),
-            coefs),im.shape)
-
-        # plots
-        phi = np.linspace(0,2*np.pi,360)
-        cp = np.cos(phi)
-        sp = np.sin(phi)
-
-        fig,ax = plt.subplots(figsize=figsize)
-        ax.imshow(
-            im,
-            vmin = 0,
-            vmax = np.max(im_fit[mask]),
-            cmap = 'gray',
+        plot_amorphous_ring(
+            im = im,
+            coefs = coefs,
+            radial_range = radial_range,
+            plot_log_scale = plot_log_scale,
+            plot_int_scale = plot_int_scale,
+            figsize = figsize,
             )
-
-        x0 = coefs[0]
-        y0 = coefs[1]
-        a = coefs[2]
-        b = coefs[3]
-        t = coefs[4]
-        s1 = coefs[9]
-        s2 = coefs[10]
-
-        ax.plot(
-            y0 + np.array((-1,1))*a*np.sin(t),
-            x0 + np.array((-1,1))*a*np.cos(t),
-            c = 'r',
-            ) 
-        ax.plot(
-            y0 + np.array((-1,1))*b*np.cos(t),
-            x0 + np.array((1,-1))*b*np.sin(t),
-            c = 'r',
-            linestyle = 'dashed',
-            )        
-
-        ax.plot(
-            y0 + a*np.sin(t)*cp + b*np.cos(t)*sp,
-            x0 + a*np.cos(t)*cp - b*np.sin(t)*sp,
-            c = 'r',
-            )
-        scale = 1 - s1 / a
-        ax.plot(
-            y0 + scale*a*np.sin(t)*cp + scale*b*np.cos(t)*sp,
-            x0 + scale*a*np.cos(t)*cp - scale*b*np.sin(t)*sp,
-            c = 'r',
-            linestyle='dashed',
-            )
-        scale = 1 + s2 / a
-        ax.plot(
-            y0 + scale*a*np.sin(t)*cp + scale*b*np.cos(t)*sp,
-            x0 + scale*a*np.cos(t)*cp - scale*b*np.sin(t)*sp,
-            c = 'r',
-            linestyle='dashed',
-            )
-        ax.set_xlim((0,im.shape[1]-1))
-        ax.set_ylim((im.shape[0]-1,0))
-
 
     # Return fit parameters
     if return_all_coefs:
         return coefs
     else:
         return coefs[0:5]
+
+
+def plot_amorphous_ring(
+    im,
+    coefs,
+    radial_range = (0,np.inf),
+    plot_log_scale = True,
+    plot_int_scale = (-3,3),
+    figsize = (8,8),
+    ):
+    """
+    Fit an amorphous halo with a two-sided Gaussian model, plus a background 
+    Gaussian function.
+
+    Parameters
+    --------
+    im: np.array
+        2D image array to perform fitting on
+    coefs: np.array
+        all fitting coefficients
+    plot_log_scale: bool
+        Plot logarithmic image intensities
+    plot_int_scale: tuple of 2 values
+        Min and max plotting range in standard deviations of image intensity
+    figsize: tuple, list, np.array (optional)
+        Figure size for plots
+    return_all_coefs: bool
+        Set to True to return the 11 parameter fit, rather than the 5 parameter ellipse
+    
+    Returns
+    --------
+
+    """
+
+    # get needed coefs
+    center = coefs[0:2]
+
+    # coordinates
+    xa,ya = np.meshgrid(
+        np.arange(im.shape[0]),
+        np.arange(im.shape[1]),
+        indexing = 'ij',
+        )
+
+    # Make fitting mask
+    ra2 = (xa - center[0])**2 + (ya - center[1])**2
+    mask = np.logical_and(
+        ra2 >= radial_range[0]**2,
+        ra2 <= radial_range[1]**2,
+        )
+    vals = im[mask]
+    basis = np.vstack((xa[mask],ya[mask]))
+
+    # Generate resulting best fit image
+    im_fit = np.reshape(amorphous_model(
+        np.vstack((xa.ravel(),ya.ravel())),
+        coefs),im.shape)
+
+    # plotting arrays
+    phi = np.linspace(0,2*np.pi,360)
+    cp = np.cos(phi)
+    sp = np.sin(phi)
+
+    # plotting intensity range
+    if plot_log_scale:
+        int_med = np.median(np.log(vals))
+        int_std = np.sqrt(np.median((np.log(vals) - int_med)**2))
+        int_range = (
+            int_med + plot_int_scale[0]*int_std, 
+            int_med + plot_int_scale[1]*int_std)
+        im_plot = np.tile(np.clip(
+            (np.log(im[:,:,None]) - int_range[0]) / (int_range[1] - int_range[0]),
+            0,1),(1,1,3))
+    else:
+        int_med = np.median(vals)
+        int_std = np.sqrt(np.median((vals - int_med)**2))
+        int_range = (
+            int_med + plot_int_scale[0]*int_std, 
+            int_med + plot_int_scale[1]*int_std)
+        im_plot = np.clip(
+            (im[:,:,None] - int_range[0]) / (int_range[1] - int_range[0]),
+            0,1)
+    # vals_mean = np.mean(vals)
+    # vals_std = np.std(vals)
+    # vmin = vals_mean - 
+
+    # plotting
+    fig,ax = plt.subplots(figsize=figsize)
+    ax.imshow(
+        im_plot,
+        vmin = 0,
+        vmax = 1,
+        cmap = 'gray',
+        )
+
+    x0 = coefs[0]
+    y0 = coefs[1]
+    a = coefs[2]
+    b = coefs[3]
+    t = coefs[4]
+    s1 = coefs[9]
+    s2 = coefs[10]
+
+    ax.plot(
+        y0 + np.array((-1,1))*a*np.sin(t),
+        x0 + np.array((-1,1))*a*np.cos(t),
+        c = 'r',
+        ) 
+    ax.plot(
+        y0 + np.array((-1,1))*b*np.cos(t),
+        x0 + np.array((1,-1))*b*np.sin(t),
+        c = 'r',
+        linestyle = 'dashed',
+        )        
+
+    ax.plot(
+        y0 + a*np.sin(t)*cp + b*np.cos(t)*sp,
+        x0 + a*np.cos(t)*cp - b*np.sin(t)*sp,
+        c = 'r',
+        )
+    scale = 1 - s1 / a
+    ax.plot(
+        y0 + scale*a*np.sin(t)*cp + scale*b*np.cos(t)*sp,
+        x0 + scale*a*np.cos(t)*cp - scale*b*np.sin(t)*sp,
+        c = 'r',
+        linestyle='dashed',
+        )
+    scale = 1 + s2 / a
+    ax.plot(
+        y0 + scale*a*np.sin(t)*cp + scale*b*np.cos(t)*sp,
+        x0 + scale*a*np.cos(t)*cp - scale*b*np.sin(t)*sp,
+        c = 'r',
+        linestyle='dashed',
+        )
+    ax.set_xlim((0,im.shape[1]-1))
+    ax.set_ylim((im.shape[0]-1,0))
+
 
 
 def amorphous_model(basis, *coefs):
