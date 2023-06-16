4db3121f4254fd45eb79a1e2920b4ff6894bc60c
Stephanie Ribet <stephanie.ribet@gmail.com>
2023-06-16 11:17:42 -0700

    bug fixes and including in importfile

--- py4DSTEM/io/filereaders/read_arina.py
+++ py4DSTEM/io/filereaders/read_arina.py
@@ -1,17 +1,34 @@
 import h5py
+import hdf5plugin
 import numpy as np
 from py4DSTEM.classes import DataCube
+from py4DSTEM.preprocess.utils import bin2D
 
-def read_arina(filename, scan_width):
+
+def read_arina(
+    filename,
+    scan_width,
+    mem="RAM",
+    binfactor:int=1,
+):
 
     """
+    File reader for arina 4D-STEM datasets
     Args:
-        filename: str or Path Path to the file
+        filename: str with path to master file
         scan_width: x dimension of scan
+        mem (str):  Must be "RAM" or "MEMMAP". Specifies how the data is
+            loaded; "RAM" transfer the data from storage to RAM, while "MEMMAP"
+            leaves the data in storage and creates a memory map which points to
+            the diffraction patterns, allowing them to be retrieved individually
+            from storage.
+        binfactor (int): Diffraction space binning factor for bin-on-load.
 
     Returns:
-            DataCube
+        DataCube
     """
+    assert mem == "RAM", "ARNIA does not support memory mapping"
+
     f = h5py.File(filename, "r")
     nimages = 0
 
@@ -21,7 +38,10 @@ def read_arina(filename, scan_width):
         height = f["entry"]["data"][dset].shape[1]
         width = f["entry"]["data"][dset].shape[2]
         dtype = f["entry"]["data"][dset].dtype
-
+    
+    width = width//binfactor
+    height = height//binfactor
+    
     assert (
         nimages % scan_width < 1e-6
     ), "scan_width must be integer multiple of x*y size"
@@ -36,7 +56,7 @@ def read_arina(filename, scan_width):
 
     for dset in f["entry"]["data"]:
         image_index = _processDataSet(
-            f["entry"]["data"][dset], image_index, array_3D, scan_width
+            f["entry"]["data"][dset], image_index, array_3D, binfactor
         )
 
     if f.__bool__():
@@ -53,13 +73,15 @@ def read_arina(filename, scan_width):
     return datacube
 
 
-def _processDataSet(dset, start_index, array_3D, scan_width):
+def _processDataSet(dset, start_index, array_3D, binfactor):
     image_index = start_index
     nimages_dset = dset.shape[0]
 
     for i in range(nimages_dset):
-        array_3D[image_index] = dset[i].astype(array_3D.dtype)
-
+        if binfactor == 1:
+            array_3D[image_index] = dset[i].astype(array_3D.dtype)
+        else: 
+            array_3D[image_index] = bin2D(dset[i].astype(array_3D.dtype), binfactor)
+        
         image_index = image_index + 1
-
     return image_index
