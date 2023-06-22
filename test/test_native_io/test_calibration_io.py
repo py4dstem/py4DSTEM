cf0ed9aaa3742dcc492945f8965cd4099ce1e396
bsavitzky <ben.savitzky@gmail.com>
2023-06-22 11:48:22 -0400

    restructures test/
    Former-commit-id: 547dddef9cf742bd4c2573ab5c74d625965fd6a1

---
+++ test/test_native_io/test_calibration_io.py
@@ -0,0 +1,44 @@
+import py4DSTEM
+import numpy as np
+from os.path import join
+
+# set filepath
+path = join(py4DSTEM._TESTPATH, "filename")
+
+
+
+class TestCalibrationIO:
+
+
+
+    def test_datacube_cal_io(self):
+        # TODO
+        # make a datacube
+        # modify its calibration
+        # save
+        # load the datacube
+        # check its calibration
+        assert 0
+        pass
+
+
+    def test_datacube_child_node(self):
+        # TODO
+        # make a datacube
+        # make a child node
+        # confirm calibrations are the same
+        # modify the calibration
+        # save
+        # load the datacube
+        # check its calibration
+        # load just the child node
+        # check its calibration
+        assert 0
+        pass
+
+
+
+
+
+
+
