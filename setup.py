586506a7135c5fa07d0b3eb2f069a2acfa09718d
bsavitzky <ben.savitzky@gmail.com>
2023-06-22 11:18:32 -0400

    bumps emdfile to 0.0.9
    Former-commit-id: d3e346c8f764999b616f08f4f9e5724562dffc76

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
         'gdown >= 4.4.0',
         'dask >= 2.3.0',
         'distributed >= 2.3.0',
-        'emdfile >= 0.0.6',
+        'emdfile >= 0.0.9',
         ],
     extras_require={
         'ipyparallel': ['ipyparallel >= 6.2.4', 'dill >= 0.3.3'],
