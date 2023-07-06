acbb33abee674644818d4d6b0985a9ac90c9aad9
Steve Zeltmann <37132012+sezelt@users.noreply.github.com>
2023-07-06 14:24:52 -0400

    change how optimized dict is assembled
    Co-authored-by: Georgios Varnavides <gvarnavides@berkeley.edu>
    Former-commit-id: 104a5cd4db1a20c720a1795122fe6c4f78d4e0d4

--- py4DSTEM/process/phase/parameter_optimize.py
+++ py4DSTEM/process/phase/parameter_optimize.py
@@ -272,12 +272,14 @@ class PtychographyOptimizer:
             where the OptimizationParameter items have been replaced with the optimal
             values obtained from the optimizer
         """
-        init_opt = self._replace_optimized_parameters(
-            self._init_args, self._parameter_list
-        )
-        affine_opt = self._replace_optimized_parameters(
-            self._affine_args, self._parameter_list
-        )
+        optimized_dict = {p.name: v for p,v in zip(self._parameter_list,self._skopt_result.x)}
+        
+        filtered_dict = { k: v for k,v in optimized_dict.items() if k in self._init_args }
+        init_opt = self._init_args | filtered_dict
+        
+        filtered_dict = { k: v for k,v in optimized_dict.items() if k in self._affine_args }
+        affine_opt = self._affine_args | filtered_dict
+        
         affine_transform = partial(AffineTransform, **self._affine_static_args)(
             **affine_opt
         )
@@ -285,29 +287,14 @@ class PtychographyOptimizer:
             affine_transform, init_opt["datacube"]
         )
         init_opt["initial_scan_positions"] = scan_positions
-
-        prep_opt = self._replace_optimized_parameters(
-            self._preprocess_args, self._parameter_list
-        )
-        reco_opt = self._replace_optimized_parameters(
-            self._reconstruction_args, self._parameter_list
-        )
-
+        
+        filtered_dict = { k: v for k,v in optimized_dict.items() if k in self._preprocess_args }
+        prep_opt = self._preprocess_args | filtered_dict
+        
+        filtered_dict = { k: v for k,v in optimized_dict.items() if k in self._reconstruction_args }
+        reco_opt = self._reconstruction_args | filtered_dict
+        
         return init_opt, prep_opt, reco_opt
-
-    def _replace_optimized_parameters(self, arg_dict, parameters):
-        opt_args = {}
-        for k, v in arg_dict.items():
-            # Check for optimization parameters
-            if isinstance(v, OptimizationParameter):
-                # Find the right parameter in the list
-                # There is probably a better way to do this inverse mapping!
-                for i, p in enumerate(parameters):
-                    if p.name == k:
-                        opt_args[k] = self._skopt_result.x[i]
-            else:
-                opt_args[k] = v
-        return opt_args
 
     def _split_static_and_optimization_vars(self, argdict):
         static_args = {}
