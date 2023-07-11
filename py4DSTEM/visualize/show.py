57a29452655d4a56d42bf759e1e6af7892f629a9
Stephanie Ribet <stephanie.ribet@gmail.com>
2023-07-11 05:57:06 -0700

    fix for virtualimage/virtualdiffraction scalebar=True
    Former-commit-id: 64c7a3a871c7917178394ee48ef7830fdea3f189

--- py4DSTEM/visualize/show.py
+++ py4DSTEM/visualize/show.py
@@ -320,6 +320,9 @@ def show(
         if intensity_range is None:
             intensity_range = clipvals
 
+    if scalebar == True: 
+        scalebar = {}
+
     # plot a grid if `ar` is a list, or use multichannel functionality to make an RGBa image
     ar = ar[0] if (isinstance(ar,list) and len(ar) == 1) else ar
     if isinstance(ar,list):
@@ -391,8 +394,6 @@ def show(
             # Output image for plotting
             ar = ar_rgb
 
-    if scalebar == True: 
-        scalebar = {}
 
     # support for native data types
     elif not isinstance(ar,np.ndarray):
