01eafb2a688f04508ce00659e0d11808cd2b0e51
Colin <cophus@gmail.com>
2023-07-28 10:28:07 -0700

    Visualization tweaks
    Former-commit-id: ca785d6e145a32024df4f1a309e9c56fca9890a9

--- py4DSTEM/visualize/show.py
+++ py4DSTEM/visualize/show.py
@@ -679,6 +679,20 @@ def show(
             scalebar['pixelsize'] = pixelsize
             scalebar['pixelunits'] = pixelunits
             scalebar['space'] = space
+            # determine good default scale bar fontsize
+            if figax is not None:
+                # print(figax[0].figsize)
+                # size = figax[1].get_size_inches()
+                # ax_h = figax[1].bbox.transformed(figax[0].gca().transAxes).height
+                bbox = figax[1].get_window_extent()
+                dpi = figax[0].dpi
+                size = (
+                    bbox.width / dpi , 
+                    bbox.height / dpi 
+                    )
+                scalebar['labelsize'] = np.min(np.array(size)) * 3.0
+            if 'labelsize' not in scalebar.keys():
+                scalebar['labelsize'] = np.min(np.array(figsize)) * 2.0
             add_scalebar(ax,scalebar)
 
 
