fixes variance normalization bug
Former-commit-id: b4dbba4455acbf9a3d65337d353b0ba2e23f95e3

py4DSTEM/process/polar/polar_analysis.py
@@ -98,7 +98,7 @@ def calculate_radial_statistics(
         (self.radial_all - self.radial_mean[None, None]) ** 2, axis=(0, 1)
     )
 
-    self.radial_var_norm = self.radial_var
+    self.radial_var_norm = np.copy(self.radial_var)
     sub = self.radial_mean > 0.0
     self.radial_var_norm[sub] /= self.radial_mean[sub] ** 2
 