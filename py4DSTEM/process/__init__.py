9d2e36c14d479c13c597026b585ffe8523de8a2c
bsavitzky <ben.savitzky@gmail.com>
2023-10-23 18:08:29 +0100

    rms deprecated latticevectors module

--- py4DSTEM/process/__init__.py
+++ py4DSTEM/process/__init__.py
@@ -5,6 +5,5 @@ from py4DSTEM.process import phase
 from py4DSTEM.process import calibration
 from py4DSTEM.process import utils
 from py4DSTEM.process import classification
-from py4DSTEM.process import latticevectors
 from py4DSTEM.process import diffraction
 from py4DSTEM.process import wholepatternfit
