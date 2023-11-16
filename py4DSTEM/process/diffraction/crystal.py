5f5d89570c2f700a1363a817ca273bdabd514a93
alex-rakowski <alexrakowski90@gmail.com>
2023-11-15 23:37:46 -0800

    F811, F523/F524, F841, F741 - Corrections

--- py4DSTEM/process/diffraction/crystal.py
+++ py4DSTEM/process/diffraction/crystal.py
@@ -34,7 +34,6 @@ class Crystal:
         save_ang_file,
         symmetry_reduce_directions,
         orientation_map_to_orix_CrystalMap,
-        save_ang_file,
     )
 
     from py4DSTEM.process.diffraction.crystal_viz import (
@@ -433,41 +432,31 @@ class Crystal:
         elif lattice_type == "hexagonal":
             assert (
                 len(latt_params) == 2
-            ), "2 lattice parametere are expected for hexagonal: a, c, but given {len(latt_params)}".format(
-                len(latt_params)
-            )
+            ), f"2 lattice parametere are expected for hexagonal: a, c, but given {len(latt_params)}"
             lattice = mg.core.Lattice.hexagonal(latt_params[0], latt_params[1])
         elif lattice_type == "tetragonal":
             assert (
                 len(latt_params) == 2
-            ), "2 lattice parametere are expected for tetragonal: a, c, but given {len(latt_params)}".format(
-                len(latt_params)
-            )
+            ), f"2 lattice parametere are expected for tetragonal: a, c, but given {len(latt_params)}"
             lattice = mg.core.Lattice.tetragonal(latt_params[0], latt_params[1])
         elif lattice_type == "orthorhombic":
             assert (
                 len(latt_params) == 3
-            ), "3 lattice parametere are expected for orthorhombic: a, b, c, but given {len(latt_params)}".format(
-                len(latt_params)
-            )
+            ), f"3 lattice parametere are expected for orthorhombic: a, b, c, but given {len(latt_params)}"
             lattice = mg.core.Lattice.orthorhombic(
                 latt_params[0], latt_params[1], latt_params[2]
             )
         elif lattice_type == "monoclinic":
             assert (
                 len(latt_params) == 4
-            ), "4 lattice parametere are expected for monoclinic: a, b, c, beta,  but given {len(latt_params)}".format(
-                len(latt_params)
-            )
+            ), f"4 lattice parametere are expected for monoclinic: a, b, c, beta,  but given {len(latt_params)}"
             lattice = mg.core.Lattice.monoclinic(
                 latt_params[0], latt_params[1], latt_params[2], latt_params[3]
             )
         else:
             assert (
                 len(latt_params) == 6
-            ), "all 6 lattice parametere are expected: a, b, c, alpha, beta, gamma, but given {len(latt_params)}".format(
-                len(latt_params)
-            )
+            ), f"all 6 lattice parametere are expected: a, b, c, alpha, beta, gamma, but given {len(latt_params)}"
             lattice = mg.core.Lattice.from_parameters(
                 latt_params[0],
                 latt_params[1],
@@ -660,7 +649,8 @@ class Crystal:
             self.setup_diffraction(300e3)
 
         # Tolerance for angular tests
-        tol = 1e-6
+        # TODO should this be used somewhere ?
+        tol = 1e-6  # noqa: F841
 
         # Parse orientation inputs
         if orientation is not None:
@@ -720,9 +710,9 @@ class Crystal:
         gy_proj = g_diff[1, keep_int]
 
         # Diffracted peak labels
-        h = hkl[0, keep_int]
-        k = hkl[1, keep_int]
-        l = hkl[2, keep_int]
+        h = hkl[0, keep_int]  # noqa: E741
+        k = hkl[1, keep_int]  # noqa: E741
+        l = hkl[2, keep_int]  # noqa: E741
 
         # Output as PointList
         if keep_qz:
@@ -816,7 +806,8 @@ class Crystal:
 
         # check accelerating voltage
         if hasattr(self, "accel_voltage"):
-            accelerating_voltage = self.accel_voltage
+            # TODO should this be used somewhere ?
+            accelerating_voltage = self.accel_voltage  # noqa: F841
         else:
             self.accel_voltage = 300e3
             print("Accelerating voltage not set. Assuming 300 keV!")
