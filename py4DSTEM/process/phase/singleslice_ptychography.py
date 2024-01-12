71191c0720f4ba0b6f98ab149f545e3da5eb2341
Georgios Varnavides <gvarnavides@berkeley.edu>
2024-01-11 17:05:05 -0800

    adding vectorized flag to CoM

--- py4DSTEM/process/phase/singleslice_ptychography.py
+++ py4DSTEM/process/phase/singleslice_ptychography.py
@@ -198,6 +198,7 @@ class SingleslicePtychography(
         force_com_rotation: float = None,
         force_com_transpose: float = None,
         force_com_shifts: float = None,
+        vectorized_com_calculation: bool = True,
         force_scan_sampling: float = None,
         force_angular_sampling: float = None,
         force_reciprocal_sampling: float = None,
@@ -246,6 +247,8 @@ class SingleslicePtychography(
             Amplitudes come from diffraction patterns shifted with
             the CoM in the upper left corner for each probe unless
             shift is overwritten.
+        vectorized_com_calculation: bool, optional
+            If True (default), the memory-intensive CoM calculation is vectorized
         force_scan_sampling: float, optional
             Override DataCube real space scan pixel size calibrations, in Angstrom
         force_angular_sampling: float, optional
@@ -340,6 +343,7 @@ class SingleslicePtychography(
             dp_mask=self._dp_mask,
             fit_function=fit_function,
             com_shifts=force_com_shifts,
+            vectorized_calculation=vectorized_com_calculation,
         )
 
         # estimate rotation / transpose
