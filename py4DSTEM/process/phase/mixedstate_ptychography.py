9f4917008bb525f9fa0fda93c637f5e4595439e5
Georgios Varnavides <gvarnavides@berkeley.edu>
2024-01-12 18:13:45 -0800

    scikit-image by default, poisson as flag
    Former-commit-id: fbc8436496a396d00a35a51098ef196f82a9388e

--- py4DSTEM/process/phase/mixedstate_ptychography.py
+++ py4DSTEM/process/phase/mixedstate_ptychography.py
@@ -622,6 +622,7 @@ class MixedstatePtychography(
         fit_probe_aberrations_max_angular_order: int = 4,
         fit_probe_aberrations_max_radial_order: int = 4,
         fit_probe_aberrations_remove_initial: bool = False,
+        fit_probe_aberrations_using_scikit_image: bool = True,
         butterworth_filter_iter: int = np.inf,
         q_lowpass: float = None,
         q_highpass: float = None,
@@ -714,6 +715,10 @@ class MixedstatePtychography(
             Max radial order of probe aberrations basis functions
         fit_probe_aberrations_remove_initial: bool
             If true, initial probe aberrations are removed before fitting
+        fit_probe_aberrations_using_scikit_image: bool
+            If true, the necessary phase unwrapping is performed using scikit-image. This is more stable, but occasionally leads
+            to a documented bug where the kernel hangs..
+            If false, a poisson-based solver is used for phase unwrapping. This won't hang, but tends to underestimate aberrations.
         butterworth_filter_iter: int, optional
             Number of iterations to run using high-pass butteworth filter
         q_lowpass: float
@@ -931,6 +936,7 @@ class MixedstatePtychography(
                 fit_probe_aberrations_max_angular_order=fit_probe_aberrations_max_angular_order,
                 fit_probe_aberrations_max_radial_order=fit_probe_aberrations_max_radial_order,
                 fit_probe_aberrations_remove_initial=fit_probe_aberrations_remove_initial,
+                fit_probe_aberrations_using_scikit_image=fit_probe_aberrations_using_scikit_image,
                 fix_probe_aperture=a0 < fix_probe_aperture_iter,
                 initial_probe_aperture=self._probe_initial_aperture,
                 fix_positions=a0 < fix_positions_iter,
