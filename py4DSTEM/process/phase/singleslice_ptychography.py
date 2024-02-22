4d222dca0fcb0c005e4d636fbebe4fa206775942
Colin <cophus@gmail.com>
2024-02-22 06:09:33 -0800

    Going back to dev version of phase contrast

--- py4DSTEM/process/phase/singleslice_ptychography.py
+++ py4DSTEM/process/phase/singleslice_ptychography.py
@@ -3,7 +3,7 @@ Module for reconstructing phase objects from 4DSTEM datasets using iterative met
 namely (single-slice) ptychography.
 """
 
-from typing import Mapping, Sequence, Tuple, Union
+from typing import Mapping, Tuple
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -37,9 +37,6 @@ from py4DSTEM.process.phase.utils import (
     polar_aliases,
     polar_symbols,
 )
-
-from py4DSTEM.process.calibration import get_origin_friedel
-from py4DSTEM.process.calibration import fit_origin
 
 
 class SingleslicePtychography(
@@ -91,8 +88,6 @@ class SingleslicePtychography(
     initial_scan_positions: np.ndarray, optional
         Probe positions in Å for each diffraction intensity
         If None, initialized to a grid scan
-    positions_offset_ang: np.ndarray, optional
-        Offset of positions in A
     verbose: bool, optional
         If True, class methods will inherit this and print additional information
     object_type: str, optional
@@ -127,7 +122,6 @@ class SingleslicePtychography(
         initial_object_guess: np.ndarray = None,
         initial_probe_guess: np.ndarray = None,
         initial_scan_positions: np.ndarray = None,
-        positions_offset_ang: np.ndarray = None,
         object_padding_px: Tuple[int, int] = None,
         object_type: str = "complex",
         positions_mask: np.ndarray = None,
@@ -173,7 +167,6 @@ class SingleslicePtychography(
         # Common Metadata
         self._vacuum_probe_intensity = vacuum_probe_intensity
         self._scan_positions = initial_scan_positions
-        self._positions_offset_ang = positions_offset_ang
         self._energy = energy
         self._semiangle_cutoff = semiangle_cutoff
         self._semiangle_cutoff_pixels = semiangle_cutoff_pixels
@@ -194,7 +187,6 @@ class SingleslicePtychography(
         region_of_interest_shape: Tuple[int, int] = None,
         dp_mask: np.ndarray = None,
         fit_function: str = "plane",
-        find_origin_friedel=False,
         plot_center_of_mass: str = "default",
         plot_rotation: bool = True,
         maximize_divergence: bool = False,
@@ -202,8 +194,7 @@ class SingleslicePtychography(
         plot_probe_overlaps: bool = True,
         force_com_rotation: float = None,
         force_com_transpose: float = None,
-        force_com_shifts: Union[Sequence[np.ndarray], Sequence[float]] = None,
-        force_com_measured: Sequence[np.ndarray] = None,
+        force_com_shifts: float = None,
         vectorized_com_calculation: bool = True,
         force_scan_sampling: float = None,
         force_angular_sampling: float = None,
@@ -235,9 +226,6 @@ class SingleslicePtychography(
             Mask for datacube intensities (Qx,Qy)
         fit_function: str, optional
             2D fitting function for CoM fitting. One of 'plane','parabola','bezier_two'
-        get_origin_friedel: bool
-            Use Friedel origin finding instead of center of mass.
-            TODO - fix GPU implementation.
         plot_center_of_mass: str, optional
             If 'default', the corrected CoM arrays will be displayed
             If 'all', the computed and fitted CoM arrays will be displayed
@@ -257,8 +245,6 @@ class SingleslicePtychography(
             Amplitudes come from diffraction patterns shifted with
             the CoM in the upper left corner for each probe unless
             shift is overwritten.
-        force_com_measured: tuple of ndarrays (CoMx measured, CoMy measured)
-            Force CoM measured shifts
         vectorized_com_calculation: bool, optional
             If True (default), the memory-intensive CoM calculation is vectorized
         force_scan_sampling: float, optional
@@ -319,7 +305,6 @@ class SingleslicePtychography(
             self._vacuum_probe_intensity,
             self._dp_mask,
             force_com_shifts,
-            force_com_measured,
         ) = self._preprocess_datacube_and_vacuum_probe(
             self._datacube,
             diffraction_intensities_shape=self._diffraction_intensities_shape,
@@ -328,7 +313,6 @@ class SingleslicePtychography(
             vacuum_probe_intensity=self._vacuum_probe_intensity,
             dp_mask=self._dp_mask,
             com_shifts=force_com_shifts,
-            com_measured=force_com_measured,
         )
 
         # calibrations
@@ -346,51 +330,21 @@ class SingleslicePtychography(
                 self._semiangle_cutoff_pixels * self._angular_sampling[0]
             )
 
-        # find origins
-        if find_origin_friedel:
-            (
-                self._com_measured_x,
-                self._com_measured_y,
-            ) = get_origin_friedel(
-                self._datacube,
-                upsample_factor=10,
-                # mask = mask,
-                # device = self._device,
-                # device = self._storage,
-            )
-            (
-                self._com_fitted_x,
-                self._com_fitted_y,
-                self._com_res_x,
-                self._com_res_y,
-            ) = fit_origin(
-                (self._com_measured_x, self._com_measured_y),
-                robust=True,
-            )
-            self._com_normalized_x = self._com_measured_x - np.mean(
-                self._com_measured_x
-            )
-            self._com_normalized_y = self._com_measured_x - np.mean(
-                self._com_measured_x
-            )
-
-        else:
-            # calculate CoM
-            (
-                self._com_measured_x,
-                self._com_measured_y,
-                self._com_fitted_x,
-                self._com_fitted_y,
-                self._com_normalized_x,
-                self._com_normalized_y,
-            ) = self._calculate_intensities_center_of_mass(
-                _intensities,
-                dp_mask=self._dp_mask,
-                fit_function=fit_function,
-                com_shifts=force_com_shifts,
-                vectorized_calculation=vectorized_com_calculation,
-                com_measured=force_com_measured,
-            )
+        # calculate CoM
+        (
+            self._com_measured_x,
+            self._com_measured_y,
+            self._com_fitted_x,
+            self._com_fitted_y,
+            self._com_normalized_x,
+            self._com_normalized_y,
+        ) = self._calculate_intensities_center_of_mass(
+            _intensities,
+            dp_mask=self._dp_mask,
+            fit_function=fit_function,
+            com_shifts=force_com_shifts,
+            vectorized_calculation=vectorized_com_calculation,
+        )
 
         # estimate rotation / transpose
         (
@@ -459,7 +413,6 @@ class SingleslicePtychography(
             self._scan_positions,
             self._positions_mask,
             self._object_padding_px,
-            self._positions_offset_ang,
         )
 
         # initialize object
@@ -509,26 +462,25 @@ class SingleslicePtychography(
         self._probe_initial = self._probe.copy()
         self._probe_initial_aperture = xp.abs(xp.fft.fft2(self._probe))
 
-        if object_fov_mask is None or plot_probe_overlaps:
-            # overlaps
-            if max_batch_size is None:
-                max_batch_size = self._num_diffraction_patterns
+        # overlaps
+        if max_batch_size is None:
+            max_batch_size = self._num_diffraction_patterns
 
-            probe_overlap = xp.zeros(self._object_shape, dtype=xp.float32)
+        probe_overlap = xp.zeros(self._object_shape, dtype=xp.float32)
 
-            for start, end in generate_batches(
-                self._num_diffraction_patterns, max_batch=max_batch_size
-            ):
-                # batch indices
-                positions_px = self._positions_px[start:end]
-                positions_px_fractional = positions_px - xp_storage.round(positions_px)
+        for start, end in generate_batches(
+            self._num_diffraction_patterns, max_batch=max_batch_size
+        ):
+            # batch indices
+            positions_px = self._positions_px[start:end]
+            positions_px_fractional = positions_px - xp_storage.round(positions_px)
 
-                shifted_probes = fft_shift(self._probe, positions_px_fractional, xp)
-                probe_overlap += self._sum_overlapping_patches_bincounts(
-                    xp.abs(shifted_probes) ** 2, positions_px
-                )
+            shifted_probes = fft_shift(self._probe, positions_px_fractional, xp)
+            probe_overlap += self._sum_overlapping_patches_bincounts(
+                xp.abs(shifted_probes) ** 2, positions_px
+            )
 
-            del shifted_probes
+        del shifted_probes
 
         # initialize object_fov_mask
         if object_fov_mask is None:
@@ -538,16 +490,14 @@ class SingleslicePtychography(
                 probe_overlap_blurred > 0.25 * probe_overlap_blurred.max()
             )
             del probe_overlap_blurred
-        elif object_fov_mask is True:
-            self._object_fov_mask = np.full(self._object_shape, True)
         else:
             self._object_fov_mask = np.asarray(object_fov_mask)
         self._object_fov_mask_inverse = np.invert(self._object_fov_mask)
 
+        probe_overlap = asnumpy(probe_overlap)
+
         # plot probe overlaps
         if plot_probe_overlaps:
-            probe_overlap = asnumpy(probe_overlap)
-
             figsize = kwargs.pop("figsize", (9, 4))
             chroma_boost = kwargs.pop("chroma_boost", 1)
             power = kwargs.pop("power", 2)
@@ -656,7 +606,6 @@ class SingleslicePtychography(
         object_positivity: bool = True,
         shrinkage_rad: float = 0.0,
         fix_potential_baseline: bool = True,
-        detector_fourier_mask: np.ndarray = None,
         store_iterations: bool = False,
         progress_bar: bool = True,
         reset: bool = None,
@@ -763,9 +712,6 @@ class SingleslicePtychography(
             Phase shift in radians to be subtracted from the potential at each iteration
         fix_potential_baseline: bool
             If true, the potential mean outside the FOV is forced to zero at each iteration
-        detector_fourier_mask: np.ndarray
-            Corner-centered mask to apply at the detector-plane for zeroing-out unreliable gradients.
-            Useful when detector has artifacts such as dead-pixels. Usually binary.
         store_iterations: bool, optional
             If True, reconstructed objects and probes are stored at each iteration
         progress_bar: bool, optional
@@ -801,7 +747,6 @@ class SingleslicePtychography(
         if object_type is not None:
             self._switch_object_type(object_type)
 
-        xp = self._xp
         xp_storage = self._xp_storage
         device = self._device
         asnumpy = self._asnumpy
@@ -844,11 +789,6 @@ class SingleslicePtychography(
             np.random.seed(seed_random)
         else:
             max_batch_size = self._num_diffraction_patterns
-
-        if detector_fourier_mask is None:
-            detector_fourier_mask = xp.ones(self._amplitudes[0].shape)
-        else:
-            detector_fourier_mask = xp.asarray(detector_fourier_mask)
 
         # initialization
         self._reset_reconstruction(store_iterations, reset)
@@ -899,7 +839,6 @@ class SingleslicePtychography(
                     positions_px_fractional,
                     amplitudes_device,
                     self._exit_waves,
-                    detector_fourier_mask,
                     use_projection_scheme,
                     projection_a,
                     projection_b,
