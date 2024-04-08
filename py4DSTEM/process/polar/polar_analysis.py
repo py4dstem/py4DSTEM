8a852fa1da4ba266d9d23c150ecb9b951cb89b31
Colin <cophus@gmail.com>
2024-04-08 11:52:05 -0700

    Removing commented out regions

--- py4DSTEM/process/polar/polar_analysis.py
+++ py4DSTEM/process/polar/polar_analysis.py
@@ -993,28 +993,3 @@ def background_pca(
             )
 
     return im_pca, coef_pca
-
-
-# # Progressbar taken from stackexchange:
-# # https://stackoverflow.com/questions/3160699/python-progress-bar
-# def update_progress(progress):
-#     barLength = 60 # Modify this to change the length of the progress bar
-#     status = ""
-#     if isinstance(progress, int):
-#         progress = float(progress)
-#     if not isinstance(progress, float):
-#         progress = 0
-#         status = "error: progress var must be float\r\n"
-#     if progress < 0:
-#         progress = 0
-#         status = "Halt...\r\n"
-#     if progress >= 1:
-#         progress = 1
-#         status = "Done...\r\n"
-#     block = int(round(barLength*progress))
-#     text = "\rPercent: [{0}] {1}% {2}".format( "#"*block + "-"*(barLength-block), 
-#         np.round(progress*100,4), 
-#         status)
-#     sys.stdout.write(text)
-#     sys.stdout.flush()
-
