777c9307c1e4161e9c7a3893eeae3e40b924d4a7
Stephanie Ribet <stephanie.ribet@gmail.com>
2024-05-18 15:11:21 -0400

    stealing device stuff from phase contrast :)

--- py4DSTEM/tomography/tomography.py
+++ py4DSTEM/tomography/tomography.py
@@ -3,10 +3,21 @@ import numpy as np
 
 from py4DSTEM.datacube import DataCube
 from py4DSTEM.process.diffraction import Crystal
+from py4DSTEM.process.phase.utils import copy_to_device
 
 from scipy.spatial.transform import Rotation as R
 
 from typing import Sequence
+
+try:
+    import cupy as cp
+
+    get_array_module = cp.get_array_module
+except (ImportError, ModuleNotFoundError):
+    cp = None
+
+    def get_array_module(*args):
+        return np
 
 
 class Tomography:
@@ -14,16 +25,25 @@ class Tomography:
 
     def __init__(
         self,
-        datacube: Sequence[DataCube] = None,
+        datacubes: Sequence[DataCube] = None,
         rotation: Sequence[np.ndarray] = None,
         translaton: Sequence[np.ndarray] = None,
         initial_object_guess: np.ndarray = None,
         verbose: bool = True,
         device: str = "cpu",
         storage: str = "cpu",
+        clear_fft_cache: bool = True,
         name: str = "tomography",
     ):
         """ """
+
+        self._datacubes = datacubes
+        self._rotation = rotation
+        self._translaton = translaton
+        self._verbose = verbose
+
+        self.set_device(device, clear_fft_cache)
+        self.set_storage(storage)
 
     def forward(
         self,
@@ -92,27 +112,28 @@ class Tomography:
             projection of current object
 
         """
+        xp = self._xp
 
         s = current_object.shape
 
-        tilt = np.deg2rad(tilt_deg)
+        tilt = xp.deg2rad(tilt_deg)
 
-        padding = int(np.ceil(np.abs(np.tan(tilt) * s[2])))
+        padding = int(xp.ceil(xp.abs(xp.tan(tilt) * s[2])))
 
-        line_z = np.arange(0, 1, 1 / num_points) * (s[2] - 1)
-        line_y = line_z * np.tan(tilt) + padding
+        line_z = xp.arange(0, 1, 1 / num_points) * (s[2] - 1)
+        line_y = line_z * xp.tan(tilt) + padding
 
-        offset = np.arange(s[1], dtype="int")
+        offset = xp.arange(s[1], dtype="int")
 
-        current_object_reshape = np.pad(
-            current_object[x_index],
+        current_object_reshape = xp.pad(
+            xp.asarray(current_object[x_index]),
             ((padding, padding), (0, 0), (0, 0), (0, 0), (0, 0)),
         ).reshape(((s[1] + padding * 2) * s[2], s[3], s[4], s[5]))
 
-        current_object_projected = np.zeros((s[1], s[3], s[4], s[5]))
+        current_object_projected = xp.zeros((s[1], s[3], s[4], s[5]))
 
-        yF = np.floor(line_y).astype("int")
-        zF = np.floor(line_z).astype("int")
+        yF = xp.floor(line_y).astype("int")
+        zF = xp.floor(line_z).astype("int")
         dy = line_y - yF
         dz = line_z - zF
 
@@ -131,15 +152,15 @@ class Tomography:
                     inds = [yF + 1, zF + 1]
                     weights = (dy) * (dz)
 
-            indy = np.tile(inds[0], (s[1], 1)) + offset[:, None]
-            indz = np.tile(inds[1], (s[1], 1))
+            indy = xp.tile(inds[0], (s[1], 1)) + offset[:, None]
+            indz = xp.tile(inds[1], (s[1], 1))
             current_object_projected += (
                 current_object_reshape[
-                    np.ravel_multi_index(
+                    xp.ravel_multi_index(
                         (indy, indz), (s[1] + 2 * padding, s[2]), mode="clip"
                     )
                 ]
-                * np.tile(weights, (s[1], 1))[:, :, None, None, None]
+                * xp.tile(weights, (s[1], 1))[:, :, None, None, None]
             ).sum(1)
 
         current_object_projected
@@ -167,28 +188,30 @@ class Tomography:
             projection of current object sliced in diffraciton space
 
         """
+        xp = self._xp
+
         s = current_object_projected.shape
 
-        tilt = np.deg2rad(tilt_deg)
+        tilt = xp.deg2rad(tilt_deg)
 
-        l = s[-1] * np.cos(tilt)
-        line_y_diff = np.arange(-1 * (l) / 2, l / 2, l / s[-1])
-        line_z_diff = line_y_diff * np.tan(tilt)
+        l = s[-1] * xp.cos(tilt)
+        line_y_diff = xp.arange(-1 * (l) / 2, l / 2, l / s[-1])
+        line_z_diff = line_y_diff * xp.tan(tilt)
 
         line_y_diff[line_y_diff < 0] = s[-1] + line_y_diff[line_y_diff < 0]
         line_z_diff[line_y_diff < 0] = s[-1] + line_z_diff[line_y_diff < 0]
 
-        order = np.argsort(line_y_diff)
+        order = xp.argsort(line_y_diff)
         line_y_diff = line_y_diff[order]
         line_z_diff = line_z_diff[order]
 
-        yF_diff = np.floor(line_y_diff).astype("int")
-        zF_diff = np.floor(line_z_diff).astype("int")
+        yF_diff = xp.floor(line_y_diff).astype("int")
+        zF_diff = xp.floor(line_z_diff).astype("int")
         dy_diff = line_y_diff - yF_diff
         dz_diff = line_z_diff - zF_diff
 
-        current_object_sliced = np.zeros((s[0], s[-1], s[-1]))
-        current_object_projected = np.pad(
+        current_object_sliced = xp.zeros((s[0], s[-1], s[-1]))
+        current_object_projected = xp.pad(
             current_object_projected, ((0, 0), (0, 0), (0, 1), (0, 1))
         )
 
@@ -214,7 +237,7 @@ class Tomography:
 
         current_object_sliced
 
-        return current_object_sliced
+        return self._asnumpy(current_object_sliced)
 
     def make_test_object(
         self,
@@ -251,19 +274,21 @@ class Tomography:
         test_object: np.ndarray
             6D test object
         """
-        test_object = np.zeros((sx, sy, sz, sq, sq, sq))
+        xp_storage = self._xp_storage
+
+        test_object = xp_storage.zeros((sx, sy, sz, sq, sq, sq))
 
         diffraction_cloud = self.make_diffraction_cloud(sq, q_max, [0, 0, 0])
 
         test_object[:, :, :, 0, 0, 0] = diffraction_cloud.sum()
 
         for a0 in range(num):
-            s1 = np.random.randint(r, sx - r)
-            s2 = np.random.randint(r, sy - r)
-            h = np.random.randint(r, sz - r, size=1)
-            t = np.random.randint(0, 360, size=3)
+            s1 = xp_storage.random.randint(r, sx - r)
+            s2 = xp_storage.random.randint(r, sy - r)
+            h = xp_storage.random.randint(r, sz - r, size=1)
+            t = xp_storage.random.randint(0, 360, size=3)
 
-            cloud = self.make_diffraction_cloud(sq, q_max, t)
+            cloud = xp_storage.asarray(self.make_diffraction_cloud(sq, q_max, t))
 
             test_object[s1 - r : s1 + r, s2 - r : s2 + r, h[0] - r : h[0] + r] = cloud
 
@@ -293,18 +318,19 @@ class Tomography:
             3D structure factor
 
         """
+        xp = self._xp
 
         gold = self.make_gold(q_max)
 
-        diffraction_cloud = np.zeros((sq, sq, sq))
+        diffraction_cloud = xp.zeros((sq, sq, sq))
 
         q_step = q_max * 2 / (sq - 1)
 
-        qz = np.fft.ifftshift(np.arange(sq) * q_step - q_step * (sq - 1) / 2)
-        qx = np.fft.ifftshift(np.arange(sq) * q_step - q_step * (sq - 1) / 2)
-        qy = np.fft.ifftshift(np.arange(sq) * q_step - q_step * (sq - 1) / 2)
+        qz = xp.fft.ifftshift(xp.arange(sq) * q_step - q_step * (sq - 1) / 2)
+        qx = xp.fft.ifftshift(xp.arange(sq) * q_step - q_step * (sq - 1) / 2)
+        qy = xp.fft.ifftshift(xp.arange(sq) * q_step - q_step * (sq - 1) / 2)
 
-        qxa, qya, qza = np.meshgrid(qx, qy, qz, indexing="ij")
+        qxa, qya, qza = xp.meshgrid(qx, qy, qz, indexing="ij")
 
         g_vecs = gold.g_vec_all.copy()
         r = R.from_euler("zxz", [rot[0], rot[1], rot[2]])
@@ -314,17 +340,17 @@ class Tomography:
 
         for a0 in range(gold.g_vec_all.shape[1]):
             bragg_spot = g_vecs[:, a0]
-            distance = np.sqrt(
+            distance = xp.sqrt(
                 (qxa - bragg_spot[0]) ** 2
                 + (qya - bragg_spot[1]) ** 2
                 + (qza - bragg_spot[2]) ** 2
             )
 
             update_index = distance < cut_off
-            update = np.zeros((distance.shape))
+            update = xp.zeros((distance.shape))
             update[update_index] = cut_off - distance[update_index]
-            update -= np.min(update)
-            update /= np.sum(update)
+            update -= xp.min(update)
+            update /= xp.sum(update)
             update *= gold.struct_factors_int[a0]
             diffraction_cloud += update
 
@@ -364,3 +390,74 @@ class Tomography:
         crystal.calculate_structure_factors(q_max)
 
         return crystal
+
+    def set_device(self, device, clear_fft_cache):
+        """
+        Sets calculation device.
+
+        Parameters
+        ----------
+        device: str
+            Calculation device will be perfomed on. Must be 'cpu' or 'gpu'
+
+        Returns
+        --------
+        self: PhaseReconstruction
+            Self to enable chaining
+        """
+
+        if clear_fft_cache is not None:
+            self._clear_fft_cache = clear_fft_cache
+
+        if device is None:
+            return self
+
+        if device == "cpu":
+            import scipy
+
+            self._xp = np
+            self._scipy = scipy
+
+        elif device == "gpu":
+            from cupyx import scipy
+
+            self._xp = cp
+            self._scipy = scipy
+
+        else:
+            raise ValueError(f"device must be either 'cpu' or 'gpu', not {device}")
+
+        self._device = device
+
+        return self
+
+    def set_storage(self, storage):
+        """
+        Sets storage device.
+
+        Parameters
+        ----------
+        storage: str
+            Device arrays will be stored on. Must be 'cpu' or 'gpu'
+
+        Returns
+        --------
+        self: PhaseReconstruction
+            Self to enable chaining
+        """
+
+        if storage == "cpu":
+            self._xp_storage = np
+
+        elif storage == "gpu":
+            if self._xp is np:
+                raise ValueError("storage='gpu' and device='cpu' is not supported")
+            self._xp_storage = cp
+
+        else:
+            raise ValueError(f"storage must be either 'cpu' or 'gpu', not {storage}")
+
+        self._asnumpy = copy_to_device
+        self._storage = storage
+
+        return self
