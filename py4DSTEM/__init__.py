ae64d83991cf290990a1df292422caa249692833
alex-rakowski <alexrakowski90@gmail.com>
2024-06-29 21:49:30 +0100

    adding multiple errored files

--- py4DSTEM/__init__.py
+++ py4DSTEM/__init__.py
@@ -1,7 +1,7 @@
 from py4DSTEM.version import __version__
 from emdfile import tqdmnd
 
-y = 250 + x 
+y = 2500 + x 
 ### io
 
 # substructure
--- py4DSTEM/utils/__init__.py
+++ py4DSTEM/utils/__init__.py
@@ -1,1 +1,2 @@
 from py4DSTEM.utils.configuration_checker import check_config
+y = 23 * z
