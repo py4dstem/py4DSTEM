f9b2f639883763d4c1ae8d22b87533ebca089501
Stephanie Ribet <stephanie.ribet@gmail.com>
2024-08-29 11:04:40 -0700

    numpy less than 2

--- setup.py
+++ setup.py
@@ -23,11 +23,11 @@ setup(
     keywords="STEM,4DSTEM",
     python_requires=">=3.10",
     install_requires=[
-        "numpy >= 1.19",
+        "numpy >= 1.19, < 2.0",
         "scipy >= 1.5.2",
         "h5py >= 3.2.0",
         "hdf5plugin >= 4.1.3",
-        "ncempy >= 1.8.1",
+        "ncempy >= 1.8.1, < 1.11.2",
         "matplotlib >= 3.2.2",
         "scikit-image >= 0.17.2",
         "scikit-learn >= 0.23.2, < 1.5",
