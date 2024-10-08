9739a1e0bdec0fbc4f68c9b11bfea0f2ed1c03c3
Georgios Varnavides <gvarnavides@berkeley.edu>
2024-10-08 09:59:47 -0700

    how about this indexing

--- py4DSTEM/process/phase/direct_ptychography.py
+++ py4DSTEM/process/phase/direct_ptychography.py
@@ -540,10 +540,12 @@ class DirectPtychography(
                 * self._semiangle_cutoff
                 / self.angular_sampling[0]
             )
-            low_ind_x = trotter_inds[0][f[*trotter_inds] < q_probe][0]
-            low_ind_y = trotter_inds[1][f[*trotter_inds] < q_probe][0]
-            high_ind_x = trotter_inds[0][f[*trotter_inds] > q_probe][0]
-            high_ind_y = trotter_inds[1][f[*trotter_inds] > q_probe][0]
+
+            bf_inds = f[*trotter_inds] < q_probe
+            low_ind_x = trotter_inds[0][bf_inds][0]
+            low_ind_y = trotter_inds[1][bf_inds][0]
+            high_ind_x = trotter_inds[0][~bf_inds][0]
+            high_ind_y = trotter_inds[1][~bf_inds][0]
 
             figsize = kwargs.pop("figsize", (13, 4))
             chroma_boost = kwargs.pop("chroma_boost", 1)
